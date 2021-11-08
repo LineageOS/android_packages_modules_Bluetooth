9bff24272e335c4bb3727c72d55e4954314bb684
Chris Manton <cmanton@google.com>
2021-11-07 22:44:08 -0800

    dumpsys: Move l2cap dumpsys to proper file
    Bug: 328135211
    Test: m .
    Flag: EXEMPT, Mechanical Refactor
    
    Change-Id: Ic35ad9c4439946c62a798228f956262a8ff735d1

--- system/stack/l2cap/l2c_api.cc
+++ system/stack/l2cap/l2c_api.cc
@@ -40,6 +40,7 @@
 #include "include/check.h"
 #include "internal_include/bt_target.h"
 #include "internal_include/bt_trace.h"
+#include "main/shim/dumpsys.h"
 #include "main/shim/entry.h"
 #include "os/log.h"
 #include "os/system_properties.h"
@@ -1773,3 +1774,27 @@ bool L2CA_isMediaChannel(uint16_t handle, uint16_t channel_id,
 
   return ret;
 }
+
+using namespace bluetooth;
+
+#define DUMPSYS_TAG "shim::legacy::l2cap"
+
+void L2CA_Dumpsys(int fd) {
+  LOG_DUMPSYS_TITLE(fd, DUMPSYS_TAG);
+  for (int i = 0; i < MAX_L2CAP_LINKS; i++) {
+    const tL2C_LCB& lcb = l2cb.lcb_pool[i];
+    if (!lcb.in_use) continue;
+    LOG_DUMPSYS(fd, "link_state:%s", link_state_text(lcb.link_state).c_str());
+    LOG_DUMPSYS(fd, "handle:0x%04x", lcb.Handle());
+
+    const tL2C_CCB* ccb = lcb.ccb_queue.p_first_ccb;
+    while (ccb != nullptr) {
+      LOG_DUMPSYS(
+          fd, "  active channel lcid:0x%04x rcid:0x%04x is_ecoc:%s in_use:%s",
+          ccb->local_cid, ccb->remote_cid, logbool(ccb->ecoc).c_str(),
+          logbool(ccb->in_use).c_str());
+      ccb = ccb->p_next_ccb;
+    }
+  }
+}
+#undef DUMPSYS_TAG
