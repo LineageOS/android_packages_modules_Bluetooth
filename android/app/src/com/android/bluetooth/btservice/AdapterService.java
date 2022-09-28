bb8740e4f0138e793c0b2e0fe42133e834e58456
Rahul Arya <aryarahul@google.com>
2022-09-28 20:54:29 +0000

    Pass IRK rotation flag into native
    Ignore-AOSP-First: Security fix
    Test: Flashed build, set the prop and watched the stack restart
    Bug: 195410559
    Change-Id: I6496b34c144e697308109d5ee9528f4ae5c9b4b4
    Merged-In: I6496b34c144e697308109d5ee9528f4ae5c9b4b4
    (cherry picked from commit f7443b78ac56071c8dce2be865f96a566b51433b)
    Merged-In: I6496b34c144e697308109d5ee9528f4ae5c9b4b4

--- android/app/src/com/android/bluetooth/btservice/AdapterService.java
+++ android/app/src/com/android/bluetooth/btservice/AdapterService.java
@@ -5035,6 +5035,7 @@ public class AdapterService extends Service {
     private static final String GD_RUST_FLAG = "INIT_gd_rust";
     private static final String GD_LINK_POLICY_FLAG = "INIT_gd_link_policy";
     private static final String GATT_ROBUST_CACHING_FLAG = "INIT_gatt_robust_caching";
+    private static final String IRK_ROTATION_FLAG = "INIT_irk_rotation";
 
     /**
      * Logging flags logic (only applies to DEBUG and VERBOSE levels):
@@ -5090,6 +5091,9 @@ public class AdapterService extends Service {
         if (DeviceConfig.getBoolean(DeviceConfig.NAMESPACE_BLUETOOTH,
                 GATT_ROBUST_CACHING_FLAG, false)) {
             initFlags.add(String.format("%s=%s", GATT_ROBUST_CACHING_FLAG, "true"));
+        }
+        if (DeviceConfig.getBoolean(DeviceConfig.NAMESPACE_BLUETOOTH, IRK_ROTATION_FLAG, false)) {
+            initFlags.add(String.format("%s=%s", IRK_ROTATION_FLAG, "true"));
         }
         if (DeviceConfig.getBoolean(DeviceConfig.NAMESPACE_BLUETOOTH,
                 LOGGING_DEBUG_ENABLED_FOR_ALL_FLAG, false)) {
