3c128277e89004b60030d40c67b716afdaae2df8
Christian Hoffmann <chrmhoffmann@gmail.com>
2022-12-12 02:20:20 -0500

    legacy: Split header for hci legacy definitions
    * To expose to c legacy hals
    * Like hardware/realtek/rtkbt.
    
    Change-Id: Ic67f9697b032b66a8af364ed41e90d51dc75dd6f

--- system/hci/include/hci_layer.h
+++ system/hci/include/hci_layer.h
@@ -24,25 +24,8 @@
 #include "stack/include/bt_hdr.h"
 #include "stack/include/bt_types.h"
 
-///// LEGACY DEFINITIONS /////
+#include "hci/include/hci_layer_legacy.h"
 
-/* Message event mask across Host/Controller lib and stack */
-#define MSG_EVT_MASK 0xFF00     /* eq. BT_EVT_MASK */
-#define MSG_SUB_EVT_MASK 0x00FF /* eq. BT_SUB_EVT_MASK */
-
-/* Message event ID passed from Host/Controller lib to stack */
-#define MSG_HC_TO_STACK_HCI_ERR 0x1300      /* eq. BT_EVT_TO_BTU_HCIT_ERR */
-#define MSG_HC_TO_STACK_HCI_ISO 0x1700      /* eq. BT_EVT_TO_BTU_HCI_ISO */
-#define MSG_HC_TO_STACK_HCI_EVT 0x1000      /* eq. BT_EVT_TO_BTU_HCI_EVT */
-
-/* Message event ID passed from stack to vendor lib */
-#define MSG_STACK_TO_HC_HCI_ISO 0x2d00 /* eq. BT_EVT_TO_LM_HCI_ISO */
-#define MSG_STACK_TO_HC_HCI_CMD 0x2000 /* eq. BT_EVT_TO_LM_HCI_CMD */
-
-/* Local Bluetooth Controller ID for BR/EDR */
-#define LOCAL_BR_EDR_CONTROLLER_ID 0
-
-///// END LEGACY DEFINITIONS /////
 typedef struct packet_fragmenter_t packet_fragmenter_t;
 typedef uint16_t command_opcode_t;
 
