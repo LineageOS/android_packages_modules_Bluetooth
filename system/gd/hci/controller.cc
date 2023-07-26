8e9632925df8acc0185d54ba8998fe202c3c68bc
LuK1337 <priv.luk@gmail.com>
2023-07-26 22:03:00 +0200

    gd: hci: Allow disabling selected local commands
    This allows us to disable selected commands in case they behave in
    unexpected ways. e.g. bluetooth.hci.disabled_commands=377 will disable
    LE_EXTENDED_CREATE_CONNECTION.
    
    Change-Id: I1fcc624df9fd07200a8406c51772ccb856a4258e

--- system/gd/hci/controller.cc
+++ system/gd/hci/controller.cc
@@ -16,6 +16,7 @@
 
 #include "hci/controller.h"
 
+#include <android-base/strings.h>
 #include <android_bluetooth_sysprop.h>
 #include <bluetooth/log.h>
 #include <com_android_bluetooth_flags.h>
@@ -48,6 +49,8 @@ constexpr bool kDefaultRpaOffload = false;
 static const std::string kPropertyVendorCapabilitiesEnabled =
         "bluetooth.core.le.vendor_capabilities.enabled";
 static const std::string kPropertyRpaOffload = "bluetooth.core.le.rpa_offload";
+static const char kPropertyDisabledCommands[] =
+        "bluetooth.hci.disabled_commands";
 
 using os::Handler;
 
@@ -358,6 +361,15 @@ struct Controller::impl {
     ErrorCode status = complete_view.GetStatus();
     log::assert_that(status == ErrorCode::SUCCESS, "Status {}", ErrorCodeText(status));
     local_supported_commands_ = complete_view.GetSupportedCommands();
+
+    if (auto disabledCommands = os::GetSystemProperty(kPropertyDisabledCommands)) {
+      for (const auto& command : android::base::Split(*disabledCommands, ",")) {
+        uint16_t index = std::stoi(command);
+        uint16_t byte_index = index / 10;
+        uint16_t bit_index = index % 10;
+        local_supported_commands_[byte_index] &= ~(1 << bit_index);
+      }
+    }
   }
 
   void read_local_extended_features_complete_handler(std::promise<void> promise,
