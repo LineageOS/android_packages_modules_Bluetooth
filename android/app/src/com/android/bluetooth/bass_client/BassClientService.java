96ff2df96523a115d76dfd8a6c9273b74a8dda2f
Rongxuan Liu <rongxuan@google.com>
2023-08-01 22:03:31 +0000

    [le audio] Improve broadcastAssistant callbacks handling
    This commit improved the handling of BroadcastAssistantCallbacks,
    it merged assistant callbacks into BroadcastAssistant for better callbacks management
    and manage callbacks in the similar way as other Bluetooth profiles. e.g. re-register callbacks on ServiceConnected.
    Also it removed unnecessary stack trace log since the client might be non-persistent UI modules.
    
    Bug: 286943323
    Test: atest BluetoothLeBroadcastAssistantTest
    Test: manually check callbacks
    
    Change-Id: Ica4f4869b2b0992d06b62f45ede94021e5e639af

--- android/app/src/com/android/bluetooth/bass_client/BassClientService.java
+++ android/app/src/com/android/bluetooth/bass_client/BassClientService.java
@@ -1388,7 +1388,7 @@ public class BassClientService extends ProfileService {
                 try {
                     invokeCallback(callback, msg);
                 } catch (RemoteException e) {
-                    Log.e(TAG, "Stack:" + Log.getStackTraceString(e));
+                    continue;
                 }
             }
             mCallbacks.finishBroadcast();
