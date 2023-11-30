b5bc2efca215f4d6e0df715bea934d3ecb48b1a0
Wei-Luan Wang <weiluanwang@google.com>
2023-11-30 05:32:49 +0000

    Migrate legacy base::Bind/Closure APIs for SchedulePeriodic
    base::Bind and base::Callback are deprecated. Migrate the usages to
    proper replacements.
    
    The function |RepeatingTimer::SchedulePeriodic| schedule a job that runs
    the |task| periodically so the type of |task| should be a
    base::RepeatingClosure. Use base::BindReapting accordingly.
    
    Bug: 272116782
    Test: m .
    Flag: EXEMPT mechanical refactor
    Change-Id: I793f79bcdd9f446724703138ee8e631b0b3d542a

--- system/btif/src/btif_a2dp_source.cc
+++ system/btif/src/btif_a2dp_source.cc
@@ -818,7 +818,7 @@ static void btif_a2dp_source_audio_tx_start_event(void) {
   wakelock_acquire();
   btif_a2dp_source_cb.media_alarm.SchedulePeriodic(
       btif_a2dp_source_thread.GetWeakPtr(), FROM_HERE,
-      base::Bind(&btif_a2dp_source_audio_handle_timer),
+      base::BindRepeating(&btif_a2dp_source_audio_handle_timer),
 #if BASE_VER < 931007
       base::TimeDelta::FromMilliseconds(
 #else
