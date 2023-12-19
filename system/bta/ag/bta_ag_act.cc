miyu-xu/packages-modules-Bluetooth

Fall back to SCO connection if eSCO connection is failed
Bug: 303168931
Test: m .
Change-Id: I202f7277f8dc5b7633f400de9de9fdb65b291efc

system/bta/ag/bta_ag_act.cc
@@ -394,6 +394,7 @@ void bta_ag_rfc_close(tBTA_AG_SCB* p_scb,
   /* Clear these flags upon SLC teardown */
   p_scb->codec_updated = false;
   p_scb->codec_fallback = false;
+  p_scb->try_zero_retransmission_effort = 0;
   p_scb->codec_msbc_settings = BTA_AG_SCO_MSBC_SETTINGS_T2;
   p_scb->codec_aptx_settings = BTA_AG_SCO_APTX_SWB_SETTINGS_Q0;
   p_scb->is_aptx_swb_codec = false;

system/bta/ag/bta_ag_main.cc
@@ -148,6 +148,7 @@ static tBTA_AG_SCB* bta_ag_scb_alloc(void) {
       p_scb->received_at_bac = false;
       p_scb->codec_updated = false;
       p_scb->codec_fallback = false;
+      p_scb->try_zero_retransmission_effort = 0;
       p_scb->peer_codecs = BTM_SCO_CODEC_CVSD;
       p_scb->sco_codec = BTM_SCO_CODEC_CVSD;
       p_scb->peer_version = HFP_HSP_VERSION_UNKNOWN;

LineageOS/android_packages_modules_Bluetooth

Bug: 316498257

system/bta/ag/bta_ag_act.cc
@@ -394,6 +394,7 @@ void bta_ag_rfc_close(tBTA_AG_SCB* p_scb,
   /* Clear these flags upon SLC teardown */
   p_scb->codec_updated = false;
   p_scb->codec_fallback = false;
+  p_scb->retransmission_effort_retries = 0;
   p_scb->codec_msbc_settings = BTA_AG_SCO_MSBC_SETTINGS_T2;
   p_scb->codec_aptx_settings = BTA_AG_SCO_APTX_SWB_SETTINGS_Q0;
   p_scb->is_aptx_swb_codec = false;

system/bta/ag/bta_ag_main.cc
@@ -148,6 +148,7 @@ static tBTA_AG_SCB* bta_ag_scb_alloc(void) {
       p_scb->received_at_bac = false;
       p_scb->codec_updated = false;
       p_scb->codec_fallback = false;
+      p_scb->retransmission_effort_retries = 0;
       p_scb->peer_codecs = BTM_SCO_CODEC_CVSD;
       p_scb->sco_codec = BTM_SCO_CODEC_CVSD;
       p_scb->peer_version = HFP_HSP_VERSION_UNKNOWN;
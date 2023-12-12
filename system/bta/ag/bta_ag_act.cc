917671e096607b8fcdbf48d02776349cea5e6c8c
Bhushan Shinkar <shinkar@google.com>
2023-12-12 15:42:41 +0000

    Fall back to SCO connection if eSCO connection is failed
    Bug: 303168931
    Bug: 316498257
    Test: m .
    Change-Id: I202f7277f8dc5b7633f400de9de9fdb65b291efc

--- system/bta/ag/bta_ag_act.cc
+++ system/bta/ag/bta_ag_act.cc
@@ -394,6 +394,7 @@ void bta_ag_rfc_close(tBTA_AG_SCB* p_scb,
   /* Clear these flags upon SLC teardown */
   p_scb->codec_updated = false;
   p_scb->codec_fallback = false;
+  p_scb->retransmission_effort_retries = 0;
   p_scb->codec_msbc_settings = BTA_AG_SCO_MSBC_SETTINGS_T2;
   p_scb->codec_aptx_settings = BTA_AG_SCO_APTX_SWB_SETTINGS_Q0;
   p_scb->is_aptx_swb_codec = false;
