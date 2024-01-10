fd938220294ac7e813faca5b35bde4afa3b4a29c
Grzegorz Kołodziejczyk <gkolodziejczyk@google.com>
2024-01-10 15:10:08 +0000

    bass: Avoid double add of devices from group
    When group operation flag is set there is possibility that second device
    from set would not have cached metadata. This change correct caching
    place to internal loop iterating over set devices.
    
    Tag: #feature
    Test: atest LeAudioServiceTest LeAudioBroadcastServiceTest
    Bug: 298080973
    Bug: 308171251
    Change-Id: Iab45ce3d06cf845e3ba3036eec3e5ad280a0006b

--- android/app/src/com/android/bluetooth/bass_client/BassClientService.java
+++ android/app/src/com/android/bluetooth/bass_client/BassClientService.java
@@ -1247,9 +1247,6 @@ public class BassClientService extends ProfileService {
             return;
         }
 
-        /* Store metadata for sink device */
-        mBroadcastMetadataMap.put(sink, sourceMetadata);
-
         byte[] code = sourceMetadata.getBroadcastCode();
         for (BluetoothDevice device : devices) {
             BassClientStateMachine stateMachine = getOrCreateStateMachine(device);
@@ -1291,6 +1288,10 @@ public class BassClientService extends ProfileService {
                         enqueueSourceGroupOp(
                                 device, BassClientStateMachine.ADD_BCAST_SOURCE, sourceMetadata);
                     }
+
+                    /* Store metadata for sink device */
+                    mBroadcastMetadataMap.put(device, sourceMetadata);
+
                     Message message =
                             stateMachine.obtainMessage(BassClientStateMachine.SWITH_BCAST_SOURCE);
                     message.obj = sourceMetadata;
@@ -1319,6 +1320,9 @@ public class BassClientService extends ProfileService {
                     continue;
                 }
             }
+
+            /* Store metadata for sink device */
+            mBroadcastMetadataMap.put(device, sourceMetadata);
 
             if (isGroupOp) {
                 enqueueSourceGroupOp(device, BassClientStateMachine.ADD_BCAST_SOURCE,
