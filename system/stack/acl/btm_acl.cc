7cf1bc98ac88b128d22f35949ba3f47a2a1e97dd
Myles Watson <mylesgw@google.com>
2024-01-10 06:31:11 -0800

    Remove types/class_of_device
    Use GD hci/class_of_device instead.
    
    Bug: 314889276
    Test: mma -j32
    Flag: EXEMPT, no logical change
    Change-Id: I3167f44e12dbba61315c52963975f39f041cc44e

--- system/stack/acl/btm_acl.cc
+++ system/stack/acl/btm_acl.cc
@@ -2550,7 +2550,7 @@ void acl_create_classic_connection(const RawAddress& bd_addr,
 }
 
 void btm_connection_request(const RawAddress& bda,
-                            const bluetooth::types::ClassOfDevice& cod) {
+                            const bluetooth::hci::ClassOfDevice& cod) {
   // Copy Cod information
   DEV_CLASS dc;
 
