b67224356383e93bdfb95308a5f2ac72b9f4a7da
Myles Watson <mylesgw@google.com>
2024-01-10 09:42:54 -0800

    Use std::array for DEV_CLASS
    Bug: 314889276
    Test: mma -j32
    Flag: EXEMPT, no logical change
    Change-Id: I45b423fbc0b93bb3ae341c53e2e3af9aa10edea1

--- system/stack/acl/btm_acl.cc
+++ system/stack/acl/btm_acl.cc
@@ -1331,8 +1331,8 @@ void btm_rejectlist_role_change_device(const RawAddress& bd_addr,
   /* check for carkits */
   const uint32_t cod_audio_device =
       (BTM_COD_SERVICE_AUDIO | BTM_COD_MAJOR_AUDIO) << 8;
-  const uint8_t* dev_class = btm_get_dev_class(bd_addr);
-  if (dev_class == nullptr) return;
+  DEV_CLASS dev_class = btm_get_dev_class(bd_addr);
+  if (dev_class == kDevClassEmpty) return;
   const uint32_t cod =
       ((dev_class[0] << 16) | (dev_class[1] << 8) | dev_class[2]) & 0xffffff;
   if ((hci_status != HCI_SUCCESS) &&
