980bbd4667619fd2711d733765c2ca9f9219c07c
Himanshu Rawat <rwt@google.com>
2024-02-02 19:41:57 +0000

    Set the latency modes for sink, not the source
    Test: mmm packages/modules/Bluetooth
    Test: Manual | connect with LE Audio device supporting DSA
    Bug: 314844884
    Bug: 309665975
    Change-Id: I3801dd03afbf6341bf5e0038f416183dbfdcf014

--- system/audio_hal_interface/le_audio_software.cc
+++ system/audio_hal_interface/le_audio_software.cc
@@ -19,6 +19,8 @@
 
 #include "le_audio_software.h"
 
+#include <android_bluetooth_flags.h>
+
 #include <unordered_map>
 #include <vector>
 
@@ -868,8 +870,18 @@ bool LeAudioClientInterface::ReleaseSource(
 }
 
 void LeAudioClientInterface::SetAllowedDsaModes(DsaModes dsa_modes) {
+  if (!IS_FLAG_ENABLED(leaudio_dynamic_spatial_audio)) {
+    return;
+  }
+
   if (HalVersionManager::GetHalTransport() ==
       BluetoothAudioHalTransport::AIDL) {
+    if (aidl::le_audio::LeAudioSinkTransport::interface_unicast_ == nullptr ||
+        aidl::le_audio::LeAudioSinkTransport::instance_unicast_ == nullptr) {
+      LOG(WARNING) << __func__ << ": LeAudioSourceTransport::interface is null";
+      return;
+    }
+
     std::vector<LatencyMode> latency_modes = {LatencyMode::FREE};
     for (auto dsa_mode : dsa_modes) {
       switch (dsa_mode) {
@@ -890,12 +902,8 @@ void LeAudioClientInterface::SetAllowedDsaModes(DsaModes dsa_modes) {
           break;
       }
     }
-    if (aidl::le_audio::LeAudioSourceTransport::interface) {
-      aidl::le_audio::LeAudioSourceTransport::interface->SetAllowedLatencyModes(
-          latency_modes);
-    } else {
-      LOG(WARNING) << "LeAudioSourceTransport::interface is null";
-    }
+    aidl::le_audio::LeAudioSinkTransport::interface_unicast_
+        ->SetAllowedLatencyModes(latency_modes);
   }
 }
 
