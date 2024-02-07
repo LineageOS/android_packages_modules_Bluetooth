1a1f7bd3ffee21cd02cc7978c414f0026d48d436
Henri Chataing <henrichataing@google.com>
2024-02-07 21:51:29 +0000

    system/stack/acl: Migrate to libbluetooth_log
    Test: m com.android.btservices
    Bug: 305066880
    Flag: EXEMPT, mechanical refactor
    Change-Id: Ia1c045d44e8627fb1a6d71970e4e6bea65ddc269

--- system/stack/include/hci_error_code.h
+++ system/stack/include/hci_error_code.h
@@ -17,6 +17,7 @@
 #pragma once
 
 #include <base/strings/stringprintf.h>
+#include <bluetooth/log.h>
 
 #include <cstdint>
 #include <string>
@@ -147,3 +148,9 @@ inline tHCI_REASON to_hci_reason_code(const uint8_t& reason_code) {
   if (reason_code > _HCI_ERR_MAX_ERR) return HCI_ERR_UNDEFINED;
   return static_cast<tHCI_REASON>(reason_code);
 }
+
+namespace fmt {
+template <>
+struct formatter<tHCI_ERROR_CODE>
+    : string_formatter<tHCI_ERROR_CODE, &hci_error_code_text> {};
+}  // namespace fmt
