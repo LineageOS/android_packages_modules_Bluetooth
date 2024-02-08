86f586674c95d1f3e117049a8b359a2a415bcb84
Chris Manton <cmanton@google.com>
2024-02-07 19:30:01 -0800

    mock: Update mock_main_shim_hci_layer
    Bug: 324339593
    Test: m .
    Flag: EXEMPT, Mock test infrastructure
    
    Change-Id: I69aea1f9a7951db73baea94dccb66496e08fb522

--- system/test/mock/mock_main_shim_hci_layer.cc
+++ system/test/mock/mock_main_shim_hci_layer.cc
@@ -16,13 +16,18 @@
 
 /*
  * Generated mock file from original source file
- *   Functions generated:3
+ *   Functions generated:5
  *
- *  mockcify.pl ver 0.3.0
+ *  mockcify.pl ver 0.7.0
  */
 
 // Mock include file to share data between tests and mock
+#include "test/mock/mock_main_shim_hci_layer.h"
+
 #include "main/shim/hci_layer.h"
+#include "test/common/mock_functions.h"
+
+// Original usings
 
 // Mocked internal structures, if any
 
@@ -33,5 +38,58 @@ void hci_layer_set_interface(const hci_t* interface) {
   test_interface = interface;
 }
 }  // namespace testing
-const hci_t* hci_layer_get_interface() { return testing::test_interface; }
 }  // namespace bluetooth::shim
+
+const hci_t* bluetooth::shim::hci_layer_get_interface() {
+  return testing::test_interface;
+}
+
+namespace test {
+namespace mock {
+namespace main_shim_hci_layer {
+
+// Function state capture and return values, if needed
+struct OnTransmitPacketCommandComplete OnTransmitPacketCommandComplete;
+struct OnTransmitPacketStatus OnTransmitPacketStatus;
+struct hci_on_reset_complete hci_on_reset_complete;
+struct hci_on_shutting_down hci_on_shutting_down;
+
+}  // namespace main_shim_hci_layer
+}  // namespace mock
+}  // namespace test
+
+// Mocked function return values, if any
+namespace test {
+namespace mock {
+namespace main_shim_hci_layer {}  // namespace main_shim_hci_layer
+}  // namespace mock
+}  // namespace test
+
+// Mocked functions, if any
+namespace cpp {
+void OnTransmitPacketCommandComplete(command_complete_cb complete_callback,
+                                     void* context,
+                                     bluetooth::hci::CommandCompleteView view) {
+  inc_func_call_count(__func__);
+  test::mock::main_shim_hci_layer::OnTransmitPacketCommandComplete(
+      complete_callback, context, view);
+}
+void OnTransmitPacketStatus(command_status_cb status_callback, void* context,
+                            std::unique_ptr<OsiObject> command,
+                            bluetooth::hci::CommandStatusView view) {
+  inc_func_call_count(__func__);
+  test::mock::main_shim_hci_layer::OnTransmitPacketStatus(
+      status_callback, context, std::move(command), view);
+}
+}  // namespace cpp
+
+void bluetooth::shim::hci_on_reset_complete() {
+  inc_func_call_count(__func__);
+  test::mock::main_shim_hci_layer::hci_on_reset_complete();
+}
+void bluetooth::shim::hci_on_shutting_down() {
+  inc_func_call_count(__func__);
+  test::mock::main_shim_hci_layer::hci_on_shutting_down();
+}
+// Mocked functions complete
+// END mockcify generation
