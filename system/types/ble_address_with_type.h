abbcef84e125afc15d3f3f982aa6e07a465340aa
Himanshu Rawat <rwt@google.com>
2024-02-15 23:11:12 +0000

    Rename tTypedAddressTransport to tAclLinkSpec
    tAclLinkSpec refers to ACL link specification.
    
    Bug: 325522529
    Test: mmm packages/modules/Bluetooth
    Flag: EXEMPT no logical change
    Change-Id: Ia1ba42d75f0ae49dd5e2c01933ffce4e5851b4e6

--- system/types/ble_address_with_type.h
+++ system/types/ble_address_with_type.h
@@ -151,11 +151,11 @@ struct std::hash<tBLE_BD_ADDR> {
   }
 };
 
-struct tTypedAddressTransport {
+struct tAclLinkSpec {
   tBLE_BD_ADDR addrt;
   tBT_TRANSPORT transport;
 
-  bool operator==(const tTypedAddressTransport rhs) const {
+  bool operator==(const tAclLinkSpec rhs) const {
     if (rhs.addrt != addrt) return false;
 
     if (rhs.transport == BT_TRANSPORT_AUTO || transport == BT_TRANSPORT_AUTO) {
@@ -165,11 +165,9 @@ struct tTypedAddressTransport {
     return rhs.transport == transport;
   }
 
-  bool operator!=(const tTypedAddressTransport rhs) const {
-    return !(*this == rhs);
-  }
+  bool operator!=(const tAclLinkSpec rhs) const { return !(*this == rhs); }
 
-  bool StrictlyEquals(const tTypedAddressTransport rhs) const {
+  bool StrictlyEquals(const tAclLinkSpec rhs) const {
     return rhs.addrt == addrt && rhs.transport == transport;
   }
 
