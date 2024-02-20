miyu-xu/packages-modules-Bluetooth

leaudio: Fix possible race on start stream request from HAL
Bug: 322106376
Bug: 325181889
Test: mmm packages/modules/Bluetooth
Test: manual streaming regression tests

Change-Id: I079c9081dd165c77c7a1547c274f4c7c1d7bbf05

system/audio_hal_interface/hidl/le_audio_software_hidl.cc
@@ -134,6 +134,32 @@ BluetoothAudioCtrlAck LeAudioTransport::StartRequest() {
   return BluetoothAudioCtrlAck::FAILURE;
 }
 
+BluetoothAudioCtrlAck LeAudioTransport::StartRequestV2() {
+  SetStartRequestState(StartRequestState::PENDING_BEFORE_RESUME);
+  if (stream_cb_.on_resume_(true)) {
+    std::lock_guard<std::mutex> guard(start_request_state_mutex_);
+    if (start_request_state_ == StartRequestState::CONFIRMED) {
+      LOG_INFO("Start completed.");
+      SetStartRequestState(StartRequestState::IDLE);
+      return BluetoothAudioCtrlAck::SUCCESS_FINISHED;
+    }
+
+    if (start_request_state_ == StartRequestState::CANCELED) {
+      LOG_INFO("Start request failed.");
+      SetStartRequestState(StartRequestState::IDLE);
+      return BluetoothAudioCtrlAck::FAILURE;
+    }
+
+    LOG_INFO("Start pending.");
+    SetStartRequestState(StartRequestState::PENDING_AFTER_RESUME);
+    return BluetoothAudioCtrlAck::PENDING;
+  }
+
+  LOG_ERROR("Start request failed.");
+  SetStartRequestState(StartRequestState::IDLE);
+  return BluetoothAudioCtrlAck::FAILURE;
+}
+
 BluetoothAudioCtrlAck LeAudioTransport::SuspendRequest() {
   LOG(INFO) << __func__;
   if (stream_cb_.on_suspend_()) {
@@ -227,6 +253,22 @@ void LeAudioTransport::LeAudioSetSelectedHalPcmConfig(uint32_t sample_rate_hz,
   pcm_config_.dataIntervalUs = data_interval;
 }
 
+bool LeAudioTransport::IsRequestCompletedAfterUpdate(
+    const std::function<std::pair<StartRequestState, bool>(StartRequestState)>&
+        lambda) {
+  std::lock_guard<std::mutex> guard(start_request_state_mutex_);
+  auto result = lambda(start_request_state_);
+  auto new_state = std::get<0>(result);
+  if (new_state != start_request_state_) {
+    start_request_state_ = new_state;
+  }
+
+  auto ret = std::get<1> result;
+  LOG_VERBOSE("new state: %d, return: %s",
+              static_cast<int>(start_request_state_mutex_.load()), ret);
+  return ret;
+}
+
 StartRequestState LeAudioTransport::GetStartRequestState(void) {
   return start_request_state_;
 }
@@ -255,6 +297,9 @@ LeAudioSinkTransport::LeAudioSinkTransport(SessionType_2_1 session_type,
 LeAudioSinkTransport::~LeAudioSinkTransport() { delete transport_; }
 
 BluetoothAudioCtrlAck LeAudioSinkTransport::StartRequest() {
+  if (IS_FLAG_ENABLED(leaudio_start_stream_race_fix)) {
+    return transport_->StartRequestV2();
+  }
   return transport_->StartRequest();
 }
 
@@ -299,6 +344,12 @@ void LeAudioSinkTransport::LeAudioSetSelectedHalPcmConfig(
                                              channels_count, data_interval);
 }
 
+bool LeAudioSinkTransport::IsRequestCompletedAfterUpdate(
+    const std::function<std::pair<StartRequestState, bool>(StartRequestState)>&
+        lambda) {
+  return transport_->IsRequestCompletedAfterUpdate(lambda);
+}
+
 StartRequestState LeAudioSinkTransport::GetStartRequestState(void) {
   return transport_->GetStartRequestState();
 }
@@ -327,6 +378,9 @@ LeAudioSourceTransport::LeAudioSourceTransport(SessionType_2_1 session_type,
 LeAudioSourceTransport::~LeAudioSourceTransport() { delete transport_; }
 
 BluetoothAudioCtrlAck LeAudioSourceTransport::StartRequest() {
+  if (IS_FLAG_ENABLED(leaudio_start_stream_race_fix)) {
+    return transport_->StartRequestV2();
+  }
   return transport_->StartRequest();
 }
 
@@ -371,6 +425,11 @@ void LeAudioSourceTransport::LeAudioSetSelectedHalPcmConfig(
                                              channels_count, data_interval);
 }
 
+bool LeAudioSourceTransport::IsRequestCompletedAfterUpdate(
+    const std::function<std::pair<StartRequestState, bool>(StartRequestState)>&
+        lambda) {
+  return transport_->IsRequestCompletedAfterUpdate(lambda);
+}
 StartRequestState LeAudioSourceTransport::GetStartRequestState(void) {
   return transport_->GetStartRequestState();
 }

system/bta/le_audio/audio_hal_client/audio_hal_client_test.cc
@@ -177,6 +177,8 @@ void LeAudioClientInterface::Sink::StartSession() {}
 void LeAudioClientInterface::Sink::StopSession() {}
 void LeAudioClientInterface::Sink::ConfirmStreamingRequest(){};
 void LeAudioClientInterface::Sink::CancelStreamingRequest(){};
+void LeAudioClientInterface::Sink::ConfirmStreamingRequestV2() {};
+void LeAudioClientInterface::Sink::CancelStreamingRequestV2() {};
 void LeAudioClientInterface::Sink::UpdateAudioConfigToHal(
     const ::le_audio::offload_config& config){};
 void LeAudioClientInterface::Sink::UpdateBroadcastAudioConfigToHal(
@@ -192,6 +194,8 @@ void LeAudioClientInterface::Source::StartSession() {}
 void LeAudioClientInterface::Source::StopSession() {}
 void LeAudioClientInterface::Source::ConfirmStreamingRequest(){};
 void LeAudioClientInterface::Source::CancelStreamingRequest(){};
+void LeAudioClientInterface::Source::ConfirmStreamingRequestV2() {};
+void LeAudioClientInterface::Source::CancelStreamingRequestV2() {};
 void LeAudioClientInterface::Source::UpdateAudioConfigToHal(
     const ::le_audio::offload_config& config){};
 void LeAudioClientInterface::Source::SuspendedForReconfiguration() {}

LineageOS/android_packages_modules_Bluetooth

system/audio_hal_interface/hidl/le_audio_software_hidl.cc
@@ -134,6 +134,32 @@ BluetoothAudioCtrlAck LeAudioTransport::StartRequest() {
   return BluetoothAudioCtrlAck::FAILURE;
 }
 
+BluetoothAudioCtrlAck LeAudioTransport::StartRequestV2() {
+  SetStartRequestState(StartRequestState::PENDING_BEFORE_RESUME);
+  if (stream_cb_.on_resume_(true)) {
+    std::lock_guard<std::mutex> guard(start_request_state_mutex_);
+    if (start_request_state_ == StartRequestState::CONFIRMED) {
+      LOG_INFO("Start completed.");
+      SetStartRequestState(StartRequestState::IDLE);
+      return BluetoothAudioCtrlAck::SUCCESS_FINISHED;
+    }
+
+    if (start_request_state_ == StartRequestState::CANCELED) {
+      LOG_INFO("Start request failed.");
+      SetStartRequestState(StartRequestState::IDLE);
+      return BluetoothAudioCtrlAck::FAILURE;
+    }
+
+    LOG_INFO("Start pending.");
+    SetStartRequestState(StartRequestState::PENDING_AFTER_RESUME);
+    return BluetoothAudioCtrlAck::PENDING;
+  }
+
+  LOG_ERROR("Start request failed.");
+  SetStartRequestState(StartRequestState::IDLE);
+  return BluetoothAudioCtrlAck::FAILURE;
+}
+
 BluetoothAudioCtrlAck LeAudioTransport::SuspendRequest() {
   LOG(INFO) << __func__;
   if (stream_cb_.on_suspend_()) {
@@ -227,6 +253,22 @@ void LeAudioTransport::LeAudioSetSelectedHalPcmConfig(uint32_t sample_rate_hz,
   pcm_config_.dataIntervalUs = data_interval;
 }
 
+bool LeAudioTransport::IsRequestCompletedAfterUpdate(
+    const std::function<std::pair<StartRequestState, bool>(StartRequestState)>&
+        lambda) {
+  std::lock_guard<std::mutex> guard(start_request_state_mutex_);
+  auto result = lambda(start_request_state_);
+  auto new_state = std::get<0>(result);
+  if (new_state != start_request_state_) {
+    start_request_state_ = new_state;
+  }
+
+  auto ret = std::get<1>(result);
+  LOG_VERBOSE("new state: %d, return: %s",
+              static_cast<int>(start_request_state_.load()), ret);
+  return ret;
+}
+
 StartRequestState LeAudioTransport::GetStartRequestState(void) {
   return start_request_state_;
 }
@@ -255,6 +297,9 @@ LeAudioSinkTransport::LeAudioSinkTransport(SessionType_2_1 session_type,
 LeAudioSinkTransport::~LeAudioSinkTransport() { delete transport_; }
 
 BluetoothAudioCtrlAck LeAudioSinkTransport::StartRequest() {
+  if (IS_FLAG_ENABLED(leaudio_start_stream_race_fix)) {
+    return transport_->StartRequestV2();
+  }
   return transport_->StartRequest();
 }
 
@@ -299,6 +344,12 @@ void LeAudioSinkTransport::LeAudioSetSelectedHalPcmConfig(
                                              channels_count, data_interval);
 }
 
+bool LeAudioSinkTransport::IsRequestCompletedAfterUpdate(
+    const std::function<std::pair<StartRequestState, bool>(StartRequestState)>&
+        lambda) {
+  return transport_->IsRequestCompletedAfterUpdate(lambda);
+}
+
 StartRequestState LeAudioSinkTransport::GetStartRequestState(void) {
   return transport_->GetStartRequestState();
 }
@@ -327,6 +378,9 @@ LeAudioSourceTransport::LeAudioSourceTransport(SessionType_2_1 session_type,
 LeAudioSourceTransport::~LeAudioSourceTransport() { delete transport_; }
 
 BluetoothAudioCtrlAck LeAudioSourceTransport::StartRequest() {
+  if (IS_FLAG_ENABLED(leaudio_start_stream_race_fix)) {
+    return transport_->StartRequestV2();
+  }
   return transport_->StartRequest();
 }
 
@@ -371,6 +425,11 @@ void LeAudioSourceTransport::LeAudioSetSelectedHalPcmConfig(
                                              channels_count, data_interval);
 }
 
+bool LeAudioSourceTransport::IsRequestCompletedAfterUpdate(
+    const std::function<std::pair<StartRequestState, bool>(StartRequestState)>&
+        lambda) {
+  return transport_->IsRequestCompletedAfterUpdate(lambda);
+}
 StartRequestState LeAudioSourceTransport::GetStartRequestState(void) {
   return transport_->GetStartRequestState();
 }

system/bta/le_audio/audio_hal_client/audio_hal_client_test.cc
@@ -177,6 +177,8 @@ void LeAudioClientInterface::Sink::StartSession() {}
 void LeAudioClientInterface::Sink::StopSession() {}
 void LeAudioClientInterface::Sink::ConfirmStreamingRequest(){};
 void LeAudioClientInterface::Sink::CancelStreamingRequest(){};
+void LeAudioClientInterface::Sink::ConfirmStreamingRequestV2(){};
+void LeAudioClientInterface::Sink::CancelStreamingRequestV2(){};
 void LeAudioClientInterface::Sink::UpdateAudioConfigToHal(
     const ::le_audio::offload_config& config){};
 void LeAudioClientInterface::Sink::UpdateBroadcastAudioConfigToHal(
@@ -192,6 +194,8 @@ void LeAudioClientInterface::Source::StartSession() {}
 void LeAudioClientInterface::Source::StopSession() {}
 void LeAudioClientInterface::Source::ConfirmStreamingRequest(){};
 void LeAudioClientInterface::Source::CancelStreamingRequest(){};
+void LeAudioClientInterface::Source::ConfirmStreamingRequestV2(){};
+void LeAudioClientInterface::Source::CancelStreamingRequestV2(){};
 void LeAudioClientInterface::Source::UpdateAudioConfigToHal(
     const ::le_audio::offload_config& config){};
 void LeAudioClientInterface::Source::SuspendedForReconfiguration() {}