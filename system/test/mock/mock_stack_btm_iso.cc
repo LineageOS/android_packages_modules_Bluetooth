miyu-xu/packages-modules-Bluetooth

Use a single IsoManager mock
Bug: 283254594
Test: mma -j32
Flag: EXEMPT, test-only changes
Change-Id: Iebe4ae81d896ff0a2f19ab6d29f4c62309737bfd

system/test/mock/mock_stack_btm_iso.cc
@@ -1,5 +1,6 @@
 /*
- * Copyright 2023 The Android Open Source Project
+ * Copyright 2020 HIMSA II K/S - www.himsa.com.
+ * Represented by EHIMA - www.ehima.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,57 +15,158 @@
  * limitations under the License.
  */
 
-#include "stack/include/btm_iso_api.h"
+#include "mock_stack_btm_iso.h"
 
-using bluetooth::hci::iso_manager::BigCallbacks;
-using bluetooth::hci::iso_manager::CigCallbacks;
+namespace {
+MockIsoManager* mock_pimpl_;
+}
+
+MockIsoManager* MockIsoManager::GetInstance() {
+  bluetooth::hci::IsoManager::GetInstance();
+  return mock_pimpl_;
+}
 
 namespace bluetooth {
 namespace hci {
 
-struct IsoManager::impl {};
+struct IsoManager::impl : public MockIsoManager {
+ public:
+  impl() = default;
+  ~impl() = default;
+};
 
 IsoManager::IsoManager() {}
-IsoManager::~IsoManager() {}
-void IsoManager::RegisterCigCallbacks(CigCallbacks* /* callbacks */) const {}
-void IsoManager::RegisterBigCallbacks(BigCallbacks* /* callbacks */) const {}
-void IsoManager::RegisterOnIsoTrafficActiveCallback(void (*)(bool)) const {}
-void IsoManager::CreateCig(
-    uint8_t /* cig_id */,
-    struct iso_manager::cig_create_params /* cig_params */) {}
-void IsoManager::ReconfigureCig(
-    uint8_t /* cig_id */,
-    struct iso_manager::cig_create_params /* cig_params */) {}
-void IsoManager::RemoveCig(uint8_t /* cig_id */, bool /* force */) {}
-void IsoManager::EstablishCis(
-    struct iso_manager::cis_establish_params /* conn_params */) {}
-void IsoManager::DisconnectCis(uint16_t /* cis_handle */,
-                               uint8_t /* reason */) {}
-void IsoManager::SetupIsoDataPath(
-    uint16_t /* iso_handle */,
-    struct iso_manager::iso_data_path_params /* path_params */) {}
-void IsoManager::RemoveIsoDataPath(uint16_t /* iso_handle */,
-                                   uint8_t /* data_path_dir */) {}
-void IsoManager::ReadIsoLinkQuality(uint16_t /* iso_handle */) {}
-void IsoManager::SendIsoData(uint16_t /* iso_handle */,
-                             const uint8_t* /* data */,
-                             uint16_t /* data_len */) {}
-void IsoManager::CreateBig(
-    uint8_t /* big_id */,
-    struct iso_manager::big_create_params /* big_params */) {}
-void IsoManager::TerminateBig(uint8_t /* big_id */, uint8_t /* reason */) {}
-void IsoManager::HandleIsoData(void* /* p_msg */) {}
-void IsoManager::HandleDisconnect(uint16_t /* handle */, uint8_t /* reason */) {
+
+void IsoManager::RegisterCigCallbacks(
+    iso_manager::CigCallbacks* callbacks) const {
+  if (!pimpl_) return;
+  pimpl_->RegisterCigCallbacks(callbacks);
 }
-void IsoManager::HandleNumComplDataPkts(uint8_t* /* p */,
-                                        uint8_t /* evt_len */) {}
-void IsoManager::HandleGdNumComplDataPkts(uint16_t /* handle */,
-                                          uint16_t /* credits */) {}
-void IsoManager::HandleHciEvent(uint8_t /* sub_code */, uint8_t* /* params */,
-                                uint16_t /* length */) {}
-void IsoManager::Start() {}
-void IsoManager::Stop() {}
-void IsoManager::Dump(int /* fd */) {}
+
+void IsoManager::RegisterBigCallbacks(
+    iso_manager::BigCallbacks* callbacks) const {
+  if (!pimpl_) return;
+  pimpl_->RegisterBigCallbacks(callbacks);
+}
+
+void IsoManager::RegisterOnIsoTrafficActiveCallback(void callback(bool)) const {
+  if (!pimpl_) return;
+  pimpl_->RegisterOnIsoTrafficActiveCallbacks(callback);
+}
+
+void IsoManager::CreateCig(uint8_t cig_id,
+                           struct iso_manager::cig_create_params cig_params) {
+  if (!pimpl_) return;
+  pimpl_->CreateCig(cig_id, std::move(cig_params));
+}
+
+void IsoManager::ReconfigureCig(
+    uint8_t cig_id, struct iso_manager::cig_create_params cig_params) {
+  if (!pimpl_) return;
+  pimpl_->ReconfigureCig(cig_id, std::move(cig_params));
+}
+
+void IsoManager::RemoveCig(uint8_t cig_id, bool force) {
+  pimpl_->RemoveCig(cig_id, force);
+}
+
+void IsoManager::EstablishCis(
+    struct iso_manager::cis_establish_params conn_params) {
+  if (!pimpl_) return;
+  pimpl_->EstablishCis(std::move(conn_params));
+}
+
+void IsoManager::DisconnectCis(uint16_t cis_handle, uint8_t reason) {
+  if (!pimpl_) return;
+  pimpl_->DisconnectCis(cis_handle, reason);
+}
+
+void IsoManager::SetupIsoDataPath(
+    uint16_t iso_handle, struct iso_manager::iso_data_path_params path_params) {
+  if (!pimpl_) return;
+  pimpl_->SetupIsoDataPath(iso_handle, std::move(path_params));
+}
+
+void IsoManager::RemoveIsoDataPath(uint16_t iso_handle, uint8_t data_path_dir) {
+  if (!pimpl_) return;
+  pimpl_->RemoveIsoDataPath(iso_handle, data_path_dir);
+}
+
+void IsoManager::ReadIsoLinkQuality(uint16_t iso_handle) {
+  if (!pimpl_) return;
+  pimpl_->ReadIsoLinkQuality(iso_handle);
+}
+
+void IsoManager::SendIsoData(uint16_t iso_handle, const uint8_t* data,
+                             uint16_t data_len) {
+  if (!pimpl_) return;
+  pimpl_->SendIsoData(iso_handle, data, data_len);
+}
+
+void IsoManager::CreateBig(uint8_t big_id,
+                           struct iso_manager::big_create_params big_params) {
+  if (!pimpl_) return;
+  pimpl_->CreateBig(big_id, std::move(big_params));
+}
+
+void IsoManager::TerminateBig(uint8_t big_id, uint8_t reason) {
+  if (!pimpl_) return;
+  pimpl_->TerminateBig(big_id, reason);
+}
+
+void IsoManager::HandleIsoData(void* p_msg) {
+  if (!pimpl_) return;
+  pimpl_->HandleIsoData(static_cast<BT_HDR*>(p_msg));
+}
+
+void IsoManager::HandleDisconnect(uint16_t handle, uint8_t reason) {
+  if (!pimpl_) return;
+  pimpl_->HandleDisconnect(handle, reason);
+}
+
+void IsoManager::HandleNumComplDataPkts(uint8_t* p, uint8_t evt_len) {
+  if (!pimpl_) return;
+  pimpl_->HandleNumComplDataPkts(p, evt_len);
+}
+
+void IsoManager::HandleGdNumComplDataPkts(uint16_t handle, uint16_t credits) {}
+
+void IsoManager::HandleHciEvent(uint8_t sub_code, uint8_t* params,
+                                uint16_t length) {
+  if (!pimpl_) return;
+  pimpl_->HandleHciEvent(sub_code, params, length);
+}
+
+void IsoManager::Start() {
+  // It is needed here as IsoManager which is a singleton creates it, but in
+  // this mock we want to destroy and recreate the mock on each test case.
+  if (!pimpl_) {
+    pimpl_ = std::make_unique<testing::NiceMock<impl>>();
+  }
+
+  mock_pimpl_ = pimpl_.get();
+  pimpl_->Start();
+}
+
+void IsoManager::Stop() {
+  // It is needed here as IsoManager which is a singleton creates it, but in
+  // this mock we want to destroy and recreate the mock on each test case.
+  if (pimpl_) {
+    pimpl_->Stop();
+    pimpl_.reset();
+  }
+
+  mock_pimpl_ = nullptr;
+}
+
+void IsoManager::Dump(int fd) {}
+
+IsoManager::~IsoManager() = default;
+
+IsoManager* IsoManager::GetInstance() {
+  static IsoManager* instance = new IsoManager();
+  return instance;
+}
 
 }  // namespace hci
 }  // namespace bluetooth

LineageOS/android_packages_modules_Bluetooth

system/test/mock/mock_stack_btm_iso.cc
@@ -1,5 +1,6 @@
 /*
- * Copyright 2023 The Android Open Source Project
+ * Copyright 2020 HIMSA II K/S - www.himsa.com.
+ * Represented by EHIMA - www.ehima.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,55 +15,156 @@
  * limitations under the License.
  */
 
-#include "stack/include/btm_iso_api.h"
+#include "mock_stack_btm_iso.h"
 
-using bluetooth::hci::iso_manager::BigCallbacks;
-using bluetooth::hci::iso_manager::CigCallbacks;
+namespace {
+MockIsoManager* mock_pimpl_;
+}
+
+MockIsoManager* MockIsoManager::GetInstance() {
+  bluetooth::hci::IsoManager::GetInstance();
+  return mock_pimpl_;
+}
 
 namespace bluetooth {
 namespace hci {
 
-struct IsoManager::impl {};
+struct IsoManager::impl : public MockIsoManager {
+ public:
+  impl() = default;
+  ~impl() = default;
+};
 
 IsoManager::IsoManager() {}
-IsoManager::~IsoManager() {}
-void IsoManager::RegisterCigCallbacks(CigCallbacks* /* callbacks */) const {}
-void IsoManager::RegisterBigCallbacks(BigCallbacks* /* callbacks */) const {}
-void IsoManager::RegisterOnIsoTrafficActiveCallback(void (*)(bool)) const {}
-void IsoManager::CreateCig(
-    uint8_t /* cig_id */,
-    struct iso_manager::cig_create_params /* cig_params */) {}
-void IsoManager::ReconfigureCig(
-    uint8_t /* cig_id */,
-    struct iso_manager::cig_create_params /* cig_params */) {}
-void IsoManager::RemoveCig(uint8_t /* cig_id */, bool /* force */) {}
-void IsoManager::EstablishCis(
-    struct iso_manager::cis_establish_params /* conn_params */) {}
-void IsoManager::DisconnectCis(uint16_t /* cis_handle */,
-                               uint8_t /* reason */) {}
-void IsoManager::SetupIsoDataPath(
-    uint16_t /* iso_handle */,
-    struct iso_manager::iso_data_path_params /* path_params */) {}
-void IsoManager::RemoveIsoDataPath(uint16_t /* iso_handle */,
-                                   uint8_t /* data_path_dir */) {}
-void IsoManager::ReadIsoLinkQuality(uint16_t /* iso_handle */) {}
-void IsoManager::SendIsoData(uint16_t /* iso_handle */,
-                             const uint8_t* /* data */,
-                             uint16_t /* data_len */) {}
-void IsoManager::CreateBig(
-    uint8_t /* big_id */,
-    struct iso_manager::big_create_params /* big_params */) {}
-void IsoManager::TerminateBig(uint8_t /* big_id */, uint8_t /* reason */) {}
-void IsoManager::HandleIsoData(void* /* p_msg */) {}
-void IsoManager::HandleDisconnect(uint16_t /* handle */, uint8_t /* reason */) {
+
+void IsoManager::RegisterCigCallbacks(
+    iso_manager::CigCallbacks* callbacks) const {
+  if (!pimpl_) return;
+  pimpl_->RegisterCigCallbacks(callbacks);
 }
-void IsoManager::HandleNumComplDataPkts(uint16_t /* handle */,
-                                        uint16_t /* credits */) {}
-void IsoManager::HandleHciEvent(uint8_t /* sub_code */, uint8_t* /* params */,
-                                uint16_t /* length */) {}
-void IsoManager::Start() {}
-void IsoManager::Stop() {}
+
+void IsoManager::RegisterBigCallbacks(
+    iso_manager::BigCallbacks* callbacks) const {
+  if (!pimpl_) return;
+  pimpl_->RegisterBigCallbacks(callbacks);
+}
+
+void IsoManager::RegisterOnIsoTrafficActiveCallback(void callback(bool)) const {
+  if (!pimpl_) return;
+  pimpl_->RegisterOnIsoTrafficActiveCallbacks(callback);
+}
+
+void IsoManager::CreateCig(uint8_t cig_id,
+                           struct iso_manager::cig_create_params cig_params) {
+  if (!pimpl_) return;
+  pimpl_->CreateCig(cig_id, std::move(cig_params));
+}
+
+void IsoManager::ReconfigureCig(
+    uint8_t cig_id, struct iso_manager::cig_create_params cig_params) {
+  if (!pimpl_) return;
+  pimpl_->ReconfigureCig(cig_id, std::move(cig_params));
+}
+
+void IsoManager::RemoveCig(uint8_t cig_id, bool force) {
+  pimpl_->RemoveCig(cig_id, force);
+}
+
+void IsoManager::EstablishCis(
+    struct iso_manager::cis_establish_params conn_params) {
+  if (!pimpl_) return;
+  pimpl_->EstablishCis(std::move(conn_params));
+}
+
+void IsoManager::DisconnectCis(uint16_t cis_handle, uint8_t reason) {
+  if (!pimpl_) return;
+  pimpl_->DisconnectCis(cis_handle, reason);
+}
+
+void IsoManager::SetupIsoDataPath(
+    uint16_t iso_handle, struct iso_manager::iso_data_path_params path_params) {
+  if (!pimpl_) return;
+  pimpl_->SetupIsoDataPath(iso_handle, std::move(path_params));
+}
+
+void IsoManager::RemoveIsoDataPath(uint16_t iso_handle, uint8_t data_path_dir) {
+  if (!pimpl_) return;
+  pimpl_->RemoveIsoDataPath(iso_handle, data_path_dir);
+}
+
+void IsoManager::ReadIsoLinkQuality(uint16_t iso_handle) {
+  if (!pimpl_) return;
+  pimpl_->ReadIsoLinkQuality(iso_handle);
+}
+
+void IsoManager::SendIsoData(uint16_t iso_handle, const uint8_t* data,
+                             uint16_t data_len) {
+  if (!pimpl_) return;
+  pimpl_->SendIsoData(iso_handle, data, data_len);
+}
+
+void IsoManager::CreateBig(uint8_t big_id,
+                           struct iso_manager::big_create_params big_params) {
+  if (!pimpl_) return;
+  pimpl_->CreateBig(big_id, std::move(big_params));
+}
+
+void IsoManager::TerminateBig(uint8_t big_id, uint8_t reason) {
+  if (!pimpl_) return;
+  pimpl_->TerminateBig(big_id, reason);
+}
+
+void IsoManager::HandleIsoData(void* p_msg) {
+  if (!pimpl_) return;
+  pimpl_->HandleIsoData(static_cast<BT_HDR*>(p_msg));
+}
+
+void IsoManager::HandleDisconnect(uint16_t handle, uint8_t reason) {
+  if (!pimpl_) return;
+  pimpl_->HandleDisconnect(handle, reason);
+}
+
+void IsoManager::HandleNumComplDataPkts(uint16_t handle, uint16_t credits) {
+  if (!pimpl_) return;
+  pimpl_->HandleNumComplDataPkts(handle, credits);
+}
+
+void IsoManager::HandleHciEvent(uint8_t sub_code, uint8_t* params,
+                                uint16_t length) {
+  if (!pimpl_) return;
+  pimpl_->HandleHciEvent(sub_code, params, length);
+}
+
+void IsoManager::Start() {
+  // It is needed here as IsoManager which is a singleton creates it, but in
+  // this mock we want to destroy and recreate the mock on each test case.
+  if (!pimpl_) {
+    pimpl_ = std::make_unique<testing::NiceMock<impl>>();
+  }
+
+  mock_pimpl_ = pimpl_.get();
+  pimpl_->Start();
+}
+
+void IsoManager::Stop() {
+  // It is needed here as IsoManager which is a singleton creates it, but in
+  // this mock we want to destroy and recreate the mock on each test case.
+  if (pimpl_) {
+    pimpl_->Stop();
+    pimpl_.reset();
+  }
+
+  mock_pimpl_ = nullptr;
+}
+
 void IsoManager::Dump(int /* fd */) {}
+
+IsoManager::~IsoManager() = default;
+
+IsoManager* IsoManager::GetInstance() {
+  static IsoManager* instance = new IsoManager();
+  return instance;
+}
 
 }  // namespace hci
 }  // namespace bluetooth