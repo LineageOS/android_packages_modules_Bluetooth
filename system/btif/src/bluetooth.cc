miyu-xu/packages-modules-Bluetooth

bta_gattc: Add dumpsys for bta_gattc_cb
Allows to debug legacy code.

BTA_GATTC_CB state BTA_GATTC_STATE_ENABLED
 ->conn_track (GATT_MAX_PHY_CHANNEL=7)
  -- used: 0
 ->bg_track (BTA_GATTC_KNOWN_SR_MAX=255)
  address: xx:xx:xx:xx:7c:9f  cif_mask: 0x00000410
  address: xx:xx:xx:xx:13:0d  cif_mask: 0x00000400
  address: xx:xx:xx:xx:13:1e  cif_mask: 0x00000400
  address: xx:xx:xx:xx:1d:c7  cif_mask: 0x00000400
  address: xx:xx:xx:xx:0d:81  cif_mask: 0x00000400
  address: xx:xx:xx:xx:37:e7  cif_mask: 0x00000400
  address: xx:xx:xx:xx:62:cd  cif_mask: 0x00000400
  address: xx:xx:xx:xx:7a:0e  cif_mask: 0x00000400
  address: xx:xx:xx:xx:bf:c8  cif_mask: 0x00000400
  -- used: 9
 ->cl_rcb (BTA_GATTC_CL_MAX=32)
  client_if: 3  app uuids: 653e2c09-1998-7135-6170-09e6e5c33762  clcb_num: 0
  client_if: 4  app uuids: 52fa4f05-c872-2a5f-8fd3-325a1ed91a27  clcb_num: 2
  client_if: 5  app uuids: ca641ade-f5ee-9f3f-e9c3-f8b85495f168  clcb_num: 0
  client_if: 6  app uuids: 0d45ce8d-27af-9647-5e5a-b9a0cbc158b7  clcb_num: 0
  client_if: 7  app uuids: ee1564af-219f-9a9c-6f91-94e975e660fc  clcb_num: 0
  client_if: 10  app uuids: a11452b9-abe0-dba7-d875-4bf84b4e9e17  clcb_num: 2
  client_if: 11  app uuids: 55ffb127-03cf-c200-8e7a-818cf246971c  clcb_num: 2
  client_if: 13  app uuids: 303edfa5-682c-8348-c0a4-e07d91d39779  clcb_num: 1
  client_if: 14  app uuids: de3c7659-a0e0-7252-a2ce-608d06146b60  clcb_num: 1
  client_if: 15  app uuids: eac72e2b-8055-fb15-f362-c4ed867bbeba  clcb_num: 1
  client_if: 16  app uuids: 972046ea-7c04-ff1e-f1fa-a12fc41ff356  clcb_num: 1
  client_if: 17  app uuids: ab1d4efe-172a-7811-adc4-c3d0aa753514  clcb_num: 1
  client_if: 18  app uuids: c4d63275-a9af-c2e8-90f6-51cb12bb3e99  clcb_num: 1
  client_if: 19  app uuids: 7525f060-00bf-1c94-880c-426d3543ac13  clcb_num: 1
  client_if: 20  app uuids: fece1e72-b3a4-a9c6-08df-f103f87d412c  clcb_num: 1
  -- used: 15
 ->clcb (BTA_GATTC_CLCB_MAX=22)
  conn_id: 0x0114  address: xx:xx:xx:xx:13:1e  transport: BT_TRANSPORT_LE  stat

system/btif/src/bluetooth.cc
@@ -178,6 +178,7 @@ bt_status_t btif_hh_connect(const RawAddress* bd_addr);
 bt_status_t btif_hd_execute_service(bool b_enable);
 
 extern void gatt_tcb_dump(int fd);
+extern void bta_dump(int fd);
 
 /*******************************************************************************
  *  Callbacks from bluetooth::core (see go/invisalign-bt)
@@ -803,6 +804,7 @@ static void dump(int fd, const char** arguments) {
   bluetooth::avrcp::AvrcpService::DebugDump(fd);
   btif_debug_config_dump(fd);
   gatt_tcb_dump(fd);
+  bta_dump(fd);
   device_debug_iot_config_dump(fd);
   BTA_HfClientDumpStatistics(fd);
   wakelock_debug_dump(fd);

LineageOS/android_packages_modules_Bluetooth

system/btif/src/bluetooth.cc
@@ -182,6 +182,7 @@ bt_status_t btif_hh_connect(const tAclLinkSpec* link_spec);
 bt_status_t btif_hd_execute_service(bool b_enable);
 
 extern void gatt_tcb_dump(int fd);
+extern void bta_gatt_client_dump(int fd);
 
 /*******************************************************************************
  *  Callbacks from bluetooth::core (see go/invisalign-bt)
@@ -824,6 +825,7 @@ static void dump(int fd, const char** arguments) {
   bluetooth::avrcp::AvrcpService::DebugDump(fd);
   btif_debug_config_dump(fd);
   gatt_tcb_dump(fd);
+  bta_gatt_client_dump(fd);
   device_debug_iot_config_dump(fd);
   BTA_HfClientDumpStatistics(fd);
   wakelock_debug_dump(fd);