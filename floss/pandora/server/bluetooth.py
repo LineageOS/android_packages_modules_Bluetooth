miyu-xu/packages-modules-Bluetooth

Floss: Implement Pandora L2CAP profile
Bug: 300954032, Bug: 300942862, Bug: 300942863, Bug: 301000121,
Bug: 301000122
Test: mma packages/modules/Bluetooth && pts-bot L2CAP
Tag: #floss
Flag: EXEMPT floss only changes
Change-Id: If3ac5043ec9894c3d6ae9773e07d2e52513665ba

floss/pandora/server/bluetooth.py
@@ -22,6 +22,7 @@ from floss.pandora.floss import advertising_client
 from floss.pandora.floss import manager_client
 from floss.pandora.floss import qa_client
 from floss.pandora.floss import scanner_client
+from floss.pandora.floss import socket_manager
 from floss.pandora.floss import gatt_client
 from floss.pandora.floss import gatt_server
 from floss.pandora.floss import floss_enums
@@ -67,6 +68,7 @@ class Bluetooth(object):
         self.qa_client = qa_client.FlossQAClient(self.bus, self.DEFAULT_ADAPTER)
         self.gatt_client = gatt_client.FlossGattClient(self.bus, self.DEFAULT_ADAPTER)
         self.gatt_server = gatt_server.FlossGattServer(self.bus, self.DEFAULT_ADAPTER)
+        self.socket_manager = socket_manager.FlossSocketManagerClient(self.bus, self.DEFAULT_ADAPTER)
 
     def __del__(self):
         if not self.is_clean:
@@ -137,6 +139,9 @@ class Bluetooth(object):
         if not self.gatt_server.register_server(self.FAKE_GATT_APP_ID, False):
             logging.error('gatt_server: Failed to register callbacks')
             return False
+        if not self.socket_manager.register_callbacks():
+            logging.error('scanner_client: Failed to register callbacks')
+            return False
         return True
 
     def is_bluetoothd_proxy_valid(self):
@@ -150,6 +155,7 @@ class Bluetooth(object):
             self.qa_client.has_proxy(),
             self.gatt_client.has_proxy(),
             self.gatt_server.has_proxy(),
+            self.socket_manager.has_proxy()
         ])
 
         if not proxy_ready:
@@ -185,6 +191,7 @@ class Bluetooth(object):
             self.qa_client = qa_client.FlossQAClient(self.bus, default_adapter)
             self.gatt_client = gatt_client.FlossGattClient(self.bus, default_adapter)
             self.gatt_server = gatt_server.FlossGattServer(self.bus, default_adapter)
+            self.socket_manager = socket_manager.FlossSocketManagerClient(self.bus, default_adapter)
 
             try:
                 utils.poll_for_condition(
@@ -360,3 +367,22 @@ class Bluetooth(object):
             logging.error('Failed to get connection state for address: %s', address)
             return False
         return connection_state > floss_enums.BtConnectionState.CONNECTED_ONLY
+
+    def listen_using_l2cap_channel(self):
+        return self.socket_manager.listen_using_l2cap_channel()
+
+    def listen_using_insecure_l2cap_channel(self):
+        return self.socket_manager.listen_using_insecure_l2cap_channel()
+
+    def create_l2cap_channel(self, address, psm):
+        name = 'Test device'
+        device = self.socket_manager.make_dbus_device(address, name)
+        return self.socket_manager.create_l2cap_channel(device, psm)
+
+    def create_insecure_l2cap_channel(self, address, psm):
+        name = 'Test device'
+        device = self.socket_manager.make_dbus_device(address, name)
+        return self.socket_manager.create_insecure_l2cap_channel(device, psm)
+
+    def accept(self, socket_id, timeout_ms=None):
+        return self.socket_manager.accept(socket_id, timeout_ms)

LineageOS/android_packages_modules_Bluetooth

Bug: 300954032
Bug: 300942862
Bug: 300942863
Bug: 301000121

floss/pandora/server/bluetooth.py
@@ -22,6 +22,7 @@ from floss.pandora.floss import advertising_client
 from floss.pandora.floss import manager_client
 from floss.pandora.floss import qa_client
 from floss.pandora.floss import scanner_client
+from floss.pandora.floss import socket_manager
 from floss.pandora.floss import gatt_client
 from floss.pandora.floss import gatt_server
 from floss.pandora.floss import floss_enums
@@ -67,6 +68,7 @@ class Bluetooth(object):
         self.qa_client = qa_client.FlossQAClient(self.bus, self.DEFAULT_ADAPTER)
         self.gatt_client = gatt_client.FlossGattClient(self.bus, self.DEFAULT_ADAPTER)
         self.gatt_server = gatt_server.FlossGattServer(self.bus, self.DEFAULT_ADAPTER)
+        self.socket_manager = socket_manager.FlossSocketManagerClient(self.bus, self.DEFAULT_ADAPTER)
 
     def __del__(self):
         if not self.is_clean:
@@ -137,6 +139,9 @@ class Bluetooth(object):
         if not self.gatt_server.register_server(self.FAKE_GATT_APP_ID, False):
             logging.error('gatt_server: Failed to register callbacks')
             return False
+        if not self.socket_manager.register_callbacks():
+            logging.error('scanner_client: Failed to register callbacks')
+            return False
         return True
 
     def is_bluetoothd_proxy_valid(self):
@@ -150,6 +155,7 @@ class Bluetooth(object):
             self.qa_client.has_proxy(),
             self.gatt_client.has_proxy(),
             self.gatt_server.has_proxy(),
+            self.socket_manager.has_proxy()
         ])
 
         if not proxy_ready:
@@ -185,6 +191,7 @@ class Bluetooth(object):
             self.qa_client = qa_client.FlossQAClient(self.bus, default_adapter)
             self.gatt_client = gatt_client.FlossGattClient(self.bus, default_adapter)
             self.gatt_server = gatt_server.FlossGattServer(self.bus, default_adapter)
+            self.socket_manager = socket_manager.FlossSocketManagerClient(self.bus, default_adapter)
 
             try:
                 utils.poll_for_condition(
@@ -360,3 +367,22 @@ class Bluetooth(object):
             logging.error('Failed to get connection state for address: %s', address)
             return False
         return connection_state > floss_enums.BtConnectionState.CONNECTED_ONLY
+
+    def listen_using_l2cap_channel(self):
+        return self.socket_manager.listen_using_l2cap_channel()
+
+    def listen_using_insecure_l2cap_channel(self):
+        return self.socket_manager.listen_using_insecure_l2cap_channel()
+
+    def create_l2cap_channel(self, address, psm):
+        name = self.adapter_client.get_remote_property(address, 'Name')
+        device = self.socket_manager.make_dbus_device(address, name)
+        return self.socket_manager.create_l2cap_channel(device, psm)
+
+    def create_insecure_l2cap_channel(self, address, psm):
+        name = self.adapter_client.get_remote_property(address, 'Name')
+        device = self.socket_manager.make_dbus_device(address, name)
+        return self.socket_manager.create_insecure_l2cap_channel(device, psm)
+
+    def accept_socket(self, socket_id, timeout_ms=None):
+        return self.socket_manager.accept(socket_id, timeout_ms)