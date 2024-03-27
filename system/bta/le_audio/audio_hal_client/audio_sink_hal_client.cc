miyu-xu/packages-modules-Bluetooth

system: Convert last remaining occurrences of GD logging
Test: m com.android.btservices
Bug: 305066880
Flag: EXEMPT, mechanical refactor
Change-Id: Iac3bd56aca234a2d33753a7d0467bfc736b38f54

system/bta/le_audio/audio_hal_client/audio_sink_hal_client.cc
@@ -93,7 +93,7 @@ bool SinkImpl::Acquire() {
     return false;
   }
 
-  LOG_INFO();
+  log::debug("");
   le_audio_source_hal_state = HAL_STOPPED;
   return true;
 }
@@ -104,7 +104,7 @@ void SinkImpl::Release() {
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   if (halSourceInterface_) {
     halSourceInterface_->Cleanup();
 
@@ -219,7 +219,7 @@ void SinkImpl::Stop() {
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
 
   halSourceInterface_->StopSession();
   le_audio_source_hal_state = HAL_STOPPED;
@@ -256,7 +256,7 @@ void SinkImpl::ConfirmStreamingRequest() {
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   if (IS_FLAG_ENABLED(leaudio_start_stream_race_fix)) {
     halSourceInterface_->ConfirmStreamingRequestV2();
   } else {
@@ -271,7 +271,7 @@ void SinkImpl::SuspendedForReconfiguration() {
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   halSourceInterface_->SuspendedForReconfiguration();
 }
 
@@ -282,7 +282,7 @@ void SinkImpl::ReconfigurationComplete() {
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   halSourceInterface_->ReconfigurationComplete();
 }
 
@@ -293,7 +293,7 @@ void SinkImpl::CancelStreamingRequest() {
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   if (IS_FLAG_ENABLED(leaudio_start_stream_race_fix)) {
     halSourceInterface_->CancelStreamingRequestV2();
   } else {
@@ -308,7 +308,7 @@ void SinkImpl::UpdateRemoteDelay(uint16_t remote_delay_ms) {
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   halSourceInterface_->SetRemoteDelay(remote_delay_ms);
 }
 
@@ -320,7 +320,7 @@ void SinkImpl::UpdateAudioConfigToHal(
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   halSourceInterface_->UpdateAudioConfigToHal(config);
 }
 }  // namespace
@@ -334,7 +334,7 @@ LeAudioSinkAudioHalClient::AcquireUnicast() {
     return nullptr;
   }
 
-  LOG_INFO();
+  log::debug("");
   return std::move(impl);
 }
 

system/bta/le_audio/audio_hal_client/audio_source_hal_client.cc
@@ -134,7 +134,7 @@ bool SourceImpl::Acquire() {
     return false;
   }
 
-  LOG_INFO();
+  log::debug("");
   le_audio_sink_hal_state_ = HAL_STOPPED;
   return this->InitAudioSinkThread();
 }
@@ -145,7 +145,7 @@ void SourceImpl::Release() {
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   worker_thread_->ShutDown();
 
   if (halSinkInterface_) {
@@ -365,7 +365,7 @@ void SourceImpl::Stop() {
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
 
   halSinkInterface_->StopSession();
   le_audio_sink_hal_state_ = HAL_STOPPED;
@@ -392,7 +392,7 @@ void SourceImpl::ConfirmStreamingRequest() {
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   if (IS_FLAG_ENABLED(leaudio_start_stream_race_fix)) {
     halSinkInterface_->ConfirmStreamingRequestV2();
   } else {
@@ -418,7 +418,7 @@ void SourceImpl::SuspendedForReconfiguration() {
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   halSinkInterface_->SuspendedForReconfiguration();
 }
 
@@ -429,7 +429,7 @@ void SourceImpl::ReconfigurationComplete() {
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   halSinkInterface_->ReconfigurationComplete();
 }
 
@@ -440,7 +440,7 @@ void SourceImpl::CancelStreamingRequest() {
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   if (IS_FLAG_ENABLED(leaudio_start_stream_race_fix)) {
     halSinkInterface_->CancelStreamingRequestV2();
   } else {
@@ -455,7 +455,7 @@ void SourceImpl::UpdateRemoteDelay(uint16_t remote_delay_ms) {
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   halSinkInterface_->SetRemoteDelay(remote_delay_ms);
 }
 
@@ -467,7 +467,7 @@ void SourceImpl::UpdateAudioConfigToHal(
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   halSinkInterface_->UpdateAudioConfigToHal(config);
 }
 
@@ -478,7 +478,7 @@ void SourceImpl::UpdateBroadcastAudioConfigToHal(
     return;
   }
 
-  LOG_INFO();
+  log::debug("");
   halSinkInterface_->UpdateBroadcastAudioConfigToHal(config);
 }
 }  // namespace
@@ -492,7 +492,7 @@ LeAudioSourceAudioHalClient::AcquireUnicast() {
     return nullptr;
   }
 
-  LOG_INFO();
+  log::debug("");
   return std::move(impl);
 }
 
@@ -505,7 +505,7 @@ LeAudioSourceAudioHalClient::AcquireBroadcast() {
     return nullptr;
   }
 
-  LOG_INFO();
+  log::debug("");
   return std::move(impl);
 }
 

system/bta/le_audio/broadcaster/broadcaster.cc
@@ -98,7 +98,7 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
         current_phy_(PHY_LE_2M),
         audio_data_path_state_(AudioDataPathState::INACTIVE),
         le_audio_source_hal_client_(nullptr) {
-    LOG_INFO();
+    log::debug("");
 
     /* Register State machine callbacks */
     BroadcastStateMachine::Initialize(&state_machine_callbacks_,
@@ -430,8 +430,7 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
   /* Choose the dominating audio context when multiple contexts are mixed */
   LeAudioContextType ChooseConfigurationContextType(
       AudioContexts audio_contexts) {
-    LOG_DEBUG("Got contexts=%s",
-              bluetooth::common::ToString(audio_contexts).c_str());
+    log::debug("Got contexts={}", bluetooth::common::ToString(audio_contexts));
 
     /* Prioritize the most common use cases. */
     if (audio_contexts.any()) {
@@ -443,16 +442,15 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
       };
       for (auto ct : context_priority_list) {
         if (audio_contexts.test(ct)) {
-          LOG_DEBUG("Selecting configuration context type: %s",
-                    ToString(ct).c_str());
+          log::debug("Selecting configuration context type: {}", ToString(ct));
           return ct;
         }
       }
     }
 
     auto fallback_config = LeAudioContextType::MEDIA;
-    LOG_DEBUG("Selecting configuration context type: %s",
-              ToString(fallback_config).c_str());
+    log::debug("Selecting configuration context type: {}",
+               ToString(fallback_config));
     return fallback_config;
   }
 
@@ -595,7 +593,7 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
 
     if (public_features & bluetooth::le_audio::kLeAudioQualityHigh &&
         config->GetSamplingFrequencyHzMax() < 48000) {
-      LOG_WARN(
+      log::warn(
           "Preferred quality isn't supported. Fallback to standard audio "
           "quality");
       public_features &= (0xFFFF & ~bluetooth::le_audio::kLeAudioQualityHigh);
@@ -1158,7 +1156,7 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
 
       if (!broadcast_config_.has_value() ||
           (broadcast_config_->subgroups.size() == 0)) {
-        LOG_ERROR("Codec was not configured properly");
+        log::error("Codec was not configured properly");
         return;
       }
 
@@ -1195,14 +1193,14 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
     }
 
     virtual void OnAudioSuspend(void) override {
-      LOG_INFO();
+      log::debug("");
       /* TODO: Should we suspend all broadcasts - remove BIGs? */
       if (instance)
         instance->audio_data_path_state_ = AudioDataPathState::SUSPENDED;
     }
 
     virtual void OnAudioResume(void) override {
-      LOG_INFO();
+      log::debug("");
       if (!instance) return;
 
       /* TODO: Should we resume all broadcasts - recreate BIGs? */
@@ -1218,7 +1216,7 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
 
     virtual void OnAudioMetadataUpdate(source_metadata_v7 source_metadata,
                                        DsaMode dsa_mode) override {
-      LOG_INFO();
+      log::debug("");
       if (!instance) return;
 
       /* TODO: Should we take supported contexts from ASCS? */
@@ -1268,7 +1266,7 @@ void LeAudioBroadcaster::Initialize(
     bluetooth::le_audio::LeAudioBroadcasterCallbacks* callbacks,
     base::Callback<bool()> audio_hal_verifier) {
   std::scoped_lock<std::mutex> lock(instance_mutex);
-  LOG_INFO();
+  log::debug("");
   if (instance) {
     log::error("Already initialized");
     return;
@@ -1299,13 +1297,13 @@ void LeAudioBroadcaster::Initialize(
 bool LeAudioBroadcaster::IsLeAudioBroadcasterRunning() { return instance; }
 
 LeAudioBroadcaster* LeAudioBroadcaster::Get(void) {
-  LOG_INFO();
+  log::debug("");
   CHECK(instance);
   return instance;
 }
 
 void LeAudioBroadcaster::Stop(void) {
-  LOG_INFO();
+  log::debug("");
 
   if (instance) {
     instance->Stop();
@@ -1314,7 +1312,7 @@ void LeAudioBroadcaster::Stop(void) {
 
 void LeAudioBroadcaster::Cleanup(void) {
   std::scoped_lock<std::mutex> lock(instance_mutex);
-  LOG_INFO();
+  log::debug("");
 
   if (instance == nullptr) return;
 

LineageOS/android_packages_modules_Bluetooth

system/bta/le_audio/audio_hal_client/audio_sink_hal_client.cc
@@ -93,7 +93,7 @@ bool SinkImpl::Acquire() {
     return false;
   }
 
-  LOG_INFO();
+  log::info("");
   le_audio_source_hal_state = HAL_STOPPED;
   return true;
 }
@@ -104,7 +104,7 @@ void SinkImpl::Release() {
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   if (halSourceInterface_) {
     halSourceInterface_->Cleanup();
 
@@ -219,7 +219,7 @@ void SinkImpl::Stop() {
     return;
   }
 
-  LOG_INFO();
+  log::info("");
 
   halSourceInterface_->StopSession();
   le_audio_source_hal_state = HAL_STOPPED;
@@ -256,7 +256,7 @@ void SinkImpl::ConfirmStreamingRequest() {
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   if (IS_FLAG_ENABLED(leaudio_start_stream_race_fix)) {
     halSourceInterface_->ConfirmStreamingRequestV2();
   } else {
@@ -271,7 +271,7 @@ void SinkImpl::SuspendedForReconfiguration() {
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   halSourceInterface_->SuspendedForReconfiguration();
 }
 
@@ -282,7 +282,7 @@ void SinkImpl::ReconfigurationComplete() {
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   halSourceInterface_->ReconfigurationComplete();
 }
 
@@ -293,7 +293,7 @@ void SinkImpl::CancelStreamingRequest() {
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   if (IS_FLAG_ENABLED(leaudio_start_stream_race_fix)) {
     halSourceInterface_->CancelStreamingRequestV2();
   } else {
@@ -308,7 +308,7 @@ void SinkImpl::UpdateRemoteDelay(uint16_t remote_delay_ms) {
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   halSourceInterface_->SetRemoteDelay(remote_delay_ms);
 }
 
@@ -320,7 +320,7 @@ void SinkImpl::UpdateAudioConfigToHal(
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   halSourceInterface_->UpdateAudioConfigToHal(config);
 }
 }  // namespace
@@ -334,7 +334,7 @@ LeAudioSinkAudioHalClient::AcquireUnicast() {
     return nullptr;
   }
 
-  LOG_INFO();
+  log::info("");
   return std::move(impl);
 }
 

system/bta/le_audio/audio_hal_client/audio_source_hal_client.cc
@@ -134,7 +134,7 @@ bool SourceImpl::Acquire() {
     return false;
   }
 
-  LOG_INFO();
+  log::info("");
   le_audio_sink_hal_state_ = HAL_STOPPED;
   return this->InitAudioSinkThread();
 }
@@ -145,7 +145,7 @@ void SourceImpl::Release() {
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   worker_thread_->ShutDown();
 
   if (halSinkInterface_) {
@@ -365,7 +365,7 @@ void SourceImpl::Stop() {
     return;
   }
 
-  LOG_INFO();
+  log::info("");
 
   halSinkInterface_->StopSession();
   le_audio_sink_hal_state_ = HAL_STOPPED;
@@ -392,7 +392,7 @@ void SourceImpl::ConfirmStreamingRequest() {
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   if (IS_FLAG_ENABLED(leaudio_start_stream_race_fix)) {
     halSinkInterface_->ConfirmStreamingRequestV2();
   } else {
@@ -418,7 +418,7 @@ void SourceImpl::SuspendedForReconfiguration() {
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   halSinkInterface_->SuspendedForReconfiguration();
 }
 
@@ -429,7 +429,7 @@ void SourceImpl::ReconfigurationComplete() {
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   halSinkInterface_->ReconfigurationComplete();
 }
 
@@ -440,7 +440,7 @@ void SourceImpl::CancelStreamingRequest() {
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   if (IS_FLAG_ENABLED(leaudio_start_stream_race_fix)) {
     halSinkInterface_->CancelStreamingRequestV2();
   } else {
@@ -455,7 +455,7 @@ void SourceImpl::UpdateRemoteDelay(uint16_t remote_delay_ms) {
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   halSinkInterface_->SetRemoteDelay(remote_delay_ms);
 }
 
@@ -467,7 +467,7 @@ void SourceImpl::UpdateAudioConfigToHal(
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   halSinkInterface_->UpdateAudioConfigToHal(config);
 }
 
@@ -478,7 +478,7 @@ void SourceImpl::UpdateBroadcastAudioConfigToHal(
     return;
   }
 
-  LOG_INFO();
+  log::info("");
   halSinkInterface_->UpdateBroadcastAudioConfigToHal(config);
 }
 }  // namespace
@@ -492,7 +492,7 @@ LeAudioSourceAudioHalClient::AcquireUnicast() {
     return nullptr;
   }
 
-  LOG_INFO();
+  log::info("");
   return std::move(impl);
 }
 
@@ -505,7 +505,7 @@ LeAudioSourceAudioHalClient::AcquireBroadcast() {
     return nullptr;
   }
 
-  LOG_INFO();
+  log::info("");
   return std::move(impl);
 }
 

system/bta/le_audio/broadcaster/broadcaster.cc
@@ -98,7 +98,7 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
         current_phy_(PHY_LE_2M),
         audio_data_path_state_(AudioDataPathState::INACTIVE),
         le_audio_source_hal_client_(nullptr) {
-    LOG_INFO();
+    log::info("");
 
     /* Register State machine callbacks */
     BroadcastStateMachine::Initialize(&state_machine_callbacks_,
@@ -430,8 +430,7 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
   /* Choose the dominating audio context when multiple contexts are mixed */
   LeAudioContextType ChooseConfigurationContextType(
       AudioContexts audio_contexts) {
-    LOG_DEBUG("Got contexts=%s",
-              bluetooth::common::ToString(audio_contexts).c_str());
+    log::debug("Got contexts={}", bluetooth::common::ToString(audio_contexts));
 
     /* Prioritize the most common use cases. */
     if (audio_contexts.any()) {
@@ -443,16 +442,15 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
       };
       for (auto ct : context_priority_list) {
         if (audio_contexts.test(ct)) {
-          LOG_DEBUG("Selecting configuration context type: %s",
-                    ToString(ct).c_str());
+          log::debug("Selecting configuration context type: {}", ToString(ct));
           return ct;
         }
       }
     }
 
     auto fallback_config = LeAudioContextType::MEDIA;
-    LOG_DEBUG("Selecting configuration context type: %s",
-              ToString(fallback_config).c_str());
+    log::debug("Selecting configuration context type: {}",
+               ToString(fallback_config));
     return fallback_config;
   }
 
@@ -595,7 +593,7 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
 
     if (public_features & bluetooth::le_audio::kLeAudioQualityHigh &&
         config->GetSamplingFrequencyHzMax() < 48000) {
-      LOG_WARN(
+      log::warn(
           "Preferred quality isn't supported. Fallback to standard audio "
           "quality");
       public_features &= (0xFFFF & ~bluetooth::le_audio::kLeAudioQualityHigh);
@@ -1158,7 +1156,7 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
 
       if (!broadcast_config_.has_value() ||
           (broadcast_config_->subgroups.size() == 0)) {
-        LOG_ERROR("Codec was not configured properly");
+        log::error("Codec was not configured properly");
         return;
       }
 
@@ -1195,14 +1193,14 @@ class LeAudioBroadcasterImpl : public LeAudioBroadcaster, public BigCallbacks {
     }
 
     virtual void OnAudioSuspend(void) override {
-      LOG_INFO();
+      log::info("");
       /* TODO: Should we suspend all broadcasts - remove BIGs? */
       if (instance)
         instance->audio_data_path_state_ = AudioDataPathState::SUSPENDED;
     }
 
     virtual void OnAudioResume(void) override {
-      LOG_INFO();
+      log::info("");
       if (!instance) return;
 
       /* TODO: Should we resume all broadcasts - recreate BIGs? */
@@ -1268,7 +1266,7 @@ void LeAudioBroadcaster::Initialize(
     bluetooth::le_audio::LeAudioBroadcasterCallbacks* callbacks,
     base::Callback<bool()> audio_hal_verifier) {
   std::scoped_lock<std::mutex> lock(instance_mutex);
-  LOG_INFO();
+  log::info("");
   if (instance) {
     log::error("Already initialized");
     return;
@@ -1299,13 +1297,13 @@ void LeAudioBroadcaster::Initialize(
 bool LeAudioBroadcaster::IsLeAudioBroadcasterRunning() { return instance; }
 
 LeAudioBroadcaster* LeAudioBroadcaster::Get(void) {
-  LOG_INFO();
+  log::info("");
   CHECK(instance);
   return instance;
 }
 
 void LeAudioBroadcaster::Stop(void) {
-  LOG_INFO();
+  log::info("");
 
   if (instance) {
     instance->Stop();
@@ -1314,7 +1312,7 @@ void LeAudioBroadcaster::Stop(void) {
 
 void LeAudioBroadcaster::Cleanup(void) {
   std::scoped_lock<std::mutex> lock(instance_mutex);
-  LOG_INFO();
+  log::info("");
 
   if (instance == nullptr) return;
 