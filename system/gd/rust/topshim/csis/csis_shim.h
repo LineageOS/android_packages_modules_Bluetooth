3c7ccd11f55404e763709bb9540ec9e89f9d1dec
Jeremy Wu <jrwu@google.com>
2024-04-01 09:53:29 +0000

    Floss: implement csis_shim
    This CL implements the topshim layer to bridge APIs between the native
    stack and the Rust interface.
    
    Bug: 317682584
    Test: m Bluetooth
    Flag: EXEMPT floss only changes
    Change-Id: I27b8a5ad473dea05243a0710d4e3edb618f22da5

--- system/gd/rust/topshim/csis/csis_shim.h
+++ system/gd/rust/topshim/csis/csis_shim.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,30 +13,36 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef GD_RUST_TOPSHIM_COMMON_TYPE_ALIAS_H
-#define GD_RUST_TOPSHIM_COMMON_TYPE_ALIAS_H
 
-/*
- * Declare type aliases in the topshim namespace in this file.
- *
- * The type declarations in cxx bridge blocks are bound to the namespace of the
- * block. Since in topshim we always put the bridge codes in namespace
- * `bluetooth::topshim::rust`, to reuse the existing types in GD it's necessary
- * to define the aliases in the topshim namespace.
- */
+#pragma once
 
-#include "types/bluetooth/uuid.h"
+#include <memory>
+
+#include "include/hardware/bt_csis.h"
+#include "rust/cxx.h"
 #include "types/raw_address.h"
 
 namespace bluetooth {
 namespace topshim {
 namespace rust {
 
-using RawAddress = ::RawAddress;
-using Uuid = ::bluetooth::Uuid;
+class CsisClientIntf {
+ public:
+  CsisClientIntf(csis::CsisClientInterface* intf) : intf_(intf){};
+
+  void init(/*CsisClientCallbacks* callbacks*/);
+  void connect(RawAddress addr);
+  void disconnect(RawAddress addr);
+  void lock_group(int group_id, bool lock);
+  void remove_device(RawAddress addr);
+  void cleanup();
+
+ private:
+  csis::CsisClientInterface* intf_;
+};
+
+std::unique_ptr<CsisClientIntf> GetCsisClientProfile(const unsigned char* btif);
 
 }  // namespace rust
 }  // namespace topshim
 }  // namespace bluetooth
-
-#endif  // GD_RUST_TOPSHIM_COMMON_TYPE_ALIAS_H
