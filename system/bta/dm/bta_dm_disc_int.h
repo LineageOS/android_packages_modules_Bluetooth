miyu-xu/packages-modules-Bluetooth

Memory safe events in bta_dm_disc
This patch explores possibility of replacing malloced memory with
unique_ptr to variant for messages passed in BTA layer.

Such container would be memory safe, and free of any potential leaks.
In such containers, one could pass types like std::vector, and std::list

Bug: 330675788
Flag: exempt, infrastructure refactor that can't be easily isolated
Test: mma -j32
Change-Id: Ia7ea581e8a0d17ea4605d50f303803380b6d6625

system/bta/dm/bta_dm_disc_int.h
@@ -36,7 +36,7 @@
 /* DM search events */
 typedef enum : uint16_t {
   /* DM search API events */
-  BTA_DM_API_SEARCH_EVT = BTA_SYS_EVT_START(BTA_ID_DM_SEARCH),
+  BTA_DM_API_SEARCH_EVT,
   BTA_DM_API_SEARCH_CANCEL_EVT,
   BTA_DM_API_DISCOVER_EVT,
   BTA_DM_INQUIRY_CMPL_EVT,
@@ -65,25 +65,21 @@ inline std::string bta_dm_event_text(const tBTA_DM_EVT& event) {
 
 /* data type for BTA_DM_API_SEARCH_EVT */
 typedef struct {
-  BT_HDR_RIGID hdr;
   tBTA_SERVICE_MASK services;
   tBTA_DM_SEARCH_CBACK* p_cback;
 } tBTA_DM_API_SEARCH;
 
 /* data type for BTA_DM_API_DISCOVER_EVT */
 typedef struct {
-  BT_HDR_RIGID hdr;
   RawAddress bd_addr;
   service_discovery_callbacks cbacks;
   tBT_TRANSPORT transport;
 } tBTA_DM_API_DISCOVER;
 
 typedef struct {
-  BT_HDR_RIGID hdr;
 } tBTA_DM_API_DISCOVERY_CANCEL;
 
 typedef struct {
-  BT_HDR_RIGID hdr;
   RawAddress bd_addr;
   BD_NAME bd_name; /* Name of peer device. */
   tHCI_STATUS hci_status;
@@ -91,45 +87,26 @@ typedef struct {
 
 /* data type for tBTA_DM_DISC_RESULT */
 typedef struct {
-  BT_HDR_RIGID hdr;
   tBTA_DM_SEARCH result;
 } tBTA_DM_DISC_RESULT;
 
 /* data type for BTA_DM_INQUIRY_CMPL_EVT */
 typedef struct {
-  BT_HDR_RIGID hdr;
   uint8_t num;
 } tBTA_DM_INQUIRY_CMPL;
 
 /* data type for BTA_DM_SDP_RESULT_EVT */
 typedef struct {
-  BT_HDR_RIGID hdr;
   tSDP_RESULT sdp_result;
 } tBTA_DM_SDP_RESULT;
 
 typedef struct {
-  BT_HDR_RIGID hdr;
   bool enable;
 } tBTA_DM_API_BLE_FEATURE;
 
-/* union of all data types */
-typedef union {
-  /* GKI event buffer header */
-  BT_HDR_RIGID hdr;
-
-  tBTA_DM_API_SEARCH search;
-
-  tBTA_DM_API_DISCOVER discover;
-
-  tBTA_DM_REMOTE_NAME remote_name_msg;
-
-  tBTA_DM_DISC_RESULT disc_result;
-
-  tBTA_DM_INQUIRY_CMPL inq_cmpl;
-
-  tBTA_DM_SDP_RESULT sdp_event;
-
-} tBTA_DM_MSG;
+using DM_MSG_SAFE =
+    std::variant<tBTA_DM_API_SEARCH, tBTA_DM_API_DISCOVER, tBTA_DM_REMOTE_NAME,
+                 tBTA_DM_DISC_RESULT, tBTA_DM_INQUIRY_CMPL, tBTA_DM_SDP_RESULT>;
 
 /* DM search state */
 typedef enum {
@@ -167,7 +144,7 @@ typedef struct {
   BD_NAME peer_name;
   alarm_t* search_timer;
   uint8_t service_index;
-  tBTA_DM_MSG* p_pending_search;
+  std::unique_ptr<DM_MSG_SAFE> p_pending_search;
   fixed_queue_t* pending_discovery_queue;
   bool wait_disc;
   bool sdp_results;

LineageOS/android_packages_modules_Bluetooth

system/bta/dm/bta_dm_disc_int.h
@@ -65,25 +65,21 @@ inline std::string bta_dm_event_text(const tBTA_DM_EVT& event) {
 
 /* data type for BTA_DM_API_SEARCH_EVT */
 typedef struct {
-  BT_HDR_RIGID hdr;
   tBTA_SERVICE_MASK services;
   tBTA_DM_SEARCH_CBACK* p_cback;
 } tBTA_DM_API_SEARCH;
 
 /* data type for BTA_DM_API_DISCOVER_EVT */
 typedef struct {
-  BT_HDR_RIGID hdr;
   RawAddress bd_addr;
   service_discovery_callbacks cbacks;
   tBT_TRANSPORT transport;
 } tBTA_DM_API_DISCOVER;
 
 typedef struct {
-  BT_HDR_RIGID hdr;
 } tBTA_DM_API_DISCOVERY_CANCEL;
 
 typedef struct {
-  BT_HDR_RIGID hdr;
   RawAddress bd_addr;
   BD_NAME bd_name; /* Name of peer device. */
   tHCI_STATUS hci_status;
@@ -91,45 +87,26 @@ typedef struct {
 
 /* data type for tBTA_DM_DISC_RESULT */
 typedef struct {
-  BT_HDR_RIGID hdr;
   tBTA_DM_SEARCH result;
 } tBTA_DM_DISC_RESULT;
 
 /* data type for BTA_DM_INQUIRY_CMPL_EVT */
 typedef struct {
-  BT_HDR_RIGID hdr;
   uint8_t num;
 } tBTA_DM_INQUIRY_CMPL;
 
 /* data type for BTA_DM_SDP_RESULT_EVT */
 typedef struct {
-  BT_HDR_RIGID hdr;
   tSDP_RESULT sdp_result;
 } tBTA_DM_SDP_RESULT;
 
 typedef struct {
-  BT_HDR_RIGID hdr;
   bool enable;
 } tBTA_DM_API_BLE_FEATURE;
 
-/* union of all data types */
-typedef union {
-  /* GKI event buffer header */
-  BT_HDR_RIGID hdr;
-
-  tBTA_DM_API_SEARCH search;
-
-  tBTA_DM_API_DISCOVER discover;
-
-  tBTA_DM_REMOTE_NAME remote_name_msg;
-
-  tBTA_DM_DISC_RESULT disc_result;
-
-  tBTA_DM_INQUIRY_CMPL inq_cmpl;
-
-  tBTA_DM_SDP_RESULT sdp_event;
-
-} tBTA_DM_MSG;
+using tBTA_DM_MSG =
+    std::variant<tBTA_DM_API_SEARCH, tBTA_DM_API_DISCOVER, tBTA_DM_REMOTE_NAME,
+                 tBTA_DM_DISC_RESULT, tBTA_DM_INQUIRY_CMPL, tBTA_DM_SDP_RESULT>;
 
 /* DM search state */
 typedef enum {
@@ -167,7 +144,7 @@ typedef struct {
   BD_NAME peer_name;
   alarm_t* search_timer;
   uint8_t service_index;
-  tBTA_DM_MSG* p_pending_search;
+  std::unique_ptr<tBTA_DM_MSG> p_pending_search;
   fixed_queue_t* pending_discovery_queue;
   bool wait_disc;
   bool sdp_results;