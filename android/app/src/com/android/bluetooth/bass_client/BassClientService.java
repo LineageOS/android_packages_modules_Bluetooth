e7ceccfba32d3968fd82515437423306ef8f6051
Grzegorz Kołodziejczyk <gkolodziejczyk@google.com>
2024-04-09 14:17:41 +0000

    bass_client: Use PA_Sync value in update source context
    Intention of this change is to unify usage of parameters in modify
    source context. To not mix, status with value let's use a most proper
    value which is PA_Sync value.
    
    Tag: #bug
    Bug: 330690507
    Bug: 333050419
    Test: atest BassClientServiceTest
    Test: atest BassClientStateMachineTest
    Change-Id: I2125994b7251b9da2abb5b7298b78ceafe52a7e7

--- android/app/src/com/android/bluetooth/bass_client/BassClientService.java
+++ android/app/src/com/android/bluetooth/bass_client/BassClientService.java
@@ -1672,7 +1672,7 @@ public class BassClientService extends ProfileService {
             Message message =
                     stateMachine.obtainMessage(BassClientStateMachine.UPDATE_BCAST_SOURCE);
             message.arg1 = deviceSourceId;
-            message.arg2 = BluetoothLeBroadcastReceiveState.PA_SYNC_STATE_INVALID;
+            message.arg2 = BassConstants.INVALID_PA_SYNC_VALUE;
             message.obj = updatedMetadata;
             stateMachine.sendMessage(message);
             if (code != null && code.length != 0) {
@@ -1744,7 +1744,7 @@ public class BassClientService extends ProfileService {
                 Message message = stateMachine.obtainMessage(
                         BassClientStateMachine.UPDATE_BCAST_SOURCE);
                 message.arg1 = sourceId;
-                message.arg2 = BluetoothLeBroadcastReceiveState.PA_SYNC_STATE_IDLE;
+                message.arg2 = BassConstants.PA_SYNC_DO_NOT_SYNC;
                 /* Pending remove set. Remove source once not synchronized to PA */
                 message.obj = metaData;
                 stateMachine.sendMessage(message);
