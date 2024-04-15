miyu-xu/packages-modules-Bluetooth

HCI: Don't abort for unsuccessful connections
When a connection complete arrives with a disconnect reason, log it and
continue.

Bug: 328137828
Test: atest bluetooth_test_gd_unit
Flag: EXEMPT, remove a debugging crash
Change-Id: I69a97b3f854e7c5f5d638796c1f831b3865a7643

system/gd/hci/acl_manager/classic_impl.h
@@ -393,16 +393,16 @@ struct classic_impl : public security::ISecurityManagerListener {
                 Address address,
                 ErrorCode status,
                 std::string valid_incoming_addresses) {
-              ASSERT_LOG(
-                  status == ErrorCode::UNKNOWN_CONNECTION,
-                  "No prior connection request for %s expecting:%s",
-                  ADDRESS_TO_LOGGABLE_CSTR(address),
-                  valid_incoming_addresses.c_str());
               log::warn(
                   "No matching connection to {} ({})",
                   ADDRESS_TO_LOGGABLE_CSTR(address),
                   ErrorCodeText(status));
-              log::warn("Firmware error after RemoteNameRequestCancel?");  // see b/184239841
+              if (status == ErrorCode::SUCCESS) {
+                log::fatal(
+                    "No prior connection request for {} expecting:{}",
+                    ADDRESS_TO_LOGGABLE_CSTR(address),
+                    valid_incoming_addresses.c_str());
+              }
               remote_name_request_module->ReportRemoteNameRequestCancellation(address);
             },
             common::Unretained(remote_name_request_module_),

LineageOS/android_packages_modules_Bluetooth

system/gd/hci/acl_manager/classic_impl.h
@@ -398,16 +398,15 @@ struct classic_impl {
                 Address address,
                 ErrorCode status,
                 std::string valid_incoming_addresses) {
-              log::assert_that(
-                  status == ErrorCode::UNKNOWN_CONNECTION,
-                  "No prior connection request for {} expecting:{}",
-                  ADDRESS_TO_LOGGABLE_CSTR(address),
-                  valid_incoming_addresses);
               log::warn(
                   "No matching connection to {} ({})",
                   ADDRESS_TO_LOGGABLE_CSTR(address),
                   ErrorCodeText(status));
-              log::warn("Firmware error after RemoteNameRequestCancel?");  // see b/184239841
+              log::assert_that(
+                  status != ErrorCode::SUCCESS,
+                  "No prior connection request for {} expecting:{}",
+                  ADDRESS_TO_LOGGABLE_CSTR(address),
+                  valid_incoming_addresses.c_str());
               remote_name_request_module->ReportRemoteNameRequestCancellation(address);
             },
             common::Unretained(remote_name_request_module_),