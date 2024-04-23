127e22f113fb470a18956db943247b35777b58e6
Jakub Rotkiewicz <rotkiewicz@google.com>
2024-04-23 12:19:31 +0000

    IOT Device Info: Replace legacy with trunk stable flag
    Bug: 261319328
    Bug: 290844229
    Bug: 261319328
    Flag: com.android.bluetooth.flags.device_iot_config_logging
    Test: atest bluetooth_test_gd
    Change-Id: I80b2e09529da260183da5e20f0a166e35be03173

--- system/gd/rust/common/src/init_flags.rs
+++ system/gd/rust/common/src/init_flags.rs
@@ -214,7 +214,6 @@ init_flags!(
         bluetooth_power_telemetry = false,
         btm_dm_flush_discovery_queue_on_search_cancel,
         classic_discovery_only,
-        device_iot_config_logging,
         dynamic_avrcp_version_enhancement = true,
         gatt_robust_caching_server,
         hci_adapter: i32,
