miyu-xu/packages-modules-Bluetooth

hci/hal: Separate AIDL/HIDL in the HAL
Bug: 333612868
Test: m
Flag: EXEMPT, code move
Change-Id: I23e36af000f8a6c527d1488490079e94d7d8bad7

system/gd/hal/hci_hal_android.cc
@@ -1,0 +1,197 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android/binder_manager.h>
+#include <bluetooth/log.h>
+
+#include <future>
+#include <mutex>
+
+#include "hal/hci_backend.h"
+#include "hal/hci_hal.h"
+#include "hal/link_clocker.h"
+#include "hal/snoop_logger.h"
+
+namespace bluetooth::hal {
+
+class HciProxyCallbacks : public HciBackendCallbacks {
+  class : public HciHalCallbacks {
+   public:
+    void hciEventReceived(HciPacket) override {
+      log::warn("Dropping HCI Event, since callback is not set");
+    }
+    void aclDataReceived(HciPacket) override {
+      log::warn("Dropping ACL Data, since callback is not set");
+    }
+    void scoDataReceived(HciPacket) override {
+      log::warn("Dropping SCO Data, since callback is not set");
+    }
+    void isoDataReceived(HciPacket) override {
+      log::warn("Dropping ISO Data, since callback is not set");
+    }
+  } kNullCallbacks;
+
+ public:
+  std::promise<void>* const init_promise = &init_promise_;
+
+  HciProxyCallbacks(SnoopLogger* btsnoop_logger, LinkClocker* link_clocker)
+      : link_clocker_(link_clocker), btsnoop_logger_(btsnoop_logger) {}
+
+  void SetCallback(HciHalCallbacks* callback) {
+    log::assert_that(callback_ == &kNullCallbacks, "callbacks already set");
+    log::assert_that(callback != nullptr, "callback != nullptr");
+    std::lock_guard<std::mutex> lock(mutex_);
+    callback_ = callback;
+  }
+
+  void ResetCallback() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    log::info("callbacks have been reset!");
+    callback_ = &kNullCallbacks;
+  }
+
+  void initializationComplete() override {
+    init_promise_.set_value();
+  }
+
+  void hciEventReceived(const std::vector<uint8_t>& packet) override {
+    link_clocker_->OnHciEvent(packet);
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::INCOMING, SnoopLogger::PacketType::EVT);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      callback_->hciEventReceived(packet);
+    }
+  }
+
+  void aclDataReceived(const std::vector<uint8_t>& packet) override {
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::INCOMING, SnoopLogger::PacketType::ACL);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      callback_->aclDataReceived(packet);
+    }
+  }
+
+  void scoDataReceived(const std::vector<uint8_t>& packet) override {
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::INCOMING, SnoopLogger::PacketType::SCO);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      callback_->scoDataReceived(packet);
+    }
+  }
+
+  void isoDataReceived(const std::vector<uint8_t>& packet) override {
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::INCOMING, SnoopLogger::PacketType::ISO);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      callback_->isoDataReceived(packet);
+    }
+  }
+
+ private:
+  std::mutex mutex_;
+  std::promise<void> init_promise_;
+  HciHalCallbacks* callback_ = &kNullCallbacks;
+  LinkClocker* link_clocker_;
+  SnoopLogger* btsnoop_logger_;
+};
+
+class HciProxy : public HciHal {
+  static constexpr char kBluetoothAidlHalServiceName[] =
+      "android.hardware.bluetooth.IBluetoothHci/default";
+
+ public:
+  void registerIncomingPacketCallback(HciHalCallbacks* callback) override {
+    callbacks_->SetCallback(callback);
+  }
+
+  void unregisterIncomingPacketCallback() override {
+    callbacks_->ResetCallback();
+  }
+
+  void sendHciCommand(HciPacket packet) override {
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::OUTGOING, SnoopLogger::PacketType::CMD);
+    backend_->sendHciCommand(packet);
+  }
+
+  void sendAclData(HciPacket packet) override {
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::OUTGOING, SnoopLogger::PacketType::ACL);
+    backend_->sendAclData(packet);
+  }
+
+  void sendScoData(HciPacket packet) override {
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::OUTGOING, SnoopLogger::PacketType::SCO);
+    backend_->sendScoData(packet);
+  }
+
+  void sendIsoData(HciPacket packet) override {
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::OUTGOING, SnoopLogger::PacketType::ISO);
+    backend_->sendIsoData(packet);
+  }
+
+ protected:
+  void ListDependencies(ModuleList* list) const override {
+    list->add<LinkClocker>();
+    list->add<SnoopLogger>();
+  }
+
+  void Start() override {
+    log::assert_that(
+        backend_ == nullptr, "Start can't be called more than once before Stop is called.");
+
+    link_clocker_ = GetDependency<LinkClocker>();
+    btsnoop_logger_ = GetDependency<SnoopLogger>();
+
+    if (AServiceManager_isDeclared(kBluetoothAidlHalServiceName)) {
+      backend_ = HciBackend::CreateAidl(kBluetoothAidlHalServiceName);
+    } else {
+      backend_ = HciBackend::CreateHidl(GetHandler());
+    }
+
+    callbacks_ = std::make_shared<HciProxyCallbacks>(btsnoop_logger_, link_clocker_);
+
+    backend_->initialize(callbacks_);
+    callbacks_->init_promise->get_future();
+  }
+
+  void Stop() override {
+    backend_ = nullptr;
+    callbacks_ = nullptr;
+    btsnoop_logger_ = nullptr;
+    link_clocker_ = nullptr;
+  }
+
+  std::string ToString() const override {
+    return std::string("HciHal");
+  }
+
+ private:
+  std::shared_ptr<HciProxyCallbacks> callbacks_;
+  std::shared_ptr<HciBackend> backend_;
+  SnoopLogger* btsnoop_logger_ = nullptr;
+  LinkClocker* link_clocker_ = nullptr;
+};
+
+const ModuleFactory HciHal::Factory = ModuleFactory([]() { return new HciProxy(); });
+
+}  // namespace bluetooth::hal

LineageOS/android_packages_modules_Bluetooth

Test: mmm packages/modules/Bluetooth

system/gd/hal/hci_hal_android.cc
@@ -1,0 +1,197 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android/binder_manager.h>
+#include <bluetooth/log.h>
+
+#include <future>
+#include <mutex>
+
+#include "hal/hci_backend.h"
+#include "hal/hci_hal.h"
+#include "hal/link_clocker.h"
+#include "hal/snoop_logger.h"
+
+namespace bluetooth::hal {
+
+class HciCallbacksImpl : public HciBackendCallbacks {
+  class : public HciHalCallbacks {
+   public:
+    void hciEventReceived(HciPacket) override {
+      log::warn("Dropping HCI Event, since callback is not set");
+    }
+    void aclDataReceived(HciPacket) override {
+      log::warn("Dropping ACL Data, since callback is not set");
+    }
+    void scoDataReceived(HciPacket) override {
+      log::warn("Dropping SCO Data, since callback is not set");
+    }
+    void isoDataReceived(HciPacket) override {
+      log::warn("Dropping ISO Data, since callback is not set");
+    }
+  } kNullCallbacks;
+
+ public:
+  std::promise<void>* const init_promise = &init_promise_;
+
+  HciCallbacksImpl(SnoopLogger* btsnoop_logger, LinkClocker* link_clocker)
+      : link_clocker_(link_clocker), btsnoop_logger_(btsnoop_logger) {}
+
+  void SetCallback(HciHalCallbacks* callback) {
+    log::assert_that(callback_ == &kNullCallbacks, "callbacks already set");
+    log::assert_that(callback != nullptr, "callback != nullptr");
+    std::lock_guard<std::mutex> lock(mutex_);
+    callback_ = callback;
+  }
+
+  void ResetCallback() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    log::info("callbacks have been reset!");
+    callback_ = &kNullCallbacks;
+  }
+
+  void initializationComplete() override {
+    init_promise_.set_value();
+  }
+
+  void hciEventReceived(const std::vector<uint8_t>& packet) override {
+    link_clocker_->OnHciEvent(packet);
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::INCOMING, SnoopLogger::PacketType::EVT);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      callback_->hciEventReceived(packet);
+    }
+  }
+
+  void aclDataReceived(const std::vector<uint8_t>& packet) override {
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::INCOMING, SnoopLogger::PacketType::ACL);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      callback_->aclDataReceived(packet);
+    }
+  }
+
+  void scoDataReceived(const std::vector<uint8_t>& packet) override {
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::INCOMING, SnoopLogger::PacketType::SCO);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      callback_->scoDataReceived(packet);
+    }
+  }
+
+  void isoDataReceived(const std::vector<uint8_t>& packet) override {
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::INCOMING, SnoopLogger::PacketType::ISO);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      callback_->isoDataReceived(packet);
+    }
+  }
+
+ private:
+  std::mutex mutex_;
+  std::promise<void> init_promise_;
+  HciHalCallbacks* callback_ = &kNullCallbacks;
+  LinkClocker* link_clocker_;
+  SnoopLogger* btsnoop_logger_;
+};
+
+class HciHalImpl : public HciHal {
+  static constexpr char kBluetoothAidlHalServiceName[] =
+      "android.hardware.bluetooth.IBluetoothHci/default";
+
+ public:
+  void registerIncomingPacketCallback(HciHalCallbacks* callback) override {
+    callbacks_->SetCallback(callback);
+  }
+
+  void unregisterIncomingPacketCallback() override {
+    callbacks_->ResetCallback();
+  }
+
+  void sendHciCommand(HciPacket packet) override {
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::OUTGOING, SnoopLogger::PacketType::CMD);
+    backend_->sendHciCommand(packet);
+  }
+
+  void sendAclData(HciPacket packet) override {
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::OUTGOING, SnoopLogger::PacketType::ACL);
+    backend_->sendAclData(packet);
+  }
+
+  void sendScoData(HciPacket packet) override {
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::OUTGOING, SnoopLogger::PacketType::SCO);
+    backend_->sendScoData(packet);
+  }
+
+  void sendIsoData(HciPacket packet) override {
+    btsnoop_logger_->Capture(
+        packet, SnoopLogger::Direction::OUTGOING, SnoopLogger::PacketType::ISO);
+    backend_->sendIsoData(packet);
+  }
+
+ protected:
+  void ListDependencies(ModuleList* list) const override {
+    list->add<LinkClocker>();
+    list->add<SnoopLogger>();
+  }
+
+  void Start() override {
+    log::assert_that(
+        backend_ == nullptr, "Start can't be called more than once before Stop is called.");
+
+    link_clocker_ = GetDependency<LinkClocker>();
+    btsnoop_logger_ = GetDependency<SnoopLogger>();
+
+    if (AServiceManager_isDeclared(kBluetoothAidlHalServiceName)) {
+      backend_ = HciBackend::CreateAidl(kBluetoothAidlHalServiceName);
+    } else {
+      backend_ = HciBackend::CreateHidl(GetHandler());
+    }
+
+    callbacks_ = std::make_shared<HciCallbacksImpl>(btsnoop_logger_, link_clocker_);
+
+    backend_->initialize(callbacks_);
+    callbacks_->init_promise->get_future();
+  }
+
+  void Stop() override {
+    backend_.reset();
+    callbacks_.reset();
+    btsnoop_logger_ = nullptr;
+    link_clocker_ = nullptr;
+  }
+
+  std::string ToString() const override {
+    return std::string("HciHal");
+  }
+
+ private:
+  std::shared_ptr<HciCallbacksImpl> callbacks_;
+  std::shared_ptr<HciBackend> backend_;
+  SnoopLogger* btsnoop_logger_ = nullptr;
+  LinkClocker* link_clocker_ = nullptr;
+};
+
+const ModuleFactory HciHal::Factory = ModuleFactory([]() { return new HciHalImpl(); });
+
+}  // namespace bluetooth::hal