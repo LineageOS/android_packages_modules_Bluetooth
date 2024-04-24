miyu-xu/packages-modules-Bluetooth

Do not remove duplicate non-LE-only device records
If identity address of the newly bonded device matches with an existing
bonded device, the existing bonded device is removed. This logic was
added to accomodate some LE-only devices which keep changing their IRK
which resulted in duplicate device records.
However this logic also removes the dual mode devices which don't
support CTKD. Pairing on both transport results in separate bond state
events which triggers this logic and removes the bond for both the
transports.

Test: mmm packages/modules/Bluetooth
Test: Manual | Pair on both transport with non-CTKD supporting dual mode
devices
Test: Manual | Repair with LE-only devices which keep changing its IRK
Bug: 322360482
Bug: 333949759

Change-Id: I48fdd9be2f76b854c47f47fb57794584468dbf16

android/app/src/com/android/bluetooth/btservice/AdapterProperties.java
@@ -674,7 +674,8 @@ class AdapterProperties {
                 Flags.identityAddressNullIfUnknown()
                         ? Utils.getBrEdrAddress(device, mService)
                         : mService.getIdentityAddress(address);
-        debugLog("cleanupPrevBondRecordsFor: " + device);
+        int deviceType = mRemoteDevices.getDeviceProperties(device).getDeviceType();
+        debugLog("cleanupPrevBondRecordsFor: " + device + ", device type: " + deviceType);
         if (identityAddress == null) {
             return;
         }
@@ -685,23 +686,47 @@ class AdapterProperties {
                     Flags.identityAddressNullIfUnknown()
                             ? Utils.getBrEdrAddress(existingDevice, mService)
                             : mService.getIdentityAddress(existingAddress);
+            int existingDeviceType =
+                    mRemoteDevices.getDeviceProperties(existingDevice).getDeviceType();
 
             if (identityAddress.equals(existingIdentityAddress)
                     && !address.equals(existingAddress)) {
-                if (mService.getNative().removeBond(Utils.getBytesFromAddress(existingAddress))) {
-                    mBondedDevices.remove(existingDevice);
-                    infoLog(
-                            "Removing old bond record: "
-                                    + existingDevice
-                                    + " for current device: "
-                                    + device);
-                } else {
-                    Log.e(
-                            TAG,
-                            "Unexpected error while removing old bond record:"
-                                    + existingDevice
-                                    + " for current device: "
-                                    + device);
+                boolean removeExisting = true;
+                if (Flags.cleanupLeOnlyDeviceType()) {
+                    // Existing device record should be removed only if the device type is LE-only
+                    if (existingDeviceType == BluetoothDevice.DEVICE_TYPE_LE
+                            && deviceType == BluetoothDevice.DEVICE_TYPE_LE) {
+                        removeExisting = true;
+                    } else {
+                        removeExisting = false;
+                        Log.w(
+                                TAG,
+                                "Device type of existing bond does not match: "
+                                        + device
+                                        + ", new device type: "
+                                        + deviceType
+                                        + ", existing device type: "
+                                        + existingDeviceType);
+                    }
+                }
+
+                if (removeExisting) {
+                    if (mService.getNative()
+                            .removeBond(Utils.getBytesFromAddress(existingAddress))) {
+                        mBondedDevices.remove(existingDevice);
+                        infoLog(
+                                "Removing old bond record: "
+                                        + existingDevice
+                                        + " for current device: "
+                                        + device);
+                    } else {
+                        Log.e(
+                                TAG,
+                                "Unexpected error while removing old bond record:"
+                                        + existingDevice
+                                        + " for current device: "
+                                        + device);
+                    }
                 }
                 break;
             }

LineageOS/android_packages_modules_Bluetooth

android/app/src/com/android/bluetooth/btservice/AdapterProperties.java
@@ -674,8 +674,13 @@ class AdapterProperties {
                 Flags.identityAddressNullIfUnknown()
                         ? Utils.getBrEdrAddress(device, mService)
                         : mService.getIdentityAddress(address);
-        debugLog("cleanupPrevBondRecordsFor: " + device);
+        int deviceType = mRemoteDevices.getDeviceProperties(device).getDeviceType();
+        debugLog("cleanupPrevBondRecordsFor: " + device + ", device type: " + deviceType);
         if (identityAddress == null) {
+            return;
+        }
+
+        if (Flags.cleanupLeOnlyDeviceType() && deviceType != BluetoothDevice.DEVICE_TYPE_LE) {
             return;
         }
 
@@ -685,29 +690,40 @@ class AdapterProperties {
                     Flags.identityAddressNullIfUnknown()
                             ? Utils.getBrEdrAddress(existingDevice, mService)
                             : mService.getIdentityAddress(existingAddress);
+            int existingDeviceType =
+                    mRemoteDevices.getDeviceProperties(existingDevice).getDeviceType();
 
-            if (!identityAddress.equals(existingIdentityAddress) || address.equals(
-                existingAddress)) {
-                continue;
+            boolean removeExisting = false;
+            if (identityAddress.equals(existingIdentityAddress)
+                    && !address.equals(existingAddress)) {
+                if (Flags.cleanupLeOnlyDeviceType()) {
+                    // Existing device record should be removed only if the device type is LE-only
+                    removeExisting = (existingDeviceType == BluetoothDevice.DEVICE_TYPE_LE);
+                } else {
+                    removeExisting = true;
+                }
             }
 
-            // Found an existing device with same identity address but different pseudo address
-            if (mService.getNative().removeBond(Utils.getBytesFromAddress(existingAddress))) {
-                mBondedDevices.remove(existingDevice);
-                infoLog(
-                    "Removing old bond record: "
-                        + existingDevice
-                        + " for the device: "
-                        + device);
-            } else {
-                Log.e(
-                    TAG,
-                    "Unexpected error while removing old bond record:"
-                        + existingDevice
-                        + " for the device: "
-                        + device);
+            if (removeExisting) {
+                // Found an existing LE-only device with the same identity address but different
+                // pseudo address
+                if (mService.getNative().removeBond(Utils.getBytesFromAddress(existingAddress))) {
+                    mBondedDevices.remove(existingDevice);
+                    infoLog(
+                            "Removing old bond record: "
+                                    + existingDevice
+                                    + " for the device: "
+                                    + device);
+                } else {
+                    Log.e(
+                            TAG,
+                            "Unexpected error while removing old bond record:"
+                                    + existingDevice
+                                    + " for the device: "
+                                    + device);
+                }
+                break;
             }
-            break;
         }
     }
 