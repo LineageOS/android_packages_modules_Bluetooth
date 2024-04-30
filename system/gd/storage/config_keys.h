miyu-xu/packages-modules-Bluetooth

Renaming HOGP specific Keys
Bug: 335366054
Test: mmm packages/modules/Bluetooth
Flag: EXEMPT code clean up
Change-Id: I763596f967ed4850f92dd7a482845d6ac0534ab9

system/gd/storage/config_keys.h
@@ -58,8 +58,6 @@
 #define BTIF_STORAGE_KEY_HID_DESCRIPTOR "HidDescriptor"
 #define BTIF_STORAGE_KEY_HID_DEVICE_CABLED "HidDeviceCabled"
 #define BTIF_STORAGE_KEY_HID_PRODUCT_ID "HidProductId"
-#define BTIF_STORAGE_KEY_HID_REPORT "HidReport"
-#define BTIF_STORAGE_KEY_HID_REPORT_VERSION "HidReportVersion"
 #define BTIF_STORAGE_KEY_HID_SSR_MAX_LATENCY "HidSSRMaxLatency"
 #define BTIF_STORAGE_KEY_HID_SSR_MIN_TIMEOUT "HidSSRMinTimeout"
 #define BTIF_STORAGE_KEY_HID_SUB_CLASS "HidSubClass"
@@ -67,6 +65,8 @@
 #define BTIF_STORAGE_KEY_HID_VERSION "HidVersion"
 #define BTIF_STORAGE_KEY_HID_DB_VERSION "HidDbVersion"
 #define BTIF_STORAGE_KEY_HID_RECONNECT_ALLOWED "HidReConnectAllowed"
+#define BTIF_STORAGE_KEY_HOGP_REPORT "HogpReport"
+#define BTIF_STORAGE_KEY_HOGP_REPORT_VERSION "HogpReportVersion"
 #define BTIF_STORAGE_KEY_HOGP_RECONNECT_ALLOWED "HogpReConnectAllowed"
 #define BTIF_STORAGE_KEY_HOGP_APP_ID "HogpAppId"
 #define BTIF_STORAGE_KEY_HOGP_ATTR_MASK "HogpAttrMask"

LineageOS/android_packages_modules_Bluetooth

system/gd/storage/config_keys.h
@@ -58,8 +58,6 @@
 #define BTIF_STORAGE_KEY_HID_DESCRIPTOR "HidDescriptor"
 #define BTIF_STORAGE_KEY_HID_DEVICE_CABLED "HidDeviceCabled"
 #define BTIF_STORAGE_KEY_HID_PRODUCT_ID "HidProductId"
-#define BTIF_STORAGE_KEY_HID_REPORT "HidReport"
-#define BTIF_STORAGE_KEY_HID_REPORT_VERSION "HidReportVersion"
 #define BTIF_STORAGE_KEY_HID_SSR_MAX_LATENCY "HidSSRMaxLatency"
 #define BTIF_STORAGE_KEY_HID_SSR_MIN_TIMEOUT "HidSSRMinTimeout"
 #define BTIF_STORAGE_KEY_HID_SUB_CLASS "HidSubClass"
@@ -67,6 +65,8 @@
 #define BTIF_STORAGE_KEY_HID_VERSION "HidVersion"
 #define BTIF_STORAGE_KEY_HID_DB_VERSION "HidDbVersion"
 #define BTIF_STORAGE_KEY_HID_RECONNECT_ALLOWED "HidReConnectAllowed"
+#define BTIF_STORAGE_KEY_HOGP_REPORT "HidReport"
+#define BTIF_STORAGE_KEY_HOGP_REPORT_VERSION "HidReportVersion"
 #define BTIF_STORAGE_KEY_HOGP_RECONNECT_ALLOWED "HogpReConnectAllowed"
 #define BTIF_STORAGE_KEY_HOGP_APP_ID "HogpAppId"
 #define BTIF_STORAGE_KEY_HOGP_ATTR_MASK "HogpAttrMask"