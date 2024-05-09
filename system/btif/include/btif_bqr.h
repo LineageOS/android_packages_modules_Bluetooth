32983d7d1d13155d839fa047f55ca97fcff49e05
Sam Chiu <samchiu@google.com>
2024-05-09 06:43:17 +0000

    Revert "Add tests for BQR Events"
    Revert submission 3074206
    
    Reason for revert: To verify the test failure b/339556159
    
    Reverted changes: /q/submissionid:3074206
    
    Change-Id: Ib1c3fb30d954afd2c5b2581a47af904ee85f6dde

--- system/btif/include/btif_bqr.h
+++ system/btif/include/btif_bqr.h
@@ -20,8 +20,7 @@
 #include <bluetooth/log.h>
 
 #include "btm_api_types.h"
-#include "common/postable_context.h"
-#include "hci/hci_packets.h"
+#include "common/leaky_bonded_queue.h"
 #include "include/hardware/bt_bqr.h"
 #include "osi/include/osi.h"
 #include "raw_address.h"
@@ -413,8 +412,9 @@ std::string PacketTypeToString(uint8_t packet_type);
 // And the minimum time interval of quality event reporting depends on the
 // setting of property "persist.bluetooth.bqr.min_interval_ms".
 //
-// @param to_bind gives the postable for the callback, or null if disabling.
-void EnableBtQualityReport(common::PostableContext* to_bind);
+// @param is_enable True/False to enable/disable Bluetooth Quality Report
+//   mechanism in the Bluetooth controller.
+void EnableBtQualityReport(bool is_enable);
 
 // Configure Bluetooth Quality Report setting to the Bluetooth controller.
 //
@@ -426,7 +426,7 @@ void ConfigureBqr(const BqrConfiguration& bqr_config);
 //
 // @param p_vsc_cmpl_params A pointer to the parameters contained in the vendor
 //   specific command complete event.
-void BqrVscCompleteCallback(hci::CommandCompleteView complete);
+void BqrVscCompleteCallback(tBTM_VSC_CMPL* p_vsc_cmpl_params);
 
 // Invoked on completion of Bluetooth Quality Report configuration. Then it will
 // Register/Unregister for receiving VSE - Bluetooth Quality Report sub-event.
