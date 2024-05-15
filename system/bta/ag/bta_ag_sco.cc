da7c3d4c0f8a16afe7041ee13b0cb895707a7e77
Chris Manton <cmanton@google.com>
2024-05-14 17:19:43 -0700

    Check sco internal API return codes
    Bug: 340680275
    Test: m .
    Flag: EXEMPT, Logging change
    
    Change-Id: Iefd7c693a08f2371919b21d07bf772b0204de4b5

--- system/bta/ag/bta_ag_sco.cc
+++ system/bta/ag/bta_ag_sco.cc
@@ -31,6 +31,7 @@
 #include "audio_hal_interface/hfp_client_interface.h"
 #include "bta/ag/bta_ag_int.h"
 #include "bta_ag_swb_aptx.h"
+#include "btm_status.h"
 #include "common/init_flags.h"
 #include "hci/controller_interface.h"
 #include "internal_include/bt_target.h"
@@ -42,6 +43,7 @@
 #include "stack/btm/btm_sco.h"
 #include "stack/btm/btm_sco_hfp_hal.h"
 #include "stack/include/btm_api.h"
+#include "stack/include/btm_client_interface.h"
 #include "stack/include/main_thread.h"
 #include "types/raw_address.h"
 
@@ -174,7 +176,9 @@ static void bta_ag_sco_conn_cback(uint16_t sco_idx) {
     /* no match found; disconnect sco, init sco variables */
     bta_ag_cb.sco.p_curr_scb = nullptr;
     bta_ag_cb.sco.state = BTA_AG_SCO_SHUTDOWN_ST;
-    BTM_RemoveSco(sco_idx);
+    if (get_btm_client_interface().sco.BTM_RemoveSco(sco_idx) != BTM_SUCCESS) {
+      log::warn("Unable to remove SCO idx:{}", sco_idx);
+    }
   }
 }
 
@@ -554,7 +558,10 @@ void bta_ag_create_sco(tBTA_AG_SCB* p_scb, bool is_orig) {
   if (is_orig) {
     bta_ag_cb.sco.is_local = true;
     /* Set eSCO Mode */
-    BTM_SetEScoMode(&params);
+    if (get_btm_client_interface().sco.BTM_SetEScoMode(&params) !=
+        BTM_SUCCESS) {
+      log::warn("Unable to set ESCO mode");
+    }
     bta_ag_cb.sco.p_curr_scb = p_scb;
     /* save the current codec as sco_codec can be updated while SCO is open. */
     p_scb->inuse_codec = esco_codec;
@@ -572,7 +579,10 @@ void bta_ag_create_sco(tBTA_AG_SCB* p_scb, bool is_orig) {
         &p_scb->peer_addr, false, params.packet_types, &p_scb->sco_idx,
         bta_ag_sco_conn_cback, bta_ag_sco_disc_cback);
     if (btm_status == BTM_CMD_STARTED) {
-      BTM_RegForEScoEvts(p_scb->sco_idx, bta_ag_esco_connreq_cback);
+      if (get_btm_client_interface().sco.BTM_RegForEScoEvts(
+              p_scb->sco_idx, bta_ag_esco_connreq_cback) != BTM_SUCCESS) {
+        log::warn("Unable to register for ESCO events");
+      }
     }
     log::debug("Listening AG SCO inx 0x{:04x} status:{} pkt types 0x{:04x}",
                p_scb->sco_idx, btm_status_text(btm_status),
