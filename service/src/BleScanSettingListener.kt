876ebbd48c69e7de2a2521b9e924af3359940005
William Escande <wescande@google.com>
2024-05-21 15:20:34 -0700

    SystemServer: Respect ble scan mode
    Bug: 341783936
    Test: atest ServiceBluetoothRoboTests
    Flag: com.android.bluetooth.flags.respect_ble_scan_setting
    Change-Id: I6dcfaebfc8094db0fdc378cd22562d4238dcbf64

---
+++ service/src/BleScanSettingListener.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// @file:JvmName("BleScanSettingListener")
+
+package com.android.server.bluetooth
+
+import android.content.ContentResolver
+import android.database.ContentObserver
+import android.os.Handler
+import android.os.Looper
+import android.provider.Settings
+
+private const val TAG = "BleScanSettingListener"
+
+object BleScanSettingListener {
+    @JvmStatic
+    var isScanAllowed = false
+        private set
+
+    /**
+     * Listen on Ble Scan setting and trigger the callback when scanning is no longer enabled
+     *
+     * @param callback: The callback to trigger when there is a mode change, pass new mode as
+     *   parameter
+     * @return The initial value of the radio
+     */
+    @JvmStatic
+    fun initialize(looper: Looper, resolver: ContentResolver, callback: () -> Unit) {
+        val notifyForDescendants = false
+
+        resolver.registerContentObserver(
+            Settings.Global.getUriFor(Settings.Global.BLE_SCAN_ALWAYS_AVAILABLE),
+            notifyForDescendants,
+            object : ContentObserver(Handler(looper)) {
+                override fun onChange(selfChange: Boolean) {
+                    isScanAllowed = getScanSettingValue(resolver)
+                    if (isScanAllowed) {
+                        Log.i(TAG, "Ble Scan mode is now allowed. Nothing to do")
+                        return
+                    } else {
+                        Log.i(TAG, "Trigger callback to disable BLE_ONLY mode")
+                        callback()
+                    }
+                }
+            }
+        )
+        isScanAllowed = getScanSettingValue(resolver)
+    }
+
+    /**
+     * Check if Bluetooth is impacted by the radio and fetch global mode status
+     *
+     * @return whether Bluetooth should consider this radio or not
+     */
+    private fun getScanSettingValue(resolver: ContentResolver): Boolean {
+        return Settings.Global.getInt(resolver, Settings.Global.BLE_SCAN_ALWAYS_AVAILABLE) != 0
+    }
+}
---
+++ service/src/BleScanSettingListenerTest.kt
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.server.bluetooth.test
+
+import android.content.ContentResolver
+import android.content.Context
+import android.os.Looper
+import android.provider.Settings
+import androidx.test.core.app.ApplicationProvider
+import com.android.server.bluetooth.BleScanSettingListener.initialize
+import com.android.server.bluetooth.BleScanSettingListener.isScanAllowed
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows.shadowOf
+
+@RunWith(RobolectricTestRunner::class)
+class BleScanSettingListenerTest {
+    private val resolver: ContentResolver =
+        ApplicationProvider.getApplicationContext<Context>().getContentResolver()
+
+    private val looper: Looper = Looper.getMainLooper()
+
+    private var callbackTriggered: Boolean = false
+
+    @Before
+    public fun setup() {
+        callbackTriggered = false
+        enableSetting()
+    }
+
+    private fun startListener() {
+        initialize(looper, resolver, this::callback)
+    }
+
+    private fun enableSetting() {
+        Settings.Global.putInt(resolver, Settings.Global.BLE_SCAN_ALWAYS_AVAILABLE, 1)
+        shadowOf(looper).idle()
+    }
+
+    private fun disableSetting() {
+        Settings.Global.putInt(resolver, Settings.Global.BLE_SCAN_ALWAYS_AVAILABLE, 0)
+        shadowOf(looper).idle()
+    }
+
+    private fun callback() {
+        callbackTriggered = true
+    }
+
+    @Test
+    fun initialize_whenSettingsOff_isOff() {
+        disableSetting()
+
+        startListener()
+
+        assertThat(isScanAllowed).isFalse()
+        assertThat(callbackTriggered).isFalse()
+    }
+
+    @Test
+    fun initialize_whenSettingsOn_isScanAllowed() {
+        startListener()
+
+        assertThat(isScanAllowed).isTrue()
+        assertThat(callbackTriggered).isFalse()
+    }
+
+    @Test
+    fun changeModeToOn_whenSettingsOn_isScanAllowedAndEventDiscarded() {
+        startListener()
+
+        enableSetting()
+
+        assertThat(isScanAllowed).isTrue()
+        assertThat(callbackTriggered).isFalse()
+    }
+
+    @Test
+    fun changeModeToOff_whenSettingsOff_isOffAndEventDiscarded() {
+        disableSetting()
+        startListener()
+
+        disableSetting()
+
+        assertThat(isScanAllowed).isFalse()
+        assertThat(callbackTriggered).isFalse()
+    }
+
+    @Test
+    fun changeModeToOn_whenSettingsOff_isScanAllowedWithoutCallback() {
+        disableSetting()
+        startListener()
+
+        enableSetting()
+
+        assertThat(isScanAllowed).isTrue()
+        assertThat(callbackTriggered).isFalse()
+    }
+
+    @Test
+    fun changeModeToOff_whenSettingsOn_isOffAndCallbackTriggered() {
+        startListener()
+
+        disableSetting()
+
+        assertThat(isScanAllowed).isFalse()
+        assertThat(callbackTriggered).isTrue()
+    }
+}
