miyu-xu/packages-modules-Bluetooth

CS: Read vendor specific characteristics from RAS client
Bug: 338259287
Bug: 324185011
Test: m com.android.btservices
Change-Id: I704d113da321661ad0c378ebd0ceee07d9145e7b

system/bta/ras/ras_client.cc
@@ -27,6 +27,7 @@ using namespace bluetooth;
 using namespace ::ras;
 using namespace ::ras::feature;
 using namespace ::ras::uuid;
+using bluetooth::ras::VendorSpecificCharacteristic;
 
 namespace {
 
@@ -45,6 +46,7 @@ class RasClientImpl : public bluetooth::ras::RasClient {
     uint32_t remote_supported_features_;
     uint16_t latest_ranging_counter_ = 0;
     bool handling_on_demand_data_ = false;
+    std::vector<VendorSpecificCharacteristic> vendor_specific_characteristics_;
 
     const gatt::Characteristic* FindCharacteristicByUuid(Uuid uuid) {
       for (auto& characteristic : service_->characteristics) {
@@ -57,6 +59,16 @@ class RasClientImpl : public bluetooth::ras::RasClient {
     const gatt::Characteristic* FindCharacteristicByHandle(uint16_t handle) {
       for (auto& characteristic : service_->characteristics) {
         if (characteristic.value_handle == handle) {
+          return &characteristic;
+        }
+      }
+      return nullptr;
+    }
+
+    VendorSpecificCharacteristic* GetVendorSpecificCharacteristic(
+        const bluetooth::Uuid& uuid) {
+      for (auto& characteristic : vendor_specific_characteristics_) {
+        if (characteristic.characteristicUuid_ == uuid) {
           return &characteristic;
         }
       }
@@ -96,6 +108,33 @@ class RasClientImpl : public bluetooth::ras::RasClient {
           std::make_shared<RasTracker>(ble_bd_addr.bda, address));
     }
     BTA_GATTC_Open(gatt_if_, ble_bd_addr.bda, BTM_BLE_DIRECT_CONNECTION, false);
+  }
+
+  void SendVendorSpecificReply(
+      const RawAddress& address,
+      const std::vector<VendorSpecificCharacteristic>& vendor_specific_data) {
+    tBLE_BD_ADDR ble_bd_addr;
+    ResolveAddress(ble_bd_addr, address);
+    log::info("address {}, resolve {}", address, ble_bd_addr.bda);
+    auto tracker = FindTrackerByAddress(ble_bd_addr.bda);
+
+    uint8_t index = 1;
+    for (auto& vendor_specific_characteristic : vendor_specific_data) {
+      auto characteristic = tracker->FindCharacteristicByUuid(
+          vendor_specific_characteristic.characteristicUuid_);
+      if (characteristic == nullptr) {
+        log::warn("Can't find characteristic uuid {}",
+                  vendor_specific_characteristic.characteristicUuid_);
+        return;
+      }
+      log::info("write to remote, uuid {}, len {}",
+                vendor_specific_characteristic.characteristicUuid_,
+                vendor_specific_characteristic.value_.size());
+      BTA_GATTC_WriteCharValue(tracker->conn_id_, characteristic->value_handle,
+                               GATT_WRITE,
+                               vendor_specific_characteristic.value_,
+                               GATT_AUTH_REQ_MITM, GattWriteCallback, nullptr);
+    }
   }
 
   void GattcCallback(tBTA_GATTC_EVT event, tBTA_GATTC* p_data) {
@@ -167,7 +206,27 @@ class RasClientImpl : public bluetooth::ras::RasClient {
       return;
     } else {
       log::info("Found Ranging Service");
-      ListCharacteristic(tracker->service_);
+      ListCharacteristic(tracker);
+    }
+
+    // Read Vendor Specific Uuid
+    if (!tracker->vendor_specific_characteristics_.empty()) {
+      for (auto& vendor_specific_characteristic :
+           tracker->vendor_specific_characteristics_) {
+        log::info("Read vendor specific characteristic uuid {}",
+                  vendor_specific_characteristic.characteristicUuid_);
+        auto characteristic = tracker->FindCharacteristicByUuid(
+            vendor_specific_characteristic.characteristicUuid_);
+
+        BTA_GATTC_ReadCharacteristic(
+            tracker->conn_id_, characteristic->value_handle, GATT_AUTH_REQ_MITM,
+            [](uint16_t conn_id, tGATT_STATUS status, uint16_t handle,
+               uint16_t len, uint8_t* value, void* data) {
+              instance->OnReadCharacteristicCallback(conn_id, status, handle,
+                                                     len, value, data);
+            },
+            nullptr);
+      }
     }
 
     // Read Ras Features
@@ -390,15 +449,26 @@ class RasClientImpl : public bluetooth::ras::RasClient {
               gatt_status_text(status));
   }
 
-  void ListCharacteristic(const gatt::Service* service) {
-    for (auto& characteristic : service->characteristics) {
+  void ListCharacteristic(std::shared_ptr<RasTracker> tracker) {
+    for (auto& characteristic : tracker->service_->characteristics) {
+      bool vendor_specific =
+          !IsRangingServiceCharacteristic(characteristic.uuid);
       log::info(
-          "Characteristic uuid:0x{:04x}, handle:0x{:04x}, properties:0x{:02x}, "
+          "{}Characteristic uuid:0x{:04x}, handle:0x{:04x}, "
+          "properties:0x{:02x}, "
           "{}",
+          vendor_specific ? "Vendor Specific" : "",
           characteristic.uuid.As16Bit(), characteristic.value_handle,
           characteristic.properties, getUuidName(characteristic.uuid));
+      if (vendor_specific) {
+        VendorSpecificCharacteristic vendor_specific_characteristic;
+        vendor_specific_characteristic.characteristicUuid_ =
+            characteristic.uuid;
+        tracker->vendor_specific_characteristics_.emplace_back(
+            vendor_specific_characteristic);
+      }
       for (auto& descriptor : characteristic.descriptors) {
-        log::info("\tDescriptor uuid: 0x{:04x}, handle:{}, {}",
+        log::info("\tDescriptor uuid:0x{:04x}, handle:0x{:04x}, {}",
                   descriptor.uuid.As16Bit(), descriptor.handle,
                   getUuidName(descriptor.uuid));
       }
@@ -430,6 +500,17 @@ class RasClientImpl : public bluetooth::ras::RasClient {
       return;
     }
 
+    auto vendor_specific_characteristic =
+        tracker->GetVendorSpecificCharacteristic(characteristic->uuid);
+    if (vendor_specific_characteristic != nullptr) {
+      log::info("Update vendor specific data, uuid: {}",
+                vendor_specific_characteristic->characteristicUuid_);
+      vendor_specific_characteristic->value_.clear();
+      vendor_specific_characteristic->value_.reserve(len);
+      vendor_specific_characteristic->value_.assign(value, value + len);
+      return;
+    }
+
     uint16_t uuid_16bit = characteristic->uuid.As16Bit();
     log::info("Handle uuid 0x{:04x}, {}", uuid_16bit,
               getUuidName(characteristic->uuid));
@@ -455,6 +536,12 @@ class RasClientImpl : public bluetooth::ras::RasClient {
           SubscribeCharacteristic(tracker,
                                   kRasRangingDataOverWrittenCharacteristic);
         }
+        uint16_t att_handle = tracker
+                                  ->FindCharacteristicByUuid(
+                                      kRasRealTimeRangingDataCharacteristic)
+                                  ->value_handle;
+        callbacks_->OnConnected(tracker->address_for_cs_, att_handle,
+                                tracker->vendor_specific_characteristics_);
       } break;
       default:
         log::warn("Unexpected UUID");

system/gd/hci/distance_measurement_manager.cc
@@ -69,7 +69,7 @@ static constexpr uint8_t kProcedureDataBufferSize = 0x10;  // Buffer size of Pro
 static constexpr uint16_t kMtuForRasData = 507;            // 512 - 5
 static constexpr uint16_t kRangingCounterMask = 0x0FFF;
 
-struct DistanceMeasurementManager::impl {
+struct DistanceMeasurementManager::impl : bluetooth::hal::RangingHalCallback {
   struct CsProcedureData {
     CsProcedureData(
         uint16_t procedure_counter,
@@ -135,6 +135,37 @@ struct DistanceMeasurementManager::impl {
     uint8_t ras_subevent_counter_ = 0;
   };
 
+  void OnOpened(
+      uint16_t connection_handle,
+      const std::vector<bluetooth::hal::VendorSpecificCharacteristic>& vendor_specific_reply) {
+    log::info(
+        "connection_handle:0x{:04x}, vendor_specific_reply size:{}",
+        connection_handle,
+        vendor_specific_reply.size());
+    if (cs_trackers_.find(connection_handle) == cs_trackers_.end()) {
+      log::error("Can't find CS tracker for connection_handle {}", connection_handle);
+      distance_measurement_callbacks_->OnDistanceMeasurementStartFail(
+          cs_trackers_[connection_handle].address, REASON_INTERNAL_ERROR, METHOD_CS);
+      return;
+    }
+
+    auto& tracker = cs_trackers_[connection_handle];
+    if (!vendor_specific_reply.empty()) {
+      // Send reply to remote
+      distance_measurement_callbacks_->OnVendorSpecificReply(
+          tracker.address, vendor_specific_reply);
+      return;
+    }
+
+    start_distance_measurement_with_cs(tracker.address, connection_handle, tracker.interval_ms);
+  }
+
+  void OnOpenFailed(uint16_t connection_handle) {
+    log::info("connection_handle:0x{:04x}", connection_handle);
+    distance_measurement_callbacks_->OnDistanceMeasurementStartFail(
+        cs_trackers_[connection_handle].address, REASON_INTERNAL_ERROR, METHOD_CS);
+  }
+
   ~impl() {}
   void start(
       os::Handler* handler,
@@ -157,6 +188,9 @@ struct DistanceMeasurementManager::impl {
     distance_measurement_interface_->EnqueueCommand(
         LeCsReadLocalSupportedCapabilitiesBuilder::Create(),
         handler_->BindOnceOn(this, &impl::on_cs_read_local_supported_capabilities));
+    if (ranging_hal_->IsBound()) {
+      ranging_hal_->RegisterCallback(this);
+    };
   }
 
   void stop() {
@@ -165,9 +199,9 @@ struct DistanceMeasurementManager::impl {
 
   void register_distance_measurement_callbacks(DistanceMeasurementCallbacks* callbacks) {
     distance_measurement_callbacks_ = callbacks;
-    if (ranging_hal_->isBound()) {
+    if (ranging_hal_->IsBound()) {
       distance_measurement_callbacks_->OnVendorSpecificCharacteristics(
-          ranging_hal_->getVendorSpecificCharacteristics());
+          ranging_hal_->GetVendorSpecificCharacteristics());
     }
   }
 
@@ -235,6 +269,12 @@ struct DistanceMeasurementManager::impl {
     cs_trackers_[connection_handle].interval_ms = interval;
     cs_trackers_[connection_handle].waiting_for_start_callback = true;
 
+    if (!cs_trackers_[connection_handle].ras_connected) {
+      log::info("Waiting for RAS connected");
+      // Waiting for ras connected.
+      return;
+    }
+
     if (!cs_trackers_[connection_handle].setup_complete) {
       send_le_cs_read_remote_supported_capabilities(connection_handle);
       return;
@@ -286,6 +326,37 @@ struct DistanceMeasurementManager::impl {
         }
       } break;
     }
+  }
+
+  void handle_ras_connected_event(
+      const Address address,
+      uint16_t att_handle,
+      const std::vector<hal::VendorSpecificCharacteristic> vendor_specific_data) {
+    uint16_t connection_handle = acl_manager_->HACK_GetLeHandle(address);
+    log::info(
+        "address:{}, connection_handle 0x{:04x}, att_handle 0x{:04x}, size of "
+        "vendor_specific_data {}",
+        address.ToString().c_str(),
+        connection_handle,
+        att_handle,
+        vendor_specific_data.size());
+
+    if (cs_trackers_.find(connection_handle) == cs_trackers_.end()) {
+      log::warn("can't find tracker for 0x{:04x}", connection_handle);
+      return;
+    }
+    auto& tracker = cs_trackers_[connection_handle];
+    if (tracker.ras_connected) {
+      log::info("Already connected");
+      return;
+    }
+    tracker.ras_connected = true;
+
+    if (ranging_hal_->IsBound()) {
+      ranging_hal_->OpenSession(connection_handle, att_handle, vendor_specific_data);
+      return;
+    }
+    start_distance_measurement_with_cs(tracker.address, connection_handle, tracker.interval_ms);
   }
 
   void send_read_rssi(const Address& address) {
@@ -1364,6 +1435,7 @@ struct DistanceMeasurementManager::impl {
     uint16_t local_counter;
     uint16_t remote_counter;
     CsRole role;
+    bool ras_connected = false;
     bool setup_complete = false;
     bool config_set = false;
     CsMainModeType main_mode_type;
@@ -1440,6 +1512,14 @@ void DistanceMeasurementManager::StartDistanceMeasurement(
 void DistanceMeasurementManager::StopDistanceMeasurement(
     const Address& address, DistanceMeasurementMethod method) {
   CallOn(pimpl_.get(), &impl::stop_distance_measurement, address, method);
+}
+
+void DistanceMeasurementManager::HandleRasConnectedEvent(
+    const Address& address,
+    uint16_t att_handle,
+    const std::vector<hal::VendorSpecificCharacteristic>& vendor_specific_data) {
+  CallOn(
+      pimpl_.get(), &impl::handle_ras_connected_event, address, att_handle, vendor_specific_data);
 }
 
 void DistanceMeasurementManager::HandleRemoteData(

LineageOS/android_packages_modules_Bluetooth

system/bta/ras/ras_client.cc
@@ -27,6 +27,7 @@ using namespace bluetooth;
 using namespace ::ras;
 using namespace ::ras::feature;
 using namespace ::ras::uuid;
+using bluetooth::ras::VendorSpecificCharacteristic;
 
 namespace {
 
@@ -45,6 +46,7 @@ class RasClientImpl : public bluetooth::ras::RasClient {
     uint32_t remote_supported_features_;
     uint16_t latest_ranging_counter_ = 0;
     bool handling_on_demand_data_ = false;
+    std::vector<VendorSpecificCharacteristic> vendor_specific_characteristics_;
 
     const gatt::Characteristic* FindCharacteristicByUuid(Uuid uuid) {
       for (auto& characteristic : service_->characteristics) {
@@ -57,6 +59,16 @@ class RasClientImpl : public bluetooth::ras::RasClient {
     const gatt::Characteristic* FindCharacteristicByHandle(uint16_t handle) {
       for (auto& characteristic : service_->characteristics) {
         if (characteristic.value_handle == handle) {
+          return &characteristic;
+        }
+      }
+      return nullptr;
+    }
+
+    VendorSpecificCharacteristic* GetVendorSpecificCharacteristic(
+        const bluetooth::Uuid& uuid) {
+      for (auto& characteristic : vendor_specific_characteristics_) {
+        if (characteristic.characteristicUuid_ == uuid) {
           return &characteristic;
         }
       }
@@ -96,6 +108,33 @@ class RasClientImpl : public bluetooth::ras::RasClient {
           std::make_shared<RasTracker>(ble_bd_addr.bda, address));
     }
     BTA_GATTC_Open(gatt_if_, ble_bd_addr.bda, BTM_BLE_DIRECT_CONNECTION, false);
+  }
+
+  void SendVendorSpecificReply(
+      const RawAddress& address,
+      const std::vector<VendorSpecificCharacteristic>& vendor_specific_data) {
+    tBLE_BD_ADDR ble_bd_addr;
+    ResolveAddress(ble_bd_addr, address);
+    log::info("address {}, resolve {}", address, ble_bd_addr.bda);
+    auto tracker = FindTrackerByAddress(ble_bd_addr.bda);
+
+    uint8_t index = 1;
+    for (auto& vendor_specific_characteristic : vendor_specific_data) {
+      auto characteristic = tracker->FindCharacteristicByUuid(
+          vendor_specific_characteristic.characteristicUuid_);
+      if (characteristic == nullptr) {
+        log::warn("Can't find characteristic uuid {}",
+                  vendor_specific_characteristic.characteristicUuid_);
+        return;
+      }
+      log::debug("write to remote, uuid {}, len {}",
+                 vendor_specific_characteristic.characteristicUuid_,
+                 vendor_specific_characteristic.value_.size());
+      BTA_GATTC_WriteCharValue(tracker->conn_id_, characteristic->value_handle,
+                               GATT_WRITE,
+                               vendor_specific_characteristic.value_,
+                               GATT_AUTH_REQ_MITM, GattWriteCallback, nullptr);
+    }
   }
 
   void GattcCallback(tBTA_GATTC_EVT event, tBTA_GATTC* p_data) {
@@ -167,7 +206,27 @@ class RasClientImpl : public bluetooth::ras::RasClient {
       return;
     } else {
       log::info("Found Ranging Service");
-      ListCharacteristic(tracker->service_);
+      ListCharacteristic(tracker);
+    }
+
+    // Read Vendor Specific Uuid
+    if (!tracker->vendor_specific_characteristics_.empty()) {
+      for (auto& vendor_specific_characteristic :
+           tracker->vendor_specific_characteristics_) {
+        log::debug("Read vendor specific characteristic uuid {}",
+                   vendor_specific_characteristic.characteristicUuid_);
+        auto characteristic = tracker->FindCharacteristicByUuid(
+            vendor_specific_characteristic.characteristicUuid_);
+
+        BTA_GATTC_ReadCharacteristic(
+            tracker->conn_id_, characteristic->value_handle, GATT_AUTH_REQ_MITM,
+            [](uint16_t conn_id, tGATT_STATUS status, uint16_t handle,
+               uint16_t len, uint8_t* value, void* data) {
+              instance->OnReadCharacteristicCallback(conn_id, status, handle,
+                                                     len, value, data);
+            },
+            nullptr);
+      }
     }
 
     // Read Ras Features
@@ -390,15 +449,26 @@ class RasClientImpl : public bluetooth::ras::RasClient {
               gatt_status_text(status));
   }
 
-  void ListCharacteristic(const gatt::Service* service) {
-    for (auto& characteristic : service->characteristics) {
+  void ListCharacteristic(std::shared_ptr<RasTracker> tracker) {
+    for (auto& characteristic : tracker->service_->characteristics) {
+      bool vendor_specific =
+          !IsRangingServiceCharacteristic(characteristic.uuid);
       log::info(
-          "Characteristic uuid:0x{:04x}, handle:0x{:04x}, properties:0x{:02x}, "
+          "{}Characteristic uuid:0x{:04x}, handle:0x{:04x}, "
+          "properties:0x{:02x}, "
           "{}",
+          vendor_specific ? "Vendor Specific " : "",
           characteristic.uuid.As16Bit(), characteristic.value_handle,
           characteristic.properties, getUuidName(characteristic.uuid));
+      if (vendor_specific) {
+        VendorSpecificCharacteristic vendor_specific_characteristic;
+        vendor_specific_characteristic.characteristicUuid_ =
+            characteristic.uuid;
+        tracker->vendor_specific_characteristics_.emplace_back(
+            vendor_specific_characteristic);
+      }
       for (auto& descriptor : characteristic.descriptors) {
-        log::info("\tDescriptor uuid: 0x{:04x}, handle:{}, {}",
+        log::info("\tDescriptor uuid:0x{:04x}, handle:0x{:04x}, {}",
                   descriptor.uuid.As16Bit(), descriptor.handle,
                   getUuidName(descriptor.uuid));
       }
@@ -430,6 +500,17 @@ class RasClientImpl : public bluetooth::ras::RasClient {
       return;
     }
 
+    auto vendor_specific_characteristic =
+        tracker->GetVendorSpecificCharacteristic(characteristic->uuid);
+    if (vendor_specific_characteristic != nullptr) {
+      log::info("Update vendor specific data, uuid: {}",
+                vendor_specific_characteristic->characteristicUuid_);
+      vendor_specific_characteristic->value_.clear();
+      vendor_specific_characteristic->value_.reserve(len);
+      vendor_specific_characteristic->value_.assign(value, value + len);
+      return;
+    }
+
     uint16_t uuid_16bit = characteristic->uuid.As16Bit();
     log::info("Handle uuid 0x{:04x}, {}", uuid_16bit,
               getUuidName(characteristic->uuid));
@@ -455,6 +536,12 @@ class RasClientImpl : public bluetooth::ras::RasClient {
           SubscribeCharacteristic(tracker,
                                   kRasRangingDataOverWrittenCharacteristic);
         }
+        uint16_t att_handle = tracker
+                                  ->FindCharacteristicByUuid(
+                                      kRasRealTimeRangingDataCharacteristic)
+                                  ->value_handle;
+        callbacks_->OnConnected(tracker->address_for_cs_, att_handle,
+                                tracker->vendor_specific_characteristics_);
       } break;
       default:
         log::warn("Unexpected UUID");

system/gd/hci/distance_measurement_manager.cc
@@ -69,7 +69,7 @@ static constexpr uint8_t kProcedureDataBufferSize = 0x10;  // Buffer size of Pro
 static constexpr uint16_t kMtuForRasData = 507;            // 512 - 5
 static constexpr uint16_t kRangingCounterMask = 0x0FFF;
 
-struct DistanceMeasurementManager::impl {
+struct DistanceMeasurementManager::impl : bluetooth::hal::RangingHalCallback {
   struct CsProcedureData {
     CsProcedureData(
         uint16_t procedure_counter,
@@ -135,6 +135,37 @@ struct DistanceMeasurementManager::impl {
     uint8_t ras_subevent_counter_ = 0;
   };
 
+  void OnOpened(
+      uint16_t connection_handle,
+      const std::vector<bluetooth::hal::VendorSpecificCharacteristic>& vendor_specific_reply) {
+    log::info(
+        "connection_handle:0x{:04x}, vendor_specific_reply size:{}",
+        connection_handle,
+        vendor_specific_reply.size());
+    if (cs_trackers_.find(connection_handle) == cs_trackers_.end()) {
+      log::error("Can't find CS tracker for connection_handle {}", connection_handle);
+      distance_measurement_callbacks_->OnDistanceMeasurementStartFail(
+          cs_trackers_[connection_handle].address, REASON_INTERNAL_ERROR, METHOD_CS);
+      return;
+    }
+
+    auto& tracker = cs_trackers_[connection_handle];
+    if (!vendor_specific_reply.empty()) {
+      // Send reply to remote
+      distance_measurement_callbacks_->OnVendorSpecificReply(
+          tracker.address, vendor_specific_reply);
+      return;
+    }
+
+    start_distance_measurement_with_cs(tracker.address, connection_handle, tracker.interval_ms);
+  }
+
+  void OnOpenFailed(uint16_t connection_handle) {
+    log::info("connection_handle:0x{:04x}", connection_handle);
+    distance_measurement_callbacks_->OnDistanceMeasurementStartFail(
+        cs_trackers_[connection_handle].address, REASON_INTERNAL_ERROR, METHOD_CS);
+  }
+
   ~impl() {}
   void start(
       os::Handler* handler,
@@ -157,6 +188,9 @@ struct DistanceMeasurementManager::impl {
     distance_measurement_interface_->EnqueueCommand(
         LeCsReadLocalSupportedCapabilitiesBuilder::Create(),
         handler_->BindOnceOn(this, &impl::on_cs_read_local_supported_capabilities));
+    if (ranging_hal_->IsBound()) {
+      ranging_hal_->RegisterCallback(this);
+    };
   }
 
   void stop() {
@@ -165,9 +199,9 @@ struct DistanceMeasurementManager::impl {
 
   void register_distance_measurement_callbacks(DistanceMeasurementCallbacks* callbacks) {
     distance_measurement_callbacks_ = callbacks;
-    if (ranging_hal_->isBound()) {
+    if (ranging_hal_->IsBound()) {
       distance_measurement_callbacks_->OnVendorSpecificCharacteristics(
-          ranging_hal_->getVendorSpecificCharacteristics());
+          ranging_hal_->GetVendorSpecificCharacteristics());
     }
   }
 
@@ -235,6 +269,11 @@ struct DistanceMeasurementManager::impl {
     cs_trackers_[connection_handle].interval_ms = interval;
     cs_trackers_[connection_handle].waiting_for_start_callback = true;
 
+    if (!cs_trackers_[connection_handle].ras_connected) {
+      log::info("Waiting for RAS connected");
+      return;
+    }
+
     if (!cs_trackers_[connection_handle].setup_complete) {
       send_le_cs_read_remote_supported_capabilities(connection_handle);
       return;
@@ -286,6 +325,37 @@ struct DistanceMeasurementManager::impl {
         }
       } break;
     }
+  }
+
+  void handle_ras_connected_event(
+      const Address address,
+      uint16_t att_handle,
+      const std::vector<hal::VendorSpecificCharacteristic> vendor_specific_data) {
+    uint16_t connection_handle = acl_manager_->HACK_GetLeHandle(address);
+    log::info(
+        "address:{}, connection_handle 0x{:04x}, att_handle 0x{:04x}, size of "
+        "vendor_specific_data {}",
+        address.ToString().c_str(),
+        connection_handle,
+        att_handle,
+        vendor_specific_data.size());
+
+    if (cs_trackers_.find(connection_handle) == cs_trackers_.end()) {
+      log::warn("can't find tracker for 0x{:04x}", connection_handle);
+      return;
+    }
+    auto& tracker = cs_trackers_[connection_handle];
+    if (tracker.ras_connected) {
+      log::debug("Already connected");
+      return;
+    }
+    tracker.ras_connected = true;
+
+    if (ranging_hal_->IsBound()) {
+      ranging_hal_->OpenSession(connection_handle, att_handle, vendor_specific_data);
+      return;
+    }
+    start_distance_measurement_with_cs(tracker.address, connection_handle, tracker.interval_ms);
   }
 
   void send_read_rssi(const Address& address) {
@@ -1364,6 +1434,7 @@ struct DistanceMeasurementManager::impl {
     uint16_t local_counter;
     uint16_t remote_counter;
     CsRole role;
+    bool ras_connected = false;
     bool setup_complete = false;
     bool config_set = false;
     CsMainModeType main_mode_type;
@@ -1440,6 +1511,14 @@ void DistanceMeasurementManager::StartDistanceMeasurement(
 void DistanceMeasurementManager::StopDistanceMeasurement(
     const Address& address, DistanceMeasurementMethod method) {
   CallOn(pimpl_.get(), &impl::stop_distance_measurement, address, method);
+}
+
+void DistanceMeasurementManager::HandleRasConnectedEvent(
+    const Address& address,
+    uint16_t att_handle,
+    const std::vector<hal::VendorSpecificCharacteristic>& vendor_specific_data) {
+  CallOn(
+      pimpl_.get(), &impl::handle_ras_connected_event, address, att_handle, vendor_specific_data);
 }
 
 void DistanceMeasurementManager::HandleRemoteData(