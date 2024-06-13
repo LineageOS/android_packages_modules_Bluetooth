miyu-xu/packages-modules-Bluetooth

Floss: Replace BT_STATUS_FAIL with more descriptive metrics
Bug: 340296381
Test: m -j
Flag: Exempt, metrics-only change

Change-Id: I40dc198a441fabdec1365a5c69b8d8dd9b1321b9

system/gd/rust/linux/stack/src/socket_manager.rs
@@ -600,7 +600,7 @@ impl BluetoothSocketManager {
                 .any(|(_, v)| v.iter().any(|s| s.uuid.map_or(false, |u| u == uuid)))
             {
                 log::warn!("Service {} already exists", DisplayUuid(&uuid));
-                return SocketResult::new(BtStatus::Fail, INVALID_SOCKET_ID);
+                return SocketResult::new(BtStatus::SocketStreamError, INVALID_SOCKET_ID);
             }
         }
 
@@ -639,7 +639,7 @@ impl BluetoothSocketManager {
                     Some(v) => v,
                     None => {
                         log::debug!("Converting from file to unixstream failed");
-                        return SocketResult::new(BtStatus::Fail, INVALID_SOCKET_ID);
+                        return SocketResult::new(BtStatus::SocketStreamError, INVALID_SOCKET_ID);
                     }
                 };
 
@@ -773,7 +773,7 @@ impl BluetoothSocketManager {
             Self::wait_and_read_stream(connection_timeout, &stream, &mut channel_bytes).await;
         let channel = i32::from_ne_bytes(channel_bytes);
         if channel <= 0 {
-            status = BtStatus::Fail;
+            status = BtStatus::SocketStreamError;
         }
 
         // If we don't get a valid channel, consider the socket as closed.
@@ -971,7 +971,7 @@ impl BluetoothSocketManager {
                                             SocketActions::OnIncomingSocketReady(
                                                 cbid,
                                                 cloned_socket_info,
-                                                BtStatus::Fail,
+                                                BtStatus::SocketStreamError,
                                             ),
                                         ))
                                         .await;
@@ -1022,7 +1022,7 @@ impl BluetoothSocketManager {
                 Ok(()) => {}
                 Err(_e) => {
                     // Stream was not readable. This is usually due to some polling error.
-                    return BtStatus::Fail;
+                    return BtStatus::SocketStreamError;
                 }
             },
             Err(_) => {
@@ -1034,12 +1034,12 @@ impl BluetoothSocketManager {
         match stream.try_read(buf) {
             Ok(n) => {
                 if n != buf.len() {
-                    return BtStatus::Fail;
+                    return BtStatus::SocketStreamError;
                 }
                 return BtStatus::Success;
             }
             _ => {
-                return BtStatus::Fail;
+                return BtStatus::SocketStreamError;
             }
         }
     }
@@ -1065,7 +1065,7 @@ impl BluetoothSocketManager {
                     .send(Message::SocketManagerActions(SocketActions::OnOutgoingConnectionResult(
                         cbid,
                         socket_id,
-                        BtStatus::Fail,
+                        BtStatus::SocketStreamError,
                         None,
                     )))
                     .await;
@@ -1078,7 +1078,7 @@ impl BluetoothSocketManager {
         let mut status =
             Self::wait_and_read_stream(connection_timeout, &stream, &mut channel_bytes).await;
         if i32::from_ne_bytes(channel_bytes) <= 0 {
-            status = BtStatus::Fail;
+            status = BtStatus::SocketStreamError;
         }
         if status != BtStatus::Success {
             log::info!(
@@ -1147,7 +1147,7 @@ impl BluetoothSocketManager {
                     .send(Message::SocketManagerActions(SocketActions::OnOutgoingConnectionResult(
                         cbid,
                         socket_id,
-                        BtStatus::Fail,
+                        BtStatus::SocketStreamError,
                         None,
                     )))
                     .await;

LineageOS/android_packages_modules_Bluetooth

system/gd/rust/linux/stack/src/socket_manager.rs
@@ -600,7 +600,7 @@ impl BluetoothSocketManager {
                 .any(|(_, v)| v.iter().any(|s| s.uuid.map_or(false, |u| u == uuid)))
             {
                 log::warn!("Service {} already exists", DisplayUuid(&uuid));
-                return SocketResult::new(BtStatus::Fail, INVALID_SOCKET_ID);
+                return SocketResult::new(BtStatus::SocketError, INVALID_SOCKET_ID);
             }
         }
 
@@ -639,7 +639,7 @@ impl BluetoothSocketManager {
                     Some(v) => v,
                     None => {
                         log::debug!("Converting from file to unixstream failed");
-                        return SocketResult::new(BtStatus::Fail, INVALID_SOCKET_ID);
+                        return SocketResult::new(BtStatus::SocketError, INVALID_SOCKET_ID);
                     }
                 };
 
@@ -773,7 +773,7 @@ impl BluetoothSocketManager {
             Self::wait_and_read_stream(connection_timeout, &stream, &mut channel_bytes).await;
         let channel = i32::from_ne_bytes(channel_bytes);
         if channel <= 0 {
-            status = BtStatus::Fail;
+            status = BtStatus::SocketError;
         }
 
         // If we don't get a valid channel, consider the socket as closed.
@@ -971,7 +971,7 @@ impl BluetoothSocketManager {
                                             SocketActions::OnIncomingSocketReady(
                                                 cbid,
                                                 cloned_socket_info,
-                                                BtStatus::Fail,
+                                                BtStatus::SocketError,
                                             ),
                                         ))
                                         .await;
@@ -1022,7 +1022,7 @@ impl BluetoothSocketManager {
                 Ok(()) => {}
                 Err(_e) => {
                     // Stream was not readable. This is usually due to some polling error.
-                    return BtStatus::Fail;
+                    return BtStatus::SocketError;
                 }
             },
             Err(_) => {
@@ -1034,12 +1034,12 @@ impl BluetoothSocketManager {
         match stream.try_read(buf) {
             Ok(n) => {
                 if n != buf.len() {
-                    return BtStatus::Fail;
+                    return BtStatus::SocketError;
                 }
                 return BtStatus::Success;
             }
             _ => {
-                return BtStatus::Fail;
+                return BtStatus::SocketError;
             }
         }
     }
@@ -1065,7 +1065,7 @@ impl BluetoothSocketManager {
                     .send(Message::SocketManagerActions(SocketActions::OnOutgoingConnectionResult(
                         cbid,
                         socket_id,
-                        BtStatus::Fail,
+                        BtStatus::SocketError,
                         None,
                     )))
                     .await;
@@ -1078,7 +1078,7 @@ impl BluetoothSocketManager {
         let mut status =
             Self::wait_and_read_stream(connection_timeout, &stream, &mut channel_bytes).await;
         if i32::from_ne_bytes(channel_bytes) <= 0 {
-            status = BtStatus::Fail;
+            status = BtStatus::SocketError;
         }
         if status != BtStatus::Success {
             log::info!(
@@ -1147,7 +1147,7 @@ impl BluetoothSocketManager {
                     .send(Message::SocketManagerActions(SocketActions::OnOutgoingConnectionResult(
                         cbid,
                         socket_id,
-                        BtStatus::Fail,
+                        BtStatus::SocketError,
                         None,
                     )))
                     .await;