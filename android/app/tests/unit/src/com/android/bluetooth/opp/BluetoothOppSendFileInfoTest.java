73ef4306e91bc500769f2abbad94b61742c43602
Billy Huang <billyhuang@google.com>
2025-03-11 10:53:32 -0700

    RESTRICT AUTOMERGE parse userId from authority in opp in the same way as ContentProvider
    Ensures that userId determination is consistent with ContentProvider
    
    Bug: 395647005
    Bug: 395643490
    Flag: EXEMPT fix for vulnerability
    Test: m com.google.android.bt
    Test: atest BluetoothJavaUnitTests:com.android.bluetooth.opp.BluetoothOppSendFileInfoTest
    Ignore-AOSP-First: fix for undisclosed vulnerability
    Merged-In: I7b6affec5dada7c9f0319fbdb35a6e977e51990f
    Merged-In: I51e73cfb1e6af69226f02347773e24b69199613d
    Change-Id: Iacb9b1fc1fe48d9b8512f6e80bcc6eeb2a5bedbd

--- android/app/tests/unit/src/com/android/bluetooth/opp/BluetoothOppSendFileInfoTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/opp/BluetoothOppSendFileInfoTest.java
@@ -35,12 +35,15 @@ import android.net.Uri;
 import android.provider.OpenableColumns;
 
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
 
 import com.android.bluetooth.BluetoothMethodProxy;
 
+import com.google.testing.junit.testparameterinjector.TestParameter;
+import com.google.testing.junit.testparameterinjector.TestParameterInjector;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -49,7 +52,7 @@ import org.mockito.MockitoAnnotations;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-@RunWith(AndroidJUnit4.class)
+@RunWith(TestParameterInjector.class)
 public class BluetoothOppSendFileInfoTest {
     Context mContext;
     MatrixCursor mCursor;
@@ -122,10 +125,12 @@ public class BluetoothOppSendFileInfoTest {
     }
 
     @Test
-    public void generateFileInfo_withContentUriForOtherUser_returnsSendFileInfoError()
-            throws Exception {
+    @Ignore("TODO: b/377774165 - Use source-built BT APEX instead of prebuilt")
+    public void generateFileInfo_withContentUriForOtherUser_returnsSendFileInfoError(
+            @TestParameter boolean encodedAt) throws Exception {
         String type = "image/jpeg";
-        Uri uri = buildContentUriWithEncodedAuthority((myUserId() + 1) + "@media");
+        String authoritySuffix = encodedAt ? "%40media" : "@media";
+        Uri uri = buildContentUriWithEncodedAuthority((myUserId() + 1) + authoritySuffix);
 
         long fileLength = 1000;
         String fileName = "pic.jpg";
@@ -185,10 +190,11 @@ public class BluetoothOppSendFileInfoTest {
     }
 
     @Test
-    public void generateFileInfo_withContentUriForSameUser_returnsInfoWithCorrectLength()
-            throws Exception {
+    public void generateFileInfo_withContentUriForSameUser_returnsInfoWithCorrectLength(
+            @TestParameter boolean encodedAt) throws Exception {
         String type = "image/jpeg";
-        Uri uri = buildContentUriWithEncodedAuthority(myUserId() + "@media");
+        String authoritySuffix = encodedAt ? "%40media" : "@media";
+        Uri uri = buildContentUriWithEncodedAuthority(myUserId() + authoritySuffix);
 
         long fileLength = 1000;
         String fileName = "pic.jpg";
