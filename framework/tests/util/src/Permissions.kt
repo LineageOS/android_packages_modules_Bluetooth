500ec490a9cf684182501445efeafcdd67a0b9e3
William Escande <wescande@google.com>
2025-03-20 17:32:23 -0700

    Test permissions checker: improve error message
    Bug: 311772251
    Flag: TEST_ONLY
    Test: atest CtsBluetoothTestsCases (any test that trigger Permissions)
    Change-Id: Ie9c70eabff6144486a633476b54e7426e2c88aa1

--- framework/tests/util/src/Permissions.kt
+++ framework/tests/util/src/Permissions.kt
@@ -53,10 +53,15 @@ object Permissions {
         }
         newPermissions.forEach {
             val permissionsSet = newPermissions.toMutableSet()
-            permissionsSet.remove(it)
+            val removedPermission = it
+            permissionsSet.remove(removedPermission)
 
             withPermissions(*arrayOf(*permissionsSet.toTypedArray())).use {
-                assertThrows(SecurityException::class.java, { action() })
+                assertThrows(
+                    "SecurityException was not thrown after removing $removedPermission",
+                    SecurityException::class.java,
+                    { action() },
+                )
             }
         }
     }
