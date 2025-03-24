36c03ae369b94b38ac5a8631ffbe37f80fd8dabe
Zhengping Jiang <jiangzp@google.com>
2025-03-24 13:59:58 -0700

    24Q4: Remove flag android_os_identifier
    Bug: 351860033
    Test: m com.android.bt
    Flag: EXEMPT, flag removal
    (cherry picked from https://android-review.googlesource.com/q/commit:edb24fba442432d009a4f0b9a66efade28955100)
    Change-Id: I289981a28340cdd6b6e12082925a223cddc7e671

--- system/stack/gatt/gatt_api.cc
+++ system/stack/gatt/gatt_api.cc
@@ -329,8 +329,7 @@ tGATT_STATUS GATTS_AddService(tGATT_IF gatt_if, btgatt_db_element_t* service, in
         *p_uuid != Uuid::From16Bit(UUID_SERVCLASS_GTBS_SERVER)) {
       if ((com::android::bluetooth::flags::channel_sounding_in_stack() &&
            *p_uuid == Uuid::From16Bit(UUID_SERVCLASS_RAS)) ||
-          (com::android::bluetooth::flags::android_os_identifier() &&
-           *p_uuid == ANDROID_INFORMATION_SERVICE_UUID)) {
+          *p_uuid == ANDROID_INFORMATION_SERVICE_UUID) {
         elem.sdp_handle = 0;
       } else {
         elem.sdp_handle = gatt_add_sdp_record(*p_uuid, elem.s_hdl, elem.e_hdl);
