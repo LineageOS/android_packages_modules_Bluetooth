74b94c3682cc4ab1f481874d1ad5028920906889
Hsin-chen Chuang <chharry@google.com>
2025-04-10 10:03:36 +0800

    Remove rfcomm_always_disc_initiator_in_disc_wait_ua flag
    Bug: 350839022
    Test: m com.google.android.bt
    Flag: EXEMPT removing rfcomm_always_disc_initiator_in_disc_wait_ua
    Change-Id: If916d621a4679561d84861da923113e1f0d77573

--- system/stack/rfcomm/rfc_port_fsm.cc
+++ system/stack/rfcomm/rfc_port_fsm.cc
@@ -576,17 +576,15 @@ void rfc_port_sm_disc_wait_ua(tPORT* p_port, tRFC_PORT_EVENT event, void* p_data
 
     case RFC_PORT_EVENT_DM:
       log::warn("RFC_EVENT_DM|RFC_EVENT_UA[{}], port_handle:{}", event, p_port->handle);
-      if (com::android::bluetooth::flags::rfcomm_always_disc_initiator_in_disc_wait_ua()) {
-        // If we got a DM in RFC_STATE_DISC_WAIT_UA, it's likely that both ends
-        // attempt to DISC at the same time and both get a DM.
-        // Without setting this flag the both ends would start the same timers,
-        // wait, and still DISC the multiplexer at the same time eventually.
-        // The wait is meaningless and would block all other services that rely
-        // on RFCOMM such as HFP.
-        // Thus, setting this flag here to save us a timeout and doesn't
-        // introduce further RFCOMM event changes.
-        p_port->rfc.p_mcb->is_disc_initiator = true;
-      }
+      // If we got a DM in RFC_STATE_DISC_WAIT_UA, it's likely that both ends
+      // attempt to DISC at the same time and both get a DM.
+      // Without setting this flag the both ends would start the same timers,
+      // wait, and still DISC the multiplexer at the same time eventually.
+      // The wait is meaningless and would block all other services that rely
+      // on RFCOMM such as HFP.
+      // Thus, setting this flag here to save us a timeout and doesn't
+      // introduce further RFCOMM event changes.
+      p_port->rfc.p_mcb->is_disc_initiator = true;
       rfc_port_closed(p_port);
       return;
 
