56057ed8fe9ed890b27bef1fc44629bdd70d5239
Omair Kamil <okamil@google.com>
2025-04-15 17:04:31 -0700

    [Samsung Convergence] Remove callback from set before invoking callback.
    The callback client may invoke another action before the removal happened in the original code.
    
    Bug: 410420475
    Fix: 410420475
    Test: atest BumbleBluetoothTests
    Flag: EXEMPT, trivial fix
    Change-Id: Iebad27a0db617da58ac8d410413a1650c14921fe

--- framework/java/android/bluetooth/le/BluetoothLeAdvertiser.java
+++ framework/java/android/bluetooth/le/BluetoothLeAdvertiser.java
@@ -772,8 +772,8 @@ public final class BluetoothLeAdvertiser {
                         handler::post,
                         () -> {
                             if (status != AdvertisingSetCallback.ADVERTISE_SUCCESS) {
-                                callback.onAdvertisingSetStarted(null, 0, status);
                                 mCallbackWrappers.remove(callback);
+                                callback.onAdvertisingSetStarted(null, 0, status);
                                 return;
                             }
 
