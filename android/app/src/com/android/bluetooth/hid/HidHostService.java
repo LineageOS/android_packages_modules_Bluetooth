e4391f1054bc5c7aaa44e6c54dc78cb807d891bd
Bhakthavatsala Raghavendra <bhaktha@google.com>
2025-04-25 15:17:03 -0700

    Remove flag donot_validate_bond_state_from_profiles
    Bug: 353738134
    Test: mmm packages/module/Bluetooth
    Flag: com.android.bluetooth.flags.donot_validate_bond_state_from_profiles
    Change-Id: I9685cecf24da5a4b99c48069bab3e631ed2efc0f

--- android/app/src/com/android/bluetooth/hid/HidHostService.java
+++ android/app/src/com/android/bluetooth/hid/HidHostService.java
@@ -1167,17 +1167,6 @@ public class HidHostService extends ProfileService {
         }
         // Check connection policy and accept or reject the connection.
         int connectionPolicy = getConnectionPolicy(device);
-        if (!Flags.donotValidateBondStateFromProfiles()) {
-            int bondState = mAdapterService.getBondState(device);
-            // Allow this connection only if the device is bonded. Any attempt to connect
-            // while bonding would potentially lead to an unauthorized connection.
-            if (bondState != BluetoothDevice.BOND_BONDED) {
-                Log.w(
-                        TAG,
-                        "okToConnect: return false, device=" + device + " bondState=" + bondState);
-                return false;
-            }
-        }
         if (connectionPolicy != CONNECTION_POLICY_UNKNOWN
                 && connectionPolicy != CONNECTION_POLICY_ALLOWED) {
             // Otherwise, reject the connection if connectionPolicy is not valid.
