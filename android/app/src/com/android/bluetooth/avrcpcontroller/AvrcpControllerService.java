f76bd4d9babd6745876a011e3ffd3a4b7d84a4dc
Ömer Faruk Yılmaz <ofy@google.com>
2025-05-20 22:06:45 +0000

    Remove singleton from AvrcpControllerNativeInterface
    Set the field `AvrcpControllerService mAvrcpController` as final.
    
    This improves testability and simplifies dependency management.
    
    Bug: 418183047
    Test: atest AdapterServiceTest
    Test: atest AvrcpControllerNativeInterfaceTest
    Test: atest AvrcpControllerServiceTest
    Flag: EXEMPT refactor no-op
    Change-Id: I3ba77d5ff6ad070bd5d038675f3ae5492b92cbc4

--- android/app/src/com/android/bluetooth/avrcpcontroller/AvrcpControllerService.java
+++ android/app/src/com/android/bluetooth/avrcpcontroller/AvrcpControllerService.java
@@ -21,6 +21,7 @@ import static android.bluetooth.BluetoothProfile.STATE_CONNECTING;
 import static android.bluetooth.BluetoothProfile.STATE_DISCONNECTED;
 
 import static java.util.Objects.requireNonNull;
+import static java.util.Objects.requireNonNullElseGet;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -133,15 +134,18 @@ public class AvrcpControllerService extends ProfileService {
     }
 
     public AvrcpControllerService(AdapterService adapterService) {
-        this(adapterService, AvrcpControllerNativeInterface.getInstance(adapterService));
+        this(adapterService, null);
     }
 
     @VisibleForTesting
     public AvrcpControllerService(
             AdapterService adapterService, AvrcpControllerNativeInterface nativeInterface) {
         super(requireNonNull(adapterService));
-        mNativeInterface = requireNonNull(nativeInterface);
-        mNativeInterface.init(this);
+        mNativeInterface =
+                requireNonNullElseGet(
+                        nativeInterface,
+                        () -> new AvrcpControllerNativeInterface(adapterService, this));
+        mNativeInterface.init();
 
         setComponentAvailable(ON_ERROR_SETTINGS_ACTIVITY, true);
         mCoverArtEnabled = getResources().getBoolean(R.bool.avrcp_controller_enable_cover_art);
