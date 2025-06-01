15495c8452d0fac21f3f6d6efa0f12432fa6466b
Ömer Faruk Yılmaz <ofy@google.com>
2025-06-01 10:26:15 +0900

    Move SapService permission enforcement of `getConnectionPolicy` to Binder
    This also allows the deletion of `getConnectionPolicy` to default back
    to the same implementation in `ConnectableProfile`.
    
    Bug: 394958283
    Test: atest SapServiceTest
    Flag: EXEMPT refactor no-op
    Change-Id: I9d1660f4305069818c2ce43b28312dc687159d86

--- android/app/src/com/android/bluetooth/sap/SapService.java
+++ android/app/src/com/android/bluetooth/sap/SapService.java
@@ -647,24 +647,6 @@ public class SapService extends ConnectableProfile
         return true;
     }
 
-    /**
-     * Get the connection policy of the profile.
-     *
-     * <p>The connection policy can be any of: {@link BluetoothProfile#CONNECTION_POLICY_ALLOWED},
-     * {@link BluetoothProfile#CONNECTION_POLICY_FORBIDDEN}, {@link
-     * BluetoothProfile#CONNECTION_POLICY_UNKNOWN}
-     *
-     * @param device Bluetooth device
-     * @return connection policy of the device
-     */
-    @RequiresPermission(BLUETOOTH_PRIVILEGED)
-    @Override
-    public int getConnectionPolicy(BluetoothDevice device) {
-        enforceCallingOrSelfPermission(
-                BLUETOOTH_PRIVILEGED, "Need BLUETOOTH_PRIVILEGED permission");
-        return mDatabaseManager.getProfileConnectionPolicy(device, mProfileId);
-    }
-
     @Override
     protected IProfileServiceBinder initBinder() {
         return new SapServiceBinder(this);
