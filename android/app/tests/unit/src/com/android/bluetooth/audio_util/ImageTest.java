3b8231c1cac9870488dd18f2419667116af5da1f
Ömer Faruk Yılmaz <ofy@google.com>
2025-06-03 22:03:39 +0900

    In Unit tests, `import com.android.bluetooth.tests.R;`
    Bug: 404652736
    Test: atest BrowserPlayerWrapperTest
    Test: atest ImageTest
    Test: atest MediaPlayerWrapperTest
    Test: atest MetadataTest
    Test: atest AvrcpBipObexServerTest
    Test: atest AvrcpCoverArtStorageTest
    Test: atest CoverArtTest
    Test: atest AvrcpCoverArtStorageTest
    Test: atest BipImageTest
    Flag: EXEMPT test only
    Change-Id: Ifef23a9fbd639bca5b747b8f8a377db3effeeb0e

--- android/app/tests/unit/src/com/android/bluetooth/audio_util/ImageTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/audio_util/ImageTest.java
@@ -39,6 +39,7 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
 import com.android.bluetooth.TestUtils;
+import com.android.bluetooth.tests.R;
 
 import org.junit.After;
 import org.junit.Before;
@@ -84,7 +85,7 @@ public class ImageTest {
 
     @Before
     public void setUp() throws Exception {
-        mTestBitmap = loadImage(com.android.bluetooth.tests.R.raw.image_200_200);
+        mTestBitmap = loadImage(R.raw.image_200_200);
         mTargetContext = InstrumentationRegistry.getInstrumentation().getContext();
         mTestContentResolver = new MockContentResolver(mTargetContext);
         mTestContentResolver.addProvider(
@@ -94,8 +95,7 @@ public class ImageTest {
                     public AssetFileDescriptor openTypedAssetFile(
                             Uri url, String mimeType, Bundle opts) {
                         if (IMAGE_URI_1.equals(url)) {
-                            return mTestResources.openRawResourceFd(
-                                    com.android.bluetooth.tests.R.raw.image_200_200);
+                            return mTestResources.openRawResourceFd(R.raw.image_200_200);
                         } else if (IMAGE_URI_SECURITY_ERROR.equals(url)) {
                             throw new SecurityException();
                         }
