d3b57ab64833c58b6326eac926da78bb8fc89269
Etienne Ruffieux <eruffieux@google.com>
2025-06-23 13:07:52 +0000

    Add logging to AVRCP SDP
    There was missing logs for AVRCP SDP service discovery.
    
    Bug: 425822243
    Flag: EXEMPT Log only
    Test: m .
    Change-Id: I10dd7062b2fb77758cea332a6a0780ea3c8f30d3

--- system/stack/avrc/avrc_sdp.cc
+++ system/stack/avrc/avrc_sdp.cc
@@ -135,12 +135,14 @@ uint16_t AVRC_FindService(uint16_t service_uuid, const RawAddress& bd_addr,
   if ((service_uuid != UUID_SERVCLASS_AV_REM_CTRL_TARGET &&
        service_uuid != UUID_SERVCLASS_AV_REMOTE_CONTROL) ||
       p_db == NULL || p_db->p_db == NULL || find_cback.is_null()) {
+    log::error("Failed to find service for {}, bad parameters", bd_addr);
     return AVRC_BAD_PARAM;
   }
 
   /* check if it is busy */
   if (avrc_cb.service_uuid == UUID_SERVCLASS_AV_REM_CTRL_TARGET ||
       avrc_cb.service_uuid == UUID_SERVCLASS_AV_REMOTE_CONTROL) {
+    log::error("Failed to find service for {}, no resources", bd_addr);
     return AVRC_NO_RESOURCES;
   }
 
@@ -167,8 +169,9 @@ uint16_t AVRC_FindService(uint16_t service_uuid, const RawAddress& bd_addr,
       log::error("Failed to init SDP for peer {}", bd_addr);
       avrc_sdp_cback(bd_addr, tSDP_STATUS::SDP_GENERIC_ERROR);
     }
+  } else {
+    log::error("Failed to find service for {}, failed to init discovery DB", bd_addr);
   }
-
   return result ? AVRC_SUCCESS : AVRC_FAIL;
 }
 
