977a0d148a97d0763dd6c0e5339abec7c5e18cff
William Escande <wescande@google.com>
2025-07-14 16:28:26 -0700

    Remove duplicate information in dumpsys
    Bug: 321804999
    Flag: EXEMPT dumpsys edit
    Test: m .
    Change-Id: I51069c6268cc0383f04aa466370e04414ec0f0d1

--- service/src/com/android/server/bluetooth/BluetoothManagerService.java
+++ service/src/com/android/server/bluetooth/BluetoothManagerService.java
@@ -820,10 +820,6 @@ class BluetoothManagerService {
         mCallbacks.unregister(callback);
     }
 
-    boolean isEnabled() {
-        return getState() == State.ON;
-    }
-
     /**
      * Sends the current foreground user id to the Bluetooth process. This user id is used to
      * determine if Binder calls are coming from the active user.
@@ -2230,7 +2226,6 @@ class BluetoothManagerService {
         String errorMsg = null;
 
         writer.println("Bluetooth Status");
-        writer.println("  enabled: " + isEnabled());
         writer.println("  state: " + mState);
         writer.println("  address: " + logAddress(mAddress));
         writer.println("  name: " + mName);
