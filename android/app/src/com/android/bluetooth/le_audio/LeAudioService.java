d5b168e6b5b51c99955201328b2e36a0692d0b52
Rongxuan Liu <rongxuan@google.com>
2025-08-26 16:20:48 -0700

    Revert "[LE Broadcast] filter out null metadata while getAllBroadcastMetadata"
    This reverts commit 4c407684cae1eab760057edb310b3252ad7dc5c4.
    
    Reason for revert: Caused regression b/441174869, forward-fix will be planned separately.
    
    Change-Id: Idbf4ccb3f601015f5262868720f2a2a552515741

--- android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
+++ android/app/src/com/android/bluetooth/le_audio/LeAudioService.java
@@ -1610,7 +1610,6 @@ public class LeAudioService extends ConnectableProfile {
     public List<BluetoothLeBroadcastMetadata> getAllBroadcastMetadata() {
         return mBroadcastDescriptors.values().stream()
                 .map(s -> s.mMetadata)
-                .filter(m -> m != null)
                 .collect(Collectors.toList());
     }
 
