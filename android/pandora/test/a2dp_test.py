32186d8d9d1ab2d02d309721853d05369e92d0f2
Jakub Rotkiewicz <rotkiewicz@google.com>
2025-09-09 18:46:58 +0000

    avatar: Manually invoke A2DP connect for collision test
    DUT might not invoke A2DP profile connection automatically after
    bonding. Assure that it's started for the collision test.
    
    Bug: 329809288
    Flag: EXEMPT TEST ONLY
    Test: atest avatar:'A2dpTest#test_avdt_signaling_channel_connection_collision_case1' -v
    Change-Id: I8ff564908558afedc348f9e63e311495703b8331

--- android/pandora/test/a2dp_test.py
+++ android/pandora/test/a2dp_test.py
@@ -663,6 +663,10 @@ class A2dpTest(base_test.BaseTestClass):  # type: ignore[misc]
         pending_configuration_request: L2capConfigurationRequest | None = L2capConfigurationRequest(
         )
 
+        # Prepare a function to call when expecting DUT to connect AVDTP
+        def dut_open_source() -> None:
+            pass
+
         class TestChannelManager(ChannelManager):
 
             def __init__(
@@ -726,6 +730,7 @@ class A2dpTest(base_test.BaseTestClass):  # type: ignore[misc]
                     pending_configuration_request.connection = self.connection
                     pending_configuration_request.cid = self.source_cid
                     pending_configuration_request.request = request
+                    dut_open_source()
                 else:
                     super().on_configure_request(request)
 
@@ -736,6 +741,9 @@ class A2dpTest(base_test.BaseTestClass):  # type: ignore[misc]
             initiate_pairing(self.dut, self.ref1.address),
             accept_pairing(self.ref1, self.dut.address),
         )
+
+        def dut_open_source() -> None:
+            self.dut_a2dp.OpenSource(connection=dut_ref1)
 
         # Retrieve Bumble connection object from Pandora connection token
         connection = pandora_snippet.get_raw_connection(device=self.ref1, connection=ref1_dut)
