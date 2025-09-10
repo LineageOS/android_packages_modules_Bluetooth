5e7d2cd16f409c64210b5daa42c3771062198a71
William Escande <wescande@google.com>
2025-09-10 12:11:50 -0700

    Get profile oneway: Followup fixup of code under flag
    We need to protect connect/disconnect against multiples calls now that
    this is being triggered asynchronously.
    
    This was already the case prior to getProfileOneway flag, and it seemed
    to be the case under flag, but since everything is running on the
    callback, there is a gap where the connect method can be called twice
    
    Also cleanup profileConnection map that doesn't need to be concurrent
    since it is protected by a Guard (that was supposed to be part of flag
    cleaning)
    
    Bug: 444091484
    Test: atest CtsBluetoothTestCases
    Test: atest BluetoothHapClientTest --rerun-until-failure
    Flag: com.android.bluetooth.flags.get_profile_oneway
    Change-Id: Ia466f0979602ac8c218e671b7317e7c2b77a66df

--- framework/java/android/bluetooth/BluetoothAdapter.java
+++ framework/java/android/bluetooth/BluetoothAdapter.java
@@ -104,7 +104,6 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.WeakHashMap;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.BiFunction;
@@ -901,6 +900,10 @@ public final class BluetoothAdapter {
 
         @GuardedBy("BluetoothAdapter.sProfileLock")
         void connect(BluetoothProfile proxy, IBinder binder) {
+            if (Flags.getProfileOneway() && mConnected) {
+                Log.v(TAG, getProfileName(mProfile) + " already connected");
+                return;
+            }
             Log.d(TAG, getProfileName(mProfile) + " connected");
             mConnected = true;
             proxy.onServiceConnected(binder);
@@ -909,6 +912,10 @@ public final class BluetoothAdapter {
 
         @GuardedBy("BluetoothAdapter.sProfileLock")
         void disconnect(BluetoothProfile proxy) {
+            if (Flags.getProfileOneway() && !mConnected) {
+                Log.v(TAG, getProfileName(mProfile) + " already disconnected");
+                return;
+            }
             Log.d(TAG, getProfileName(mProfile) + " disconnected");
             mConnected = false;
             proxy.onServiceDisconnected();
@@ -919,8 +926,7 @@ public final class BluetoothAdapter {
     private static final Object sProfileLock = new Object();
 
     @GuardedBy("sProfileLock")
-    private final Map<BluetoothProfile, ProfileConnection> mProfileConnections =
-            new ConcurrentHashMap<>();
+    private final Map<BluetoothProfile, ProfileConnection> mProfileConnections = new HashMap<>();
 
     private final Handler mMainHandler = new Handler(Looper.getMainLooper());
 
