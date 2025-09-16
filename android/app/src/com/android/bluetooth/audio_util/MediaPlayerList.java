8c2661e031f6c4db7159e758ef471a9763e8b319
Etienne Ruffieux <eruffieux@google.com>
2025-09-16 07:51:00 +0000

    Ringtone should be considered audio playback.
    Bug: 441440160
    Flag: EXEMPT PURE_REFACTOR
    Test: m .
    Change-Id: I58047c52f39a9b7193d3d0b54181162f432690ed

--- android/app/src/com/android/bluetooth/audio_util/MediaPlayerList.java
+++ android/app/src/com/android/bluetooth/audio_util/MediaPlayerList.java
@@ -1096,8 +1096,10 @@ public class MediaPlayerList {
                                                                 .USAGE_ASSISTANCE_NAVIGATION_GUIDANCE)
                                                 && (config.getAudioAttributes().getContentType()
                                                         == AudioAttributes.CONTENT_TYPE_SPEECH))
-                                        || config.getAudioAttributes().getUsage()
-                                                == AudioAttributes.USAGE_MEDIA)) {
+                                        || (config.getAudioAttributes().getUsage()
+                                                == AudioAttributes.USAGE_MEDIA)
+                                        || (config.getAudioAttributes().getUsage()
+                                                == AudioAttributes.USAGE_NOTIFICATION_RINGTONE))) {
                             activeConfig = config;
                             isActive = true;
                         }
