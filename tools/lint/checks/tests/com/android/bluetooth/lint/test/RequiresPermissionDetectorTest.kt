688126d78d6c45f60ef945dd2c04840388b70188
Ömer Faruk Yılmaz <ofy@google.com>
2025-09-18 12:15:01 +0900

    RequiresPermissionDetector: Check super methods for permission annotations
    The PermissionEnforcementVisitor was only checking the currently called
    method for `@RequiresPermission` or `@EnforcePermission`. This caused
    it to miss required permissions if the annotation was defined on a
    super method (e.g., in an AIDL interface).
    
    Real example: In `HidDeviceServiceBinder` and `HidHostServiceBinder`,
    2 functions that have a `@RequiresPermission(BLUETOOTH_CONNECT)` were
    calling another public function that had the same requirement for code reuse.
    
    Bug: 409768276
    Test: atest --host BluetoothLintCheckerTest:com.android.bluetooth.lint.test.RequiresPermissionDetectorTest
    Flag: NONE LINT_ONLY
    Change-Id: Ifef9f3de7421c2dcb3d11aa190d8efadfb1009b8

--- tools/lint/checks/tests/com/android/bluetooth/lint/test/RequiresPermissionDetectorTest.kt
+++ tools/lint/checks/tests/com/android/bluetooth/lint/test/RequiresPermissionDetectorTest.kt
@@ -148,6 +148,9 @@ class RequiresPermissionDetectorTest : LintDetectorTest() {
                 override fun connect() {
                     getServiceAndEnforceConnect()
                 }
+                override fun connectSecond() {
+                    connect()
+                }
             }
             class Util(val context: Context) {
                 @android.annotation.RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)
@@ -1334,6 +1337,7 @@ class RequiresPermissionDetectorTest : LintDetectorTest() {
         public interface IFoo extends android.os.IInterface {
             public static class Default extends android.os.Binder implements IFoo {
                 @Override public void connect() {}
+                @Override public void connectSecond() {}
                 @Override public void scanAndAdvertise() {}
                 @Override public void disable() {}
             }
@@ -1342,6 +1346,8 @@ class RequiresPermissionDetectorTest : LintDetectorTest() {
             public void foo();
             @RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)
             public void connect();
+            @RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)
+            public void connectSecond();
             @RequiresPermission(allOf = {
                 android.Manifest.permission.BLUETOOTH_SCAN,
                 android.Manifest.permission.BLUETOOTH_ADVERTISE
