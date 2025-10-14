8325d75a802acda8303a71e698ae066385f3a671
Kyunglyul Hyun <klhyun@google.com>
2025-10-14 03:13:25 +0000

    Prevent updating remote properties without scan response
    The remote properties could be updated without waiting
    for scan response due to passive scanning.
    
    Flag: EXEMPT BUGFIX
    Bug: 448814820
    Test: atest bluetooth_test_gd_unit
    Change-Id: I5499451ec6fa2e8aa5082f6f68f7751b85e0d362

--- system/main/shim/le_scanning_manager.cc
+++ system/main/shim/le_scanning_manager.cc
@@ -481,9 +481,13 @@ void BleScannerInterfaceImpl::on_scan_result(uint16_t event_type, uint8_t addres
 
   // Do not update device properties of already bonded devices.
   if (!BTM_IsBonded(raw_address)) {
-    do_in_jni_thread(base::BindOnce(&BleScannerInterfaceImpl::handle_remote_properties,
-                                    base::Unretained(this), raw_address, ble_addr_type,
-                                    advertising_data));
+    // Prevent updating properties without scan response
+    if (!com_android_bluetooth_flags_support_passive_scanning() || !(event_type & kScannableMask) ||
+        (event_type & kScanResponseMask) || msft_adv_monitor_enabled_) {
+      do_in_jni_thread(base::BindOnce(&BleScannerInterfaceImpl::handle_remote_properties,
+                                      base::Unretained(this), raw_address, ble_addr_type,
+                                      advertising_data));
+    }
   }
 
   do_in_jni_thread(base::BindOnce(
