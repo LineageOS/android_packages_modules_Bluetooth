283902c1cda44280df4c0b760413043ed33b17b9
Ömer Faruk Yılmaz <ofy@google.com>
2025-10-21 10:03:24 +0900

    Increase line coverage for AppScanStats#dump
    Expands AppScanStatsTest#dump_doesNotCrash to exercise more
    conditional branches and improve test coverage.
    
    There is no reason to validate the output string in the unit test. We
    simply want to traverse code as much as we can.
    
    Bug: 404652736
    Test: atest AppScanStatsTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: Ibefcd09a40917899bc79fa5ac3858c413f7f8537

--- android/app/tests/unit/src/com/android/bluetooth/le_scan/AppScanStatsTest.kt
+++ android/app/tests/unit/src/com/android/bluetooth/le_scan/AppScanStatsTest.kt
@@ -16,6 +16,7 @@
 
 package com.android.bluetooth.le_scan
 
+import android.app.ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND_SERVICE
 import android.bluetooth.le.ScanFilter
 import android.bluetooth.le.ScanSettings
 import android.os.BatteryStatsManager
@@ -27,11 +28,15 @@ import com.android.bluetooth.btservice.AdapterService
 import com.android.bluetooth.util.TimeProvider
 import com.android.tests.bluetooth.MockitoRule
 import com.google.common.truth.Truth.assertThat
+import java.util.UUID
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.whenever
 
 /** Test cases for [AppScanStats]. */
 @SmallTest
@@ -46,6 +51,7 @@ class AppScanStatsTest {
     @Before
     fun setUp() {
         mockGetSystemService(adapterService, BatteryStatsManager::class.java, batteryStatsManager)
+        doReturn(ScanUtil.DEFAULT_SCAN_QUOTA_COUNT).whenever(adapterService).scanQuotaCount
     }
 
     @Test
@@ -55,7 +61,6 @@ class AppScanStatsTest {
         val uid = 1234
         val pid = 5678
         val appScanStats = AppScanStats(uid, pid, name, source, adapterService, timeProvider)
-
         assertThat(appScanStats.isScanning()).isFalse()
     }
 
@@ -67,22 +72,100 @@ class AppScanStatsTest {
         val pid = 5678
         val appScanStats = AppScanStats(uid, pid, name, source, adapterService, timeProvider)
 
-        val settings = ScanSettings.Builder().build()
-        val filters = listOf(ScanFilter.Builder().setDeviceName("TestName").build())
-        val isFilterScan = false
-        val isCallbackScan = false
-        val scannerId = 0
+        val app1 = mock(ScannerApp::class.java)
+        whenever(app1.id).thenReturn(101)
+        whenever(app1.uuid).thenReturn(UUID.randomUUID())
+        whenever(app1.attributionTag).thenReturn("appTag1")
 
-        appScanStats.recordScanStart(
-            settings,
-            filters,
-            isFilterScan,
-            isCallbackScan,
-            scannerId,
-            "tag",
-        )
+        val app2 = mock(ScannerApp::class.java)
+        whenever(app2.id).thenReturn(102)
+        whenever(app2.uuid).thenReturn(UUID.randomUUID())
+        whenever(app2.attributionTag).thenReturn(null)
+
+        AppScanStats.setScreenState(true)
         appScanStats.isRegistered = true
+        appScanStats.recordBatchAlarmScheduled()
 
-        appScanStats.dump(emptyList())
+        // Scan 1: Ongoing, Suspended, Filtered, Opp, Callback, Timeout, High Importance
+        val settings1 =
+            ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_OPPORTUNISTIC).build()
+        val filters1 = listOf(ScanFilter.Builder().setDeviceName("TestName").build())
+        appScanStats.appImportance = IMPORTANCE_FOREGROUND_SERVICE - 1
+        appScanStats.recordScanStart(
+            settings = settings1,
+            filters = filters1,
+            isFilterScan = true,
+            isCallbackScan = true,
+            scannerId = 1,
+            attributionTag = "tag1",
+        )
+        appScanStats.recordScanSuspend(1)
+        appScanStats.setScanTimeout(1)
+
+        // Scan 2: Completed, Batch, Low Power, PI, Normal Importance
+        val settings2 =
+            ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
+                .setReportDelay(5000) // Makes it a batch scan
+                .build()
+        appScanStats.appImportance = IMPORTANCE_FOREGROUND_SERVICE
+        appScanStats.recordScanStart(
+            settings = settings2,
+            filters = emptyList(),
+            isFilterScan = false,
+            isCallbackScan = false,
+            scannerId = 2,
+            attributionTag = null,
+        )
+        appScanStats.recordScanStop(2)
+
+        // Scan 3: Ongoing, Active, AutoBatch, Balanced, CB, Low Importance
+        val settings3 = ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_BALANCED).build()
+        appScanStats.appImportance = IMPORTANCE_FOREGROUND_SERVICE + 1
+        appScanStats.recordScanStart(
+            settings = settings3,
+            filters = emptyList(),
+            isFilterScan = false,
+            isCallbackScan = true,
+            scannerId = 3,
+            attributionTag = "tag3",
+        )
+        appScanStats.setAutoBatchScan(3, true)
+
+        // Scan 4: Completed, Suspended/Resumed, Low Latency
+        val settings4 =
+            ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).build()
+        appScanStats.appImportance = IMPORTANCE_FOREGROUND_SERVICE + 1
+        appScanStats.recordScanStart(
+            settings = settings4,
+            filters = emptyList(),
+            isFilterScan = false,
+            isCallbackScan = true,
+            scannerId = 4,
+            attributionTag = "tag4",
+        )
+        appScanStats.recordScanSuspend(4)
+        appScanStats.recordScanResume(4)
+        appScanStats.recordScanStop(4)
+
+        // Scan 5: Ongoing, Ambient Discovery
+        val settings5 =
+            ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_AMBIENT_DISCOVERY).build()
+        appScanStats.recordScanStart(
+            settings = settings5,
+            filters = emptyList(),
+            isFilterScan = false,
+            isCallbackScan = true,
+            scannerId = 5,
+            attributionTag = "tag5",
+        )
+
+        appScanStats.addResults(1, 50)
+        AppScanStats.setScreenState(false)
+        appScanStats.addResults(2, 60)
+        appScanStats.addResults(1, 70)
+        AppScanStats.setScreenState(true)
+
+        appScanStats.dump(listOf(app1, app2))
     }
 }
