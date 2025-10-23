7623bbabeeab08b5b33072f65e7c5767d2203485
Ömer Faruk Yılmaz <ofy@google.com>
2025-10-24 06:26:25 +0900

    Convert AdvertiseManagerTest to Kotlin
    Bug: 435548204
    Test: atest com.android.bluetooth.gatt
    Flag: EXEMPT PURE_REFACTOR
    Change-Id: I5874ed007b93de5118e50f230a7beb461c74edf8

--- android/app/tests/unit/src/com/android/bluetooth/gatt/AdvertiseManagerTest.kt
+++ android/app/tests/unit/src/com/android/bluetooth/gatt/AdvertiseManagerTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2025 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,144 +14,144 @@
  * limitations under the License.
  */
 
-package com.android.bluetooth.gatt;
+package com.android.bluetooth.gatt
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertisingSetParameters
+import android.bluetooth.le.IAdvertisingSetCallback
+import android.bluetooth.le.PeriodicAdvertisingParameters
+import android.content.AttributionSource
+import android.content.pm.PackageManager
+import android.os.IBinder
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.android.bluetooth.TestLooper
+import com.android.bluetooth.btservice.AdapterService
+import com.android.tests.bluetooth.MockitoRule
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.any
+import org.mockito.Mockito.anyInt
+import org.mockito.Mockito.eq
+import org.mockito.Mockito.verify
+import org.mockito.kotlin.doNothing
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.whenever
 
-import android.bluetooth.le.AdvertiseData;
-import android.bluetooth.le.AdvertisingSetParameters;
-import android.bluetooth.le.IAdvertisingSetCallback;
-import android.bluetooth.le.PeriodicAdvertisingParameters;
-import android.content.AttributionSource;
-import android.content.pm.PackageManager;
-import android.os.IBinder;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.SmallTest;
-
-import com.android.bluetooth.TestLooper;
-import com.android.bluetooth.btservice.AdapterService;
-import com.android.tests.bluetooth.MockitoRule;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-
-/** Test cases for {@link AdvertiseManager}. */
+/** Test cases for [AdvertiseManager]. */
 @SmallTest
-@RunWith(AndroidJUnit4.class)
-public class AdvertiseManagerTest {
-    @Rule public final MockitoRule mMockitoRule = new MockitoRule();
+@RunWith(AndroidJUnit4::class)
+class AdvertiseManagerTest {
+    @get:Rule val mockitoRule = MockitoRule()
 
-    @Mock private AttributionSource mSource;
-    @Mock private AdapterService mAdapterService;
-    @Mock private GattService mGattService;
-    @Mock private AdvertiserMap mAdvertiserMap;
-    @Mock private AdvertiseManagerNativeInterface mNativeInterface;
-    @Mock private IAdvertisingSetCallback mCallback;
-    @Mock private IBinder mBinder;
-    @Mock private PackageManager mPackageManager;
+    @Mock private lateinit var source: AttributionSource
+    @Mock private lateinit var adapterService: AdapterService
+    @Mock private lateinit var gattService: GattService
+    @Mock private lateinit var advertiserMap: AdvertiserMap
+    @Mock private lateinit var nativeInterface: AdvertiseManagerNativeInterface
+    @Mock private lateinit var callback: IAdvertisingSetCallback
+    @Mock private lateinit var binder: IBinder
+    @Mock private lateinit var packageManager: PackageManager
 
-    private static final String APP_NAME = "com.android.what.a.name";
-
-    private AdvertiseManager mAdvertiseManager;
-    private int mAdvertiserId;
+    private lateinit var advertiseManager: AdvertiseManager
+    private var advertiserId = 0
 
     @Before
-    public void setUp() throws Exception {
-        doReturn(mPackageManager).when(mAdapterService).getPackageManager();
-        doReturn(APP_NAME).when(mPackageManager).getNameForUid(anyInt());
+    fun setUp() {
+        doReturn(packageManager).whenever(adapterService).packageManager
+        doReturn(APP_NAME).whenever(packageManager).getNameForUid(anyInt())
 
-        mAdvertiseManager =
-                new AdvertiseManager(
-                        mAdapterService,
-                        mGattService,
-                        mNativeInterface,
-                        new TestLooper().getLooper(),
-                        mAdvertiserMap);
+        advertiseManager =
+            AdvertiseManager(
+                adapterService,
+                gattService,
+                nativeInterface,
+                TestLooper().getLooper(),
+                advertiserMap,
+            )
 
-        final var parameters = new AdvertisingSetParameters.Builder().build();
-        final var advertiseData = new AdvertiseData.Builder().build();
-        final var scanResponse = new AdvertiseData.Builder().build();
-        final var periodicParameters = new PeriodicAdvertisingParameters.Builder().build();
-        final var periodicData = new AdvertiseData.Builder().build();
-        final int duration = 10;
-        final int maxExtAdvEvents = 15;
+        val parameters = AdvertisingSetParameters.Builder().build()
+        val advertiseData = AdvertiseData.Builder().build()
+        val scanResponse = AdvertiseData.Builder().build()
+        val periodicParameters = PeriodicAdvertisingParameters.Builder().build()
+        val periodicData = AdvertiseData.Builder().build()
+        val duration = 10
+        val maxExtAdvEvents = 15
 
-        doReturn(mBinder).when(mCallback).asBinder();
-        doNothing().when(mBinder).linkToDeath(any(), eq(0));
+        doReturn(binder).whenever(callback).asBinder()
+        doNothing().whenever(binder).linkToDeath(any(), eq(0))
 
-        mAdvertiseManager.startAdvertisingSet(
-                parameters,
-                advertiseData,
-                scanResponse,
-                periodicParameters,
-                periodicData,
-                duration,
-                maxExtAdvEvents,
-                null,
-                mCallback,
-                mSource);
+        advertiseManager.startAdvertisingSet(
+            parameters,
+            advertiseData,
+            scanResponse,
+            periodicParameters,
+            periodicData,
+            duration,
+            maxExtAdvEvents,
+            null,
+            callback,
+            source,
+        )
 
-        mAdvertiserId = mAdvertiseManager.mTempRegistrationId;
+        advertiserId = advertiseManager.mTempRegistrationId
     }
 
     @Test
-    public void advertisingSet() {
-        boolean enable = true;
-        int duration = 60;
-        int maxExtAdvEvents = 100;
-
-        mAdvertiseManager.enableAdvertisingSet(
-                mAdvertiserId, enable, duration, maxExtAdvEvents, mSource);
-        verify(mAdvertiserMap)
-                .enableAdvertisingSet(mAdvertiserId, enable, duration, maxExtAdvEvents);
+    fun advertisingSet() {
+        val enable = true
+        val duration = 60
+        val maxExtAdvEvents = 100
+        advertiseManager.enableAdvertisingSet(
+            advertiserId,
+            enable,
+            duration,
+            maxExtAdvEvents,
+            source,
+        )
+        verify(advertiserMap).enableAdvertisingSet(advertiserId, enable, duration, maxExtAdvEvents)
     }
 
     @Test
-    public void advertisingData() {
-        final var advertiseData = new AdvertiseData.Builder().build();
+    fun advertisingData() {
+        val advertiseData = AdvertiseData.Builder().build()
 
-        mAdvertiseManager.setAdvertisingData(mAdvertiserId, advertiseData);
-        verify(mAdvertiserMap).setAdvertisingData(mAdvertiserId, advertiseData);
+        advertiseManager.setAdvertisingData(advertiserId, advertiseData)
+        verify(advertiserMap).setAdvertisingData(advertiserId, advertiseData)
     }
 
     @Test
-    public void scanResponseData() {
-        final var scanResponse = new AdvertiseData.Builder().build();
-
-        mAdvertiseManager.setScanResponseData(mAdvertiserId, scanResponse);
-        verify(mAdvertiserMap).setScanResponseData(mAdvertiserId, scanResponse);
+    fun scanResponseData() {
+        val scanResponse = AdvertiseData.Builder().build()
+        advertiseManager.setScanResponseData(advertiserId, scanResponse)
+        verify(advertiserMap).setScanResponseData(advertiserId, scanResponse)
     }
 
     @Test
-    public void advertisingParameters() {
-        final var parameters = new AdvertisingSetParameters.Builder().build();
-
-        mAdvertiseManager.setAdvertisingParameters(mAdvertiserId, parameters);
-        verify(mAdvertiserMap).setAdvertisingParameters(mAdvertiserId, parameters);
+    fun advertisingParameters() {
+        val parameters = AdvertisingSetParameters.Builder().build()
+        advertiseManager.setAdvertisingParameters(advertiserId, parameters)
+        verify(advertiserMap).setAdvertisingParameters(advertiserId, parameters)
     }
 
     @Test
-    public void periodicAdvertisingParameters() {
-        final var periodicParameters = new PeriodicAdvertisingParameters.Builder().build();
-
-        mAdvertiseManager.setPeriodicAdvertisingParameters(mAdvertiserId, periodicParameters);
-        verify(mAdvertiserMap).setPeriodicAdvertisingParameters(mAdvertiserId, periodicParameters);
+    fun periodicAdvertisingParameters() {
+        val periodicParameters = PeriodicAdvertisingParameters.Builder().build()
+        advertiseManager.setPeriodicAdvertisingParameters(advertiserId, periodicParameters)
+        verify(advertiserMap).setPeriodicAdvertisingParameters(advertiserId, periodicParameters)
     }
 
     @Test
-    public void periodicAdvertisingData() {
-        final var periodicData = new AdvertiseData.Builder().build();
+    fun periodicAdvertisingData() {
+        val periodicData = AdvertiseData.Builder().build()
+        advertiseManager.setPeriodicAdvertisingData(advertiserId, periodicData)
+        verify(advertiserMap).setPeriodicAdvertisingData(advertiserId, periodicData)
+    }
 
-        mAdvertiseManager.setPeriodicAdvertisingData(mAdvertiserId, periodicData);
-        verify(mAdvertiserMap).setPeriodicAdvertisingData(mAdvertiserId, periodicData);
+    private companion object {
+        private const val APP_NAME = "com.android.what.a.name"
     }
 }
