3e46d2ca4623b2e0a052ab3498e4158811361099
Himanshu Rohilla <rohillah@google.com>
2025-11-03 06:43:20 -0800

    Updating initialized check for De-Init
    Bug: 437790903
    Bug: 444620685
    Test: m com.google.android.bt
    Flag: com.android.bluetooth.flags.use_array_instead_list_in_sec_dev_rec
    Change-Id: I14d3100046b60ccf0d5cd9c0e608d5b057569f9f

--- system/stack/test/btm/stack_btm_sec_test.cc
+++ system/stack/test/btm/stack_btm_sec_test.cc
@@ -212,7 +212,8 @@ TEST_F(StackBtmSecWithInitFreeTest, btm_sec_allocate_dev_rec__all) {
       ASSERT_NE(nullptr, p_devices[i]);
     }
   } else {
-    ASSERT_EQ(kBtmSecMaxDeviceRecords, p_devices[kBtmSecMaxDeviceRecords - 1]->timestamp);
+    // kBtmSecMaxDeviceRecords = BTM_SEC_MAX_DEVICE_RECORDS + 1;
+    ASSERT_EQ(kBtmSecMaxDeviceRecords-1, p_devices[kBtmSecMaxDeviceRecords - 1]->timestamp);
 
     for (size_t i = 0; i < kBtmSecMaxDeviceRecords; i++) {
       BtmDevice* p_device = btm_sec_allocate_dev_rec();
