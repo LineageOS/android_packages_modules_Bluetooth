6ac4fa60d12e037863cfe3019b352911d6397c70
Archie Pusaka <apusaka@google.com>
2025-11-05 09:11:39 +0000

    sdp: Validate buffer length in sdpu_build_attrib_seq
    The num attributes can be large, so we could potentially write outside
    the given buffer. Add similar validation as b:239414876.
    
    Bug: 447536200
    Test: m com.google.android.bt
    Flag: EXEMPT BUGFIX
    Change-Id: I6c0a8ee92c3b4d25849f29b7023c05694deec1e6
    (cherry picked from commit ce1de9ef78ca4ff7b408d934815a8967e390eb6a)

--- system/stack/sdp/sdpint.h
+++ system/stack/sdp/sdpint.h
@@ -256,7 +256,7 @@ tCONN_CB* sdpu_allocate_ccb(void);
 void sdpu_release_ccb(tCONN_CB& p_ccb);
 
 uint8_t* sdpu_build_attrib_seq(uint8_t* p_out, uint16_t* p_attr,
-                               uint16_t num_attrs);
+                               uint16_t num_attrs, uint16_t& bytes_left);
 uint8_t* sdpu_build_attrib_entry(uint8_t* p_out, const tSDP_ATTRIBUTE* p_attr);
 void sdpu_build_n_send_error(tCONN_CB* p_ccb, uint16_t trans_num,
                              uint16_t error_code, char* p_error_text);
