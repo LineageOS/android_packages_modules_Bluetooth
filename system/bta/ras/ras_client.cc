2e168d26aaeef62e1bb036a0b76241e2319b5eac
Chienyuan Huang <chienyuanhuang@google.com>
2025-11-14 07:01:45 +0000

    RAS: Prevent Alarm Leak in SetTimeOutAlarm
    The SetTimeOutAlarm function was potentially leaking alarm_t resources. When called for a RasTracker that already had an active timeout alarm, the old alarm was overwritten without being freed. The fix ensures that any existing ranging_data_timeout_timer_ is cancelled and freed before a new alarm is created and assigned, thus preventing memory leaks.
    
    Flag: EXEMPT BUGFIX
    Bug: 459995188
    Test: atest bluetooth_ras_test
    Change-Id: Ic78fd13b6c4765a0ffbb7323d9fa4321c8815434

--- system/bta/ras/ras_client.cc
+++ system/bta/ras/ras_client.cc
@@ -482,7 +482,7 @@ public:
     data.resize(evt.len);
     std::copy(evt.value, evt.value + evt.len, data.begin());
     bool is_last = (data[0] >> 1 & 0x01);
-    alarm_cancel(tracker->ranging_data_timeout_timer_);
+    CancelRangingTimer(tracker);
     if (!is_last) {
       SetTimeOutAlarm(tracker, kFollowingSegmentTimeoutMs, TimeoutType::FOLLOWING_SEGMENT);
     }
@@ -518,7 +518,7 @@ public:
     // Send get ranging data command
     tracker->latest_ranging_counter_ = ranging_counter;
     if (tracker->timeout_type_ == TimeoutType::RANGING_DATA_READY) {
-      alarm_cancel(tracker->ranging_data_timeout_timer_);
+      CancelRangingTimer(tracker);
     }
     GetRangingData(ranging_counter, tracker);
   }
@@ -876,6 +876,15 @@ public:
     }
   }
 
+  void CancelRangingTimer(std::shared_ptr<RasTracker> tracker) {
+    if (tracker->ranging_data_timeout_timer_ != nullptr) {
+      alarm_cancel(tracker->ranging_data_timeout_timer_);
+      alarm_free(tracker->ranging_data_timeout_timer_);
+      tracker->ranging_data_timeout_timer_ = nullptr;
+    }
+    tracker->timeout_type_ = TimeoutType::TIMEOUT_NONE;
+  }
+
   std::string GetFeaturesString(uint32_t value) {
     std::stringstream ss;
     ss << value;
@@ -941,6 +950,8 @@ public:
     }
     log::debug("ranging_type_: {}, timeout_type: {}", (uint8_t)tracker->ranging_type_,
                (uint8_t)timeout_type);
+    // Clean up any previous timer first
+    CancelRangingTimer(tracker);
     tracker->timeout_type_ = timeout_type;
     tracker->ranging_data_timeout_timer_ = alarm_new("Ranging Data Timeout");
     alarm_set_on_mloop(
