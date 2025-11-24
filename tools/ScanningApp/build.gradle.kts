328fa79fd9226aa59e414d302253067049b18cd0
Ömer Faruk Yılmaz <ofy@google.com>
2025-11-24 16:31:01 +0900

    Convert ScanningApp to use Jetpack Compose
    Because why not? It's 2025...
    
    Bug: 417855334
    Test: m ScanningApp
    Flag: EXEMPT DEBUG
    Change-Id: I128b6cec0b9a68b9260c19814253aaef5faf82fd

--- tools/ScanningApp/build.gradle.kts
+++ tools/ScanningApp/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -12,7 +13,7 @@ android {
         minSdk = 36
         targetSdk = 36
         versionCode = 2
-        versionName = "1.2"
+        versionName = "1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -26,6 +27,8 @@ android {
 
     buildTypes { release { isMinifyEnabled = false } }
 
+    buildFeatures { compose = true }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -35,12 +38,19 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.tooling.preview)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
