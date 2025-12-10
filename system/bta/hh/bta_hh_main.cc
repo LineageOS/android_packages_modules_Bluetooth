2f6a5bcd6e26808ffb2b6c7b6975c161aeb34567
Himanshu Rawat <rwt@google.com>
2025-12-10 11:21:08 -0800

    Remove flag hogp_direct_connection_upgrade
    Test: mmm packages/modules/Bluetooth
    Flag: EXEMPT FLAG_REMOVAL
    Bug: 467196844
    Bug: 430179438
    Change-Id: I6b7ed153dba36e3baf408cae14a94ed8cdeea2c5

--- system/bta/hh/bta_hh_main.cc
+++ system/bta/hh/bta_hh_main.cc
@@ -211,10 +211,6 @@ void bta_hh_sm_execute(tBTA_HH_DEV_CB* p_cb, tBTA_HH_INT_EVT event, const tBTA_H
     case BTA_HH_W4_CONN_ST:
       switch (event) {
         case BTA_HH_API_OPEN_EVT:
-          if (!com_android_bluetooth_flags_hogp_direct_connection_upgrade()) {
-            unexpected_event = true;
-            break;
-          }
           bta_hh_connect_upgrade(p_cb, p_data);
           break;
         case BTA_HH_API_CANCEL_OPEN_EVT:
