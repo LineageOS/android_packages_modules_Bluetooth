435f03ab21a4438a3b65c2bc00aa79bff444687f
Krzysztof Kopyscinski <kopyscinski@google.com>
2025-12-12 08:29:15 +0100

    Cleanup tbs_set_lea_from_btservice flag
    Flag has been delivered
    
    Bug: 467195492
    Test: mmm packages/modules/Bluetooth
    Flag: EXEMPT FLAG_REMOVAL
    Change-Id: I3d18cec707aa93a9e5e7800271ad3f6f35693796

--- android/app/tests/unit/src/com/android/bluetooth/tbs/TbsGenericTest.java
+++ android/app/tests/unit/src/com/android/bluetooth/tbs/TbsGenericTest.java
@@ -37,15 +37,12 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothLeCall;
 import android.media.AudioManager;
-import android.platform.test.annotations.DisableFlags;
-import android.platform.test.annotations.EnableFlags;
 import android.platform.test.flag.junit.SetFlagsRule;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.MediumTest;
 
 import com.android.bluetooth.btservice.AdapterService;
-import com.android.bluetooth.flags.Flags;
 import com.android.bluetooth.le_audio.LeAudioService;
 import com.android.tests.bluetooth.MockitoRule;
 
@@ -355,59 +352,6 @@ public class TbsGenericTest {
     }
 
     @Test
-    @EnableFlags(Flags.FLAG_TBS_SET_LEA_FROM_BTSERVICE)
-    // To replace testCallAccept test after flag is released
-    public void testCallAccept_setLeActiveDeviceFromAdapterService() {
-        Integer ccid = prepareTestBearer();
-        reset(mTbsGatt);
-
-        // Prepare the incoming call
-        UUID callUuid = UUID.randomUUID();
-        List<BluetoothLeCall> tbsCalls = new ArrayList<>();
-        tbsCalls.add(
-                new BluetoothLeCall(
-                        callUuid,
-                        "tel:987654321",
-                        "aFriendlyCaller",
-                        BluetoothLeCall.STATE_INCOMING,
-                        0));
-        mTbsGeneric.currentCallsList(ccid, tbsCalls);
-
-        ArgumentCaptor<Map> currentCallsCaptor = ArgumentCaptor.forClass(Map.class);
-        verify(mTbsGatt).setCallState(currentCallsCaptor.capture());
-        Map<Integer, TbsCall> capturedCurrentCalls = currentCallsCaptor.getValue();
-        assertThat(capturedCurrentCalls).hasSize(1);
-        Integer callIndex = capturedCurrentCalls.entrySet().iterator().next().getKey();
-        reset(mTbsGatt);
-
-        byte args[] = new byte[1];
-        args[0] = (byte) (callIndex & 0xFF);
-        mTbsGattCallback
-                .getValue()
-                .onCallControlPointRequest(mDevice, TbsGatt.CALL_CONTROL_POINT_OPCODE_ACCEPT, args);
-
-        ArgumentCaptor<Integer> requestIdCaptor = ArgumentCaptor.forClass(Integer.class);
-        ArgumentCaptor<UUID> callUuidCaptor = ArgumentCaptor.forClass(UUID.class);
-        verify(mCallback).onAcceptCall(requestIdCaptor.capture(), callUuidCaptor.capture());
-        assertThat(callUuidCaptor.getValue()).isEqualTo(callUuid);
-        // Active device should be changed
-        verify(mAdapterService).setActiveDevice(mDevice, BluetoothAdapter.ACTIVE_DEVICE_AUDIO);
-
-        // Respond with requestComplete...
-        mTbsGeneric.requestResult(ccid, requestIdCaptor.getValue(), Result.SUCCESS);
-        mTbsGeneric.callStateChanged(ccid, callUuid, BluetoothLeCall.STATE_ACTIVE);
-
-        // ..and verify if GTBS control point is updated to notifier the peer about the result
-        verify(mTbsGatt)
-                .setCallControlPointResult(
-                        eq(mDevice),
-                        eq(TbsGatt.CALL_CONTROL_POINT_OPCODE_ACCEPT),
-                        eq(callIndex),
-                        eq(Result.SUCCESS));
-    }
-
-    @Test
-    @DisableFlags(Flags.FLAG_TBS_SET_LEA_FROM_BTSERVICE)
     public void testCallAccept() {
         Integer ccid = prepareTestBearer();
         reset(mTbsGatt);
@@ -442,7 +386,7 @@ public class TbsGenericTest {
         verify(mCallback).onAcceptCall(requestIdCaptor.capture(), callUuidCaptor.capture());
         assertThat(callUuidCaptor.getValue()).isEqualTo(callUuid);
         // Active device should be changed
-        verify(mLeAudioService).setActiveDevice(mDevice);
+        verify(mAdapterService).setActiveDevice(mDevice, BluetoothAdapter.ACTIVE_DEVICE_AUDIO);
 
         // Respond with requestComplete...
         mTbsGeneric.requestResult(ccid, requestIdCaptor.getValue(), Result.SUCCESS);
@@ -621,11 +565,7 @@ public class TbsGenericTest {
         verify(mCallback).onPlaceCall(requestIdCaptor.capture(), callUuidCaptor.capture(), eq(uri));
 
         // Active device should be changed
-        if (Flags.tbsSetLeaFromBtservice()) {
-            verify(mAdapterService).setActiveDevice(mDevice, BluetoothAdapter.ACTIVE_DEVICE_AUDIO);
-        } else {
-            verify(mLeAudioService).setActiveDevice(mDevice);
-        }
+        verify(mAdapterService).setActiveDevice(mDevice, BluetoothAdapter.ACTIVE_DEVICE_AUDIO);
 
         // Respond with requestComplete...
         mTbsGeneric.requestResult(ccid, requestIdCaptor.getValue(), Result.SUCCESS);
@@ -746,12 +686,9 @@ public class TbsGenericTest {
                 .onCallControlPointRequest(mDevice, TbsGatt.CALL_CONTROL_POINT_OPCODE_ACCEPT, args);
 
         // Active device should not be changed
-        if (Flags.tbsSetLeaFromBtservice()) {
-            verify(mAdapterService, never())
-                    .setActiveDevice(mDevice, BluetoothAdapter.ACTIVE_DEVICE_AUDIO);
-        } else {
-            verify(mLeAudioService, never()).setActiveDevice(mDevice);
-        }
+        verify(mAdapterService, never())
+                .setActiveDevice(mDevice, BluetoothAdapter.ACTIVE_DEVICE_AUDIO);
+
         // Verify if GTBS control point is updated to notify the peer about the result
         verify(mTbsGatt)
                 .setCallControlPointResult(
