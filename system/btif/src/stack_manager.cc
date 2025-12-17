17cfe50a7d3da62b04e63e493e1f0623c0833441
Archie Pusaka <apusaka@google.com>
2025-12-17 10:45:01 +0000

    Remove call_sdp_free_in_main_thread flag
    Bug: 467189528
    Bug: 415917037
    Test: m com.google.android.bt
    Flag: EXEMPT FLAG_REMOVAL
    Change-Id: I3658469f6e449217ad1d7466eafd2e57be915375

--- system/btif/src/stack_manager.cc
+++ system/btif/src/stack_manager.cc
@@ -279,11 +279,7 @@ static void stop_stack(ProfileStopCallback stopProfiles) {
   future_await(local_hack_future);
 
   gatt_free();
-  if (com_android_bluetooth_flags_call_sdp_free_in_main_thread()) {
-    do_in_main_thread(base::BindOnce(sdp_free));
-  } else {
-    sdp_free();
-  }
+  do_in_main_thread(base::BindOnce(sdp_free));
   l2c_free();
   get_btm_client_interface().lifecycle.btm_ble_free();
 
