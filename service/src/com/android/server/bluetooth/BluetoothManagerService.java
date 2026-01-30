8cb40c9215015b9eaaa68702b69703e544fac27a
William Escande <wescande@google.com>
2026-01-29 19:05:17 -0800

    SystemServer: factorize getter for persisted state
    Bug: 427631465
    Test: atest ServiceBluetoothRoboTests
    Flag: EXEMPT BUGFIX
    Change-Id: I6a6a696465c299966f9dd448eacdf1bec982488f

--- service/src/com/android/server/bluetooth/BluetoothManagerService.java
+++ service/src/com/android/server/bluetooth/BluetoothManagerService.java
@@ -697,28 +697,27 @@ public class BluetoothManagerService {
 
     /** Returns true if the Bluetooth saved state is "on" */
     private boolean isBluetoothPersistedStateOn() {
-        final int state =
-                BluetoothServerProxy.getInstance()
-                        .getBluetoothPersistedState(mContentResolver, BLUETOOTH_ON_BLUETOOTH);
-        Log.d(TAG, "isBluetoothPersistedStateOn: " + state);
+        final int state = getBluetoothPersistedState();
+        Log.v(TAG, "isBluetoothPersistedStateOn: " + state);
         return state != BLUETOOTH_OFF;
     }
 
     private boolean isBluetoothPersistedStateOnAirplane() {
-        final int state =
-                BluetoothServerProxy.getInstance()
-                        .getBluetoothPersistedState(mContentResolver, BLUETOOTH_ON_BLUETOOTH);
-        Log.d(TAG, "isBluetoothPersistedStateOnAirplane: " + state);
+        final int state = getBluetoothPersistedState();
+        Log.v(TAG, "isBluetoothPersistedStateOnAirplane: " + state);
         return state == BLUETOOTH_ON_AIRPLANE;
     }
 
     /** Returns true if the Bluetooth saved state is BLUETOOTH_ON_BLUETOOTH */
     private boolean isBluetoothPersistedStateOnBluetooth() {
-        final int state =
-                BluetoothServerProxy.getInstance()
-                        .getBluetoothPersistedState(mContentResolver, BLUETOOTH_ON_BLUETOOTH);
-        Log.d(TAG, "isBluetoothPersistedStateOnBluetooth: " + state);
+        final int state = getBluetoothPersistedState();
+        Log.v(TAG, "isBluetoothPersistedStateOnBluetooth: " + state);
         return state == BLUETOOTH_ON_BLUETOOTH;
+    }
+
+    private int getBluetoothPersistedState() {
+        return BluetoothServerProxy.getInstance()
+                .getBluetoothPersistedState(mContentResolver, BLUETOOTH_ON_BLUETOOTH);
     }
 
     private void setBluetoothPersistedState(int state) {
