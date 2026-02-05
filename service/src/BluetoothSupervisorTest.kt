88fc647948a2d129890ff027d18f7616e51b1c64
William Escande <wescande@google.com>
2026-02-05 12:45:19 -0800

    SystemServer: Robolectric skeleton test for supervisor
    Bug: 319685396
    Bug: 478056203
    Test: atest ServiceBluetoothRoboTests
    Flag: com.android.bluetooth.flags.system_server_migrate_bms_to_kotlin
    Change-Id: I8eea243bae51993f0c5b6a68c73b3f9e6a6a6964

---
+++ service/src/BluetoothSupervisorTest.kt
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.bluetooth.test
+
+import android.app.ActivityManager
+import android.content.Context
+import android.os.Looper
+import android.os.UserHandle
+import androidx.test.core.app.ApplicationProvider
+import com.android.server.bluetooth.BluetoothSupervisorNew
+import com.google.common.truth.Truth.assertThat
+import java.io.PrintWriter
+import java.io.StringWriter
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+@ExperimentalCoroutinesApi
+class BluetoothSupervisorTest {
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+    private val looper = Looper.getMainLooper()
+
+    private lateinit var supervisor: BluetoothSupervisorNew
+
+    @Before
+    fun setUp() {
+        supervisor = BluetoothSupervisorNew(context, looper, BluetoothComponentTest.setup())
+    }
+
+    private fun verifyActiveUser(user: UserHandle) {
+        val stringWriter = StringWriter()
+        val printWriter = PrintWriter(stringWriter)
+
+        supervisor.api.dump(null, printWriter, null)
+        val dumpOutput = stringWriter.toString()
+
+        assertThat(dumpOutput).contains("BluetoothManagerServiceNew for $user")
+    }
+
+    @Test
+    fun onUserStarting_delegatesToSwitch() = runTest {
+        val user = UserHandle.of(10)
+        supervisor.onUserStarting(user)
+        verifyActiveUser(user)
+    }
+
+    @Test
+    fun onUserSwitching_startsNewService() = runTest {
+        val user1 = UserHandle.of(10)
+        val user2 = UserHandle.of(11)
+
+        supervisor.onUserSwitching(user1)
+        verifyActiveUser(user1)
+
+        supervisor.onUserSwitching(user2)
+        verifyActiveUser(user2)
+    }
+
+    @Test
+    fun onUserStopping_currentUser_stopsService() = runTest {
+        val user = UserHandle.of(10)
+        supervisor.onUserSwitching(user)
+        verifyActiveUser(user)
+
+        supervisor.onUserStopping(user)
+
+        // Should trigger fallback to foreground user
+        val foregroundUser = UserHandle.of(ActivityManager.getCurrentUser())
+        verifyActiveUser(foregroundUser)
+    }
+
+    @Test
+    fun onUserStopping_otherUser_ignored() = runTest {
+        val currentUser = UserHandle.of(10)
+        val otherUser = UserHandle.of(11)
+        supervisor.onUserSwitching(currentUser)
+
+        supervisor.onUserStopping(otherUser)
+
+        // Should remain on currentUser
+        verifyActiveUser(currentUser)
+    }
+
+    @Test
+    fun rapidUserSwitching_onlyLastUserStarted() = runTest {
+        val user1 = UserHandle.of(10)
+        val user2 = UserHandle.of(11)
+        val user3 = UserHandle.of(12)
+
+        // Since shutdown is instant in the real class currently, this test mostly verifies
+        // that sequential calls update the state correctly.
+        supervisor.onUserSwitching(user1)
+        supervisor.onUserSwitching(user2)
+        supervisor.onUserSwitching(user3)
+
+        verifyActiveUser(user3)
+    }
+}
