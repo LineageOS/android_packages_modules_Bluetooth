939932a699ea305eb977d2fd3a7fc0f0e3353f25
Ömer Faruk Yılmaz <ofy@google.com>
2026-02-10 20:02:05 +0900

    Convert CompanionManagerTest to Kotlin
    - Update Mockito usage to mockito-kotlin syntax
    - Remove Hungarian notation ('m' prefix) from variable names
    - Replace Util functions with Kotlin Idiomatic versions
    
    Bug: 404652736
    Test: atest CompanionManagerTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: I749b8976910c92b5dfc6147f8e210dea2e04b209

--- android/app/tests/unit/src/com/android/bluetooth/btservice/CompanionManagerTest.kt
+++ android/app/tests/unit/src/com/android/bluetooth/btservice/CompanionManagerTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,184 +14,172 @@
  * limitations under the License.
  */
 
-package com.android.bluetooth.btservice;
+package com.android.bluetooth.btservice
 
-import static android.bluetooth.BluetoothDevice.METADATA_SOFTWARE_VERSION;
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothDevice.METADATA_SOFTWARE_VERSION
+import android.bluetooth.BluetoothGatt
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.HandlerThread
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import androidx.test.platform.app.InstrumentationRegistry
+import com.android.bluetooth.btservice.CompanionManager.GATT_CONN_INTERVAL_MAX
+import com.android.bluetooth.btservice.CompanionManager.GATT_CONN_INTERVAL_MIN
+import com.android.bluetooth.btservice.CompanionManager.GATT_CONN_LATENCY
+import com.android.bluetooth.getTestDevice
+import com.android.bluetooth.mockBluetoothManager
+import com.android.bluetooth.mockGetRemoteDevice
+import com.android.bluetooth.mockResources
+import com.android.tests.bluetooth.MockitoRule
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.verify
+import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 
-import static com.android.bluetooth.TestUtils.getTestDevice;
-import static com.android.bluetooth.TestUtils.mockGetBluetoothManager;
-import static com.android.bluetooth.TestUtils.mockGetRemoteDevice;
-
-import static com.google.common.truth.Truth.assertThat;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.HandlerThread;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.MediumTest;
-import androidx.test.platform.app.InstrumentationRegistry;
-
-import com.android.tests.bluetooth.MockitoRule;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-
-/** Test cases for {@link CompanionManager}. */
+/** Test cases for [CompanionManager]. */
 @MediumTest
-@RunWith(AndroidJUnit4.class)
-public class CompanionManagerTest {
-    @Rule public final MockitoRule mMockitoRule = new MockitoRule();
+@RunWith(AndroidJUnit4::class)
+class CompanionManagerTest {
+    @get:Rule val mockitoRule = MockitoRule()
 
-    @Mock private AdapterService mAdapterService;
-    @Mock private SharedPreferences mSharedPreferences;
-    @Mock private SharedPreferences.Editor mEditor;
+    @Mock private lateinit var adapterService: AdapterService
+    @Mock private lateinit var sharedPreferences: SharedPreferences
+    @Mock private lateinit var editor: SharedPreferences.Editor
 
-    private final Context mContext = InstrumentationRegistry.getInstrumentation().getContext();
-    private final BluetoothDevice mDevice = getTestDevice(123);
+    private val context = InstrumentationRegistry.getInstrumentation().context
+    private val device = getTestDevice(123)
 
-    private CompanionManager mCompanionManager;
-    private HandlerThread mHandlerThread;
+    private lateinit var handlerThread: HandlerThread
+    private lateinit var companionManager: CompanionManager
 
     @Before
-    public void setUp() throws Exception {
-        mockGetRemoteDevice(mAdapterService, mDevice);
+    fun setUp() {
+        adapterService.mockGetRemoteDevice(device)
         // Start handler thread for this test
-        mHandlerThread = new HandlerThread("CompanionManagerTestHandlerThread");
-        mHandlerThread.start();
+        handlerThread = HandlerThread("CompanionManagerTestHandlerThread")
+        handlerThread.start()
         // Mock the looper
-        doReturn(mHandlerThread.getLooper()).when(mAdapterService).getMainLooper();
+        doReturn(handlerThread.looper).whenever(adapterService).mainLooper
         // Mock SharedPreferences
-        doReturn(mEditor).when(mSharedPreferences).edit();
-        doReturn(mSharedPreferences)
-                .when(mAdapterService)
-                .getSharedPreferences(
-                        eq(CompanionManager.COMPANION_INFO), eq(Context.MODE_PRIVATE));
+        doReturn(editor).whenever(sharedPreferences).edit()
+        doReturn(sharedPreferences)
+            .whenever(adapterService)
+            .getSharedPreferences(eq(CompanionManager.COMPANION_INFO), eq(Context.MODE_PRIVATE))
         // Use the resources in the instrumentation instead of the mocked AdapterService
-        doReturn(mContext.getResources()).when(mAdapterService).getResources();
-        mockGetBluetoothManager(mAdapterService);
+        adapterService.mockResources(context.resources)
+        adapterService.mockBluetoothManager()
 
-        // Must be called to initialize services
-        mCompanionManager = new CompanionManager(mAdapterService);
+        companionManager = CompanionManager(adapterService)
     }
 
     @After
-    public void tearDown() throws Exception {
-        mHandlerThread.quit();
+    fun tearDown() {
+        handlerThread.quit()
     }
 
     @Test
-    public void testLoadCompanionInfo_noCompanionDeviceInPrefs_checksMetadataOfBondedDevices() {
-        var device1 = mock(BluetoothDevice.class);
-        var device2 = mock(BluetoothDevice.class);
-        var devices = new BluetoothDevice[] {device1, device2};
-        doReturn(devices).when(mAdapterService).getBondedDevices();
+    fun testLoadCompanionInfo_noCompanionDeviceInPrefs_checksMetadataOfBondedDevices() {
+        val device1 = mock<BluetoothDevice>()
+        val device2 = mock<BluetoothDevice>()
+        val devices = arrayOf(device1, device2)
+        doReturn(devices).whenever(adapterService).bondedDevices
 
         doReturn("")
-                .when(mSharedPreferences)
-                .getString(eq(CompanionManager.COMPANION_DEVICE_KEY), anyString());
+            .whenever(sharedPreferences)
+            .getString(eq(CompanionManager.COMPANION_DEVICE_KEY), any<String>())
 
         doReturn(CompanionManager.COMPANION_TYPE_NONE)
-                .when(mSharedPreferences)
-                .getInt(eq(CompanionManager.COMPANION_TYPE_KEY), anyInt());
+            .whenever(sharedPreferences)
+            .getInt(eq(CompanionManager.COMPANION_TYPE_KEY), any<Int>())
 
         doReturn(null)
-                .when(mAdapterService)
-                .getMetadata(any(BluetoothDevice.class), eq(METADATA_SOFTWARE_VERSION));
+            .whenever(adapterService)
+            .getMetadata(any<BluetoothDevice>(), eq(METADATA_SOFTWARE_VERSION))
 
-        mCompanionManager.loadCompanionInfo();
+        companionManager.loadCompanionInfo()
 
-        verify(mAdapterService).getMetadata(eq(device1), eq(METADATA_SOFTWARE_VERSION));
-        verify(mAdapterService).getMetadata(eq(device2), eq(METADATA_SOFTWARE_VERSION));
+        verify(adapterService).getMetadata(eq(device1), eq(METADATA_SOFTWARE_VERSION))
+        verify(adapterService).getMetadata(eq(device2), eq(METADATA_SOFTWARE_VERSION))
     }
 
     @Test
-    public void testIsCompanionDevice() {
-        int type = CompanionManager.COMPANION_TYPE_NONE;
-        loadCompanionInfoHelper(type);
-        assertThat(mCompanionManager.getCompanionType(mDevice)).isEqualTo(type);
+    fun testIsCompanionDevice() {
+        var type = CompanionManager.COMPANION_TYPE_NONE
+        loadCompanionInfoHelper(type)
+        assertThat(companionManager.getCompanionType(device)).isEqualTo(type)
 
-        type = CompanionManager.COMPANION_TYPE_PRIMARY;
-        loadCompanionInfoHelper(type);
-        assertThat(mCompanionManager.getCompanionType(mDevice)).isEqualTo(type);
+        type = CompanionManager.COMPANION_TYPE_PRIMARY
+        loadCompanionInfoHelper(type)
+        assertThat(companionManager.getCompanionType(device)).isEqualTo(type)
 
-        type = CompanionManager.COMPANION_TYPE_SECONDARY;
-        loadCompanionInfoHelper(type);
-        assertThat(mCompanionManager.getCompanionType(mDevice)).isEqualTo(type);
+        type = CompanionManager.COMPANION_TYPE_SECONDARY
+        loadCompanionInfoHelper(type)
+        assertThat(companionManager.getCompanionType(device)).isEqualTo(type)
     }
 
     @Test
-    public void testGetGattConnParameterPrimary() {
-        loadCompanionInfoHelper(CompanionManager.COMPANION_TYPE_PRIMARY);
-        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_HIGH);
-        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_BALANCED);
-        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_LOW_POWER);
+    fun testGetGattConnParameterPrimary() {
+        loadCompanionInfoHelper(CompanionManager.COMPANION_TYPE_PRIMARY)
+        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_HIGH)
+        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_BALANCED)
+        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_LOW_POWER)
 
-        loadCompanionInfoHelper(CompanionManager.COMPANION_TYPE_SECONDARY);
-        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_HIGH);
-        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_BALANCED);
-        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_LOW_POWER);
+        loadCompanionInfoHelper(CompanionManager.COMPANION_TYPE_SECONDARY)
+        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_HIGH)
+        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_BALANCED)
+        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_LOW_POWER)
 
-        loadCompanionInfoHelper(CompanionManager.COMPANION_TYPE_NONE);
-        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_HIGH);
-        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_BALANCED);
-        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_LOW_POWER);
-        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_DCK);
+        loadCompanionInfoHelper(CompanionManager.COMPANION_TYPE_NONE)
+        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_HIGH)
+        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_BALANCED)
+        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_LOW_POWER)
+        checkReasonableConnParameterHelper(BluetoothGatt.CONNECTION_PRIORITY_DCK)
     }
 
-    private void loadCompanionInfoHelper(int companionType) {
-        final String address = mDevice.getAddress();
+    private fun loadCompanionInfoHelper(companionType: Int) {
+        val address = device.address
         doReturn(address)
-                .when(mSharedPreferences)
-                .getString(eq(CompanionManager.COMPANION_DEVICE_KEY), anyString());
+            .whenever(sharedPreferences)
+            .getString(eq(CompanionManager.COMPANION_DEVICE_KEY), any<String>())
         doReturn(companionType)
-                .when(mSharedPreferences)
-                .getInt(eq(CompanionManager.COMPANION_TYPE_KEY), anyInt());
-        mCompanionManager.loadCompanionInfo();
+            .whenever(sharedPreferences)
+            .getInt(eq(CompanionManager.COMPANION_TYPE_KEY), any<Int>())
+        companionManager.loadCompanionInfo()
     }
 
-    private void checkReasonableConnParameterHelper(int priority) {
+    private fun checkReasonableConnParameterHelper(priority: Int) {
         // Max/Min values from the Bluetooth spec Version 5.3 | Vol 4, Part E | 7.8.18
-        final int minInterval = 6; // 0x0006
-        final int maxInterval = 3200; // 0x0C80
-        final int minLatency = 0; // 0x0000
-        final int maxLatency = 499; // 0x01F3
-        final int minTimeout = 500; // 0x01F4
-        final int maxTimeout = 3200; // 0x0C80
+        val minInterval = 6 // 0x0006
+        val maxInterval = 3200 // 0x0C80
+        val minLatency = 0 // 0x0000
+        val maxLatency = 499 // 0x01F3
+        val minTimeout = 500 // 0x01F4
+        val maxTimeout = 3200 // 0x0C80
 
-        int min =
-                mCompanionManager.getGattConnParameters(
-                        mDevice, CompanionManager.GATT_CONN_INTERVAL_MIN, priority);
-        int max =
-                mCompanionManager.getGattConnParameters(
-                        mDevice, CompanionManager.GATT_CONN_INTERVAL_MAX, priority);
-        int latency =
-                mCompanionManager.getGattConnParameters(
-                        mDevice, CompanionManager.GATT_CONN_LATENCY, priority);
-        int timeout = mCompanionManager.getGattSupervisionTimeout(mDevice);
+        val min = companionManager.getGattConnParameters(device, GATT_CONN_INTERVAL_MIN, priority)
+        val max = companionManager.getGattConnParameters(device, GATT_CONN_INTERVAL_MAX, priority)
+        val latency = companionManager.getGattConnParameters(device, GATT_CONN_LATENCY, priority)
+        val timeout = companionManager.getGattSupervisionTimeout(device)
 
-        assertThat(max).isAtLeast(min);
-        assertThat(max).isAtLeast(minInterval);
-        assertThat(min).isAtLeast(minInterval);
-        assertThat(max).isAtMost(maxInterval);
-        assertThat(min).isAtMost(maxInterval);
-        assertThat(latency).isAtLeast(minLatency);
-        assertThat(latency).isAtMost(maxLatency);
-        assertThat(timeout).isAtLeast(minTimeout);
-        assertThat(timeout).isAtMost(maxTimeout);
+        assertThat(max).isAtLeast(min)
+        assertThat(max).isAtLeast(minInterval)
+        assertThat(min).isAtLeast(minInterval)
+        assertThat(max).isAtMost(maxInterval)
+        assertThat(min).isAtMost(maxInterval)
+        assertThat(latency).isAtLeast(minLatency)
+        assertThat(latency).isAtMost(maxLatency)
+        assertThat(timeout).isAtLeast(minTimeout)
+        assertThat(timeout).isAtMost(maxTimeout)
     }
 }
