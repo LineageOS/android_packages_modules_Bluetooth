6685f4d3af15edf9bc254f20f0ea00baaba280ae
Tabish Rizvi <tabishrizvi@google.com>
2026-02-10 15:43:12 -0800

    25Q4 flag cleanup: abs_volume_sdp_conflict
    Bug: 336915955
    Test: m .
    Flag: EXEMPT FLAG_REMOVAL
    Change-Id: Iae6c6cdc13fce3fdbe654aa0d2db8bdba60425ba

--- system/profile/avrcp/connection_handler.cc
+++ system/profile/avrcp/connection_handler.cc
@@ -167,19 +167,17 @@ bool ConnectionHandler::ConnectDevice(const RawAddress& bdaddr) {
 
     instance_->feature_map_[bdaddr] = features;
 
-    if (com_android_bluetooth_flags_abs_volume_sdp_conflict()) {
-      // Peer may connect avrcp during SDP. Check the connection state when
-      // SDP completed to resolve the conflict.
-      for (const auto& pair : instance_->device_map_) {
-        if (bdaddr == pair.second->GetAddress()) {
-          log::warn("Connected by peer device with address {}", bdaddr);
-          if (features & BTA_AV_FEAT_ADV_CTRL) {
-            pair.second->RegisterVolumeChanged();
-          } else if (instance_->vol_ != nullptr) {
-            instance_->vol_->DeviceConnected(pair.second->GetAddress());
-          }
-          return;
+    // Peer may connect avrcp during SDP. Check the connection state when SDP completed to resolve
+    // the conflict.
+    for (const auto& pair : instance_->device_map_) {
+      if (bdaddr == pair.second->GetAddress()) {
+        log::warn("Connected by peer device with address {}", bdaddr);
+        if (features & BTA_AV_FEAT_ADV_CTRL) {
+          pair.second->RegisterVolumeChanged();
+        } else if (instance_->vol_ != nullptr) {
+          instance_->vol_->DeviceConnected(pair.second->GetAddress());
         }
+        return;
       }
     }
     instance_->AvrcpConnect(true, bdaddr);
