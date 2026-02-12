50e124b30649736230fdddb24287d1dd1bae7741
Ömer Faruk Yılmaz <ofy@google.com>
2026-02-12 22:53:15 +0900

    Convert AdapterPropertiesTest to Kotlin
    - Update Mockito usage to mockito-kotlin syntax
    - Remove Hungarian notation ('m' prefix) from variable names
    - Replace Util functions with Kotlin Idiomatic versions
    
    Bug: 404652736
    Test: atest AdapterPropertiesTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: I30f2cb5645a825102aa6ce283c16b339048a2108

--- android/app/tests/unit/src/com/android/bluetooth/btservice/AdapterPropertiesTest.kt
+++ android/app/tests/unit/src/com/android/bluetooth/btservice/AdapterPropertiesTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,237 +14,214 @@
  * limitations under the License.
  */
 
-package com.android.bluetooth.btservice;
+package com.android.bluetooth.btservice
 
-import static com.android.bluetooth.TestUtils.mockGetBluetoothManager;
-import static com.android.bluetooth.TestUtils.mockGetSystemService;
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothDevice.DEVICE_TYPE_LE
+import android.bluetooth.BluetoothManager
+import android.companion.CompanionDeviceManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.HandlerThread
+import android.platform.test.annotations.DisableFlags
+import android.platform.test.flag.junit.SetFlagsRule
+import androidx.test.filters.MediumTest
+import androidx.test.platform.app.InstrumentationRegistry
+import com.android.bluetooth.Utils
+import com.android.bluetooth.flags.Flags
+import com.android.bluetooth.mockBluetoothManager
+import com.android.bluetooth.mockGetSystemService
+import com.android.bluetooth.mockPackageManager
+import com.android.tests.bluetooth.FlagsWrapper
+import com.android.tests.bluetooth.MockitoRule
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.clearInvocations
+import org.mockito.Mockito.doCallRealMethod
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.verify
+import org.mockito.kotlin.any
+import org.mockito.kotlin.argumentCaptor
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.whenever
+import platform.test.runner.parameterized.ParameterizedAndroidJunit4
+import platform.test.runner.parameterized.Parameters
 
-import static com.google.common.truth.Truth.assertThat;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.clearInvocations;
-import static org.mockito.Mockito.doCallRealMethod;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
-
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothManager;
-import android.companion.CompanionDeviceManager;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.HandlerThread;
-import android.platform.test.annotations.DisableFlags;
-import android.platform.test.flag.junit.SetFlagsRule;
-
-import androidx.test.filters.MediumTest;
-import androidx.test.platform.app.InstrumentationRegistry;
-
-import com.android.bluetooth.Utils;
-import com.android.bluetooth.flags.Flags;
-import com.android.tests.bluetooth.FlagsWrapper;
-import com.android.tests.bluetooth.MockitoRule;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-
-import platform.test.runner.parameterized.ParameterizedAndroidJunit4;
-import platform.test.runner.parameterized.Parameters;
-
-import java.util.List;
-
-/** Test cases for {@link AdapterProperties}. */
+/** Test cases for [AdapterProperties]. */
 @MediumTest
-@RunWith(ParameterizedAndroidJunit4.class)
-public class AdapterPropertiesTest {
-    private static final byte[] TEST_BT_ADDR_BYTES = {00, 11, 22, 33, 44, 55};
-    private static final byte[] TEST_BT_ADDR_BYTES_2 = {00, 11, 22, 33, 44, 66};
+@RunWith(ParameterizedAndroidJunit4::class)
+class AdapterPropertiesTest(flags: FlagsWrapper) {
+    @get:Rule val mockitoRule = MockitoRule()
+    @get:Rule val setFlagsRule = SetFlagsRule(flags.flags)
 
-    @Rule public final MockitoRule mMockitoRule = new MockitoRule();
-    @Rule public final SetFlagsRule mSetFlagsRule;
+    @Mock private lateinit var adapterService: AdapterService
+    @Mock private lateinit var packageManager: PackageManager
+    @Mock private lateinit var nativeInterface: AdapterNativeInterface
 
-    @Mock private AdapterService mAdapterService;
-    @Mock private PackageManager mPackageManager;
-    @Mock private AdapterNativeInterface mNativeInterface;
-
-    private AdapterProperties mAdapterProperties;
-    private RemoteDevices mRemoteDevices;
-    private HandlerThread mHandlerThread;
-
-    @Parameters(name = "{0}")
-    public static List<FlagsWrapper> getParams() {
-        return FlagsWrapper.progressionOf();
-    }
-
-    public AdapterPropertiesTest(FlagsWrapper flags) {
-        mSetFlagsRule = new SetFlagsRule(flags.getFlags());
-    }
+    private lateinit var adapterProperties: AdapterProperties
+    private lateinit var remoteDevices: RemoteDevices
+    private lateinit var handlerThread: HandlerThread
 
     @Before
-    public void setUp() throws Exception {
-        doReturn(mNativeInterface).when(mAdapterService).getNative();
-        mHandlerThread = new HandlerThread("RemoteDevicesTestHandlerThread");
-        mHandlerThread.start();
+    fun setUp() {
+        doReturn(nativeInterface).whenever(adapterService).native
+        handlerThread = HandlerThread("RemoteDevicesTestHandlerThread")
+        handlerThread.start()
 
-        mockGetBluetoothManager(mAdapterService);
-        mockGetSystemService(mAdapterService, CompanionDeviceManager.class);
-        doReturn(mPackageManager).when(mAdapterService).getPackageManager();
-        doCallRealMethod().when(mAdapterService).getBrEdrAddress(any(BluetoothDevice.class));
-        doCallRealMethod().when(mAdapterService).getBrEdrAddress(any(String.class));
+        adapterService.mockBluetoothManager()
+        adapterService.mockGetSystemService<CompanionDeviceManager>()
+        adapterService.mockPackageManager(packageManager)
+        doCallRealMethod().whenever(adapterService).getBrEdrAddress(any<BluetoothDevice>())
+        doCallRealMethod().whenever(adapterService).getBrEdrAddress(any<String>())
         doReturn(Utils.getAddressStringFromByte(TEST_BT_ADDR_BYTES))
-                .when(mAdapterService)
-                .getIdentityAddress(Utils.getAddressStringFromByte(TEST_BT_ADDR_BYTES));
+            .whenever(adapterService)
+            .getIdentityAddress(Utils.getAddressStringFromByte(TEST_BT_ADDR_BYTES))
         doReturn(Utils.getAddressStringFromByte(TEST_BT_ADDR_BYTES))
-                .when(mAdapterService)
-                .getIdentityAddress(Utils.getAddressStringFromByte(TEST_BT_ADDR_BYTES_2));
-        doReturn(true).when(mNativeInterface).removeBond(any(byte[].class));
+            .whenever(adapterService)
+            .getIdentityAddress(Utils.getAddressStringFromByte(TEST_BT_ADDR_BYTES_2))
+        doReturn(true).whenever(nativeInterface).removeBond(any<ByteArray>())
 
-        mRemoteDevices = new RemoteDevices(mAdapterService, mHandlerThread.getLooper());
-        verify(mAdapterService).getSystemService(BluetoothManager.class);
+        remoteDevices = RemoteDevices(adapterService, handlerThread.looper)
+        verify(adapterService).getSystemService(BluetoothManager::class.java)
 
-        mRemoteDevices.reset();
+        remoteDevices.reset()
 
-        doReturn(mHandlerThread.getLooper()).when(mAdapterService).getMainLooper();
-        doReturn(InstrumentationRegistry.getInstrumentation().getContext().getResources())
-                .when(mAdapterService)
-                .getResources();
+        doReturn(handlerThread.looper).whenever(adapterService).mainLooper
+        doReturn(InstrumentationRegistry.getInstrumentation().context.resources)
+            .whenever(adapterService)
+            .resources
 
         // Must be called to initialize services
-        mAdapterProperties =
-                new AdapterProperties(mAdapterService, mRemoteDevices, mHandlerThread.getLooper());
-        mAdapterProperties.init();
+        adapterProperties = AdapterProperties(adapterService, remoteDevices, handlerThread.looper)
+        adapterProperties.init()
     }
 
     @Test
-    public void testCleanupPrevBondRecordsFor() {
-        mRemoteDevices.reset();
-        mRemoteDevices
-                .addDeviceProperties(TEST_BT_ADDR_BYTES)
-                .setDeviceType(BluetoothDevice.DEVICE_TYPE_LE);
-        mRemoteDevices
-                .addDeviceProperties(TEST_BT_ADDR_BYTES_2)
-                .setDeviceType(BluetoothDevice.DEVICE_TYPE_LE);
-
-        BluetoothDevice device1, device2;
-        device1 = mRemoteDevices.getDevice(TEST_BT_ADDR_BYTES);
-        device2 = mRemoteDevices.getDevice(TEST_BT_ADDR_BYTES_2);
+    fun testCleanupPrevBondRecordsFor() {
+        remoteDevices.reset()
+        remoteDevices.addDeviceProperties(TEST_BT_ADDR_BYTES).deviceType = DEVICE_TYPE_LE
+        remoteDevices.addDeviceProperties(TEST_BT_ADDR_BYTES_2).deviceType = DEVICE_TYPE_LE
+        val device1 = remoteDevices.getDevice(TEST_BT_ADDR_BYTES)
+        val device2 = remoteDevices.getDevice(TEST_BT_ADDR_BYTES_2)
 
         // Bond record for device1 should be deleted when pairing with device2
         // as they are same device (have same identity address)
-        mAdapterProperties.onBondStateChanged(device1, BluetoothDevice.BOND_BONDED);
-        mAdapterProperties.onBondStateChanged(device2, BluetoothDevice.BOND_BONDED);
-        assertThat(mAdapterProperties.getBondedDevices().length).isEqualTo(1);
-        assertThat(mAdapterProperties.getBondedDevices()[0].getAddress())
-                .isEqualTo(Utils.getAddressStringFromByte(TEST_BT_ADDR_BYTES_2));
+        adapterProperties.onBondStateChanged(device1, BluetoothDevice.BOND_BONDED)
+        adapterProperties.onBondStateChanged(device2, BluetoothDevice.BOND_BONDED)
+        assertThat(adapterProperties.getBondedDevices().size).isEqualTo(1)
+        assertThat(adapterProperties.getBondedDevices()[0].address)
+            .isEqualTo(Utils.getAddressStringFromByte(TEST_BT_ADDR_BYTES_2))
     }
 
     @Test
     @DisableFlags(Flags.FLAG_SET_NAME_IN_SYSTEM_SERVER)
-    public void setName_shortName_isEqual() {
-        StringBuilder builder = new StringBuilder();
-        String stringName = "Wonderful Bluetooth Name Using utf8";
-        builder.append(stringName);
-        builder.append(Character.toChars(0x20AC));
+    fun setName_shortName_isEqual() {
+        val builder = StringBuilder()
+        val stringName = "Wonderful Bluetooth Name Using utf8"
+        builder.append(stringName)
+        builder.append(Character.toChars(0x20AC))
 
-        String initial = builder.toString();
+        val initial = builder.toString()
 
-        final ArgumentCaptor<byte[]> argumentName = ArgumentCaptor.forClass(byte[].class);
-
-        mAdapterProperties.setName(initial);
-        verify(mNativeInterface)
-                .setAdapterProperty(
-                        eq(AbstractionLayer.BT_PROPERTY_BDNAME), argumentName.capture());
-
-        assertThat(argumentName.getValue()).isEqualTo(initial.getBytes());
+        val argumentName = argumentCaptor<ByteArray>()
+        adapterProperties.setName(initial)
+        verify(nativeInterface)
+            .setAdapterProperty(eq(AbstractionLayer.BT_PROPERTY_BDNAME), argumentName.capture())
+        assertThat(argumentName.firstValue).isEqualTo(initial.toByteArray())
     }
 
     @Test
     @DisableFlags(Flags.FLAG_SET_NAME_IN_SYSTEM_SERVER)
-    public void setName_tooLongName_isTruncated() {
-        StringBuilder builder = new StringBuilder();
-        String stringName = "Wonderful Bluetooth Name Using utf8 ... But this name is too long";
-        builder.append(stringName);
+    fun setName_tooLongName_isTruncated() {
+        val builder = StringBuilder()
+        val stringName = "Wonderful Bluetooth Name Using utf8 ... But this name is too long"
+        builder.append(stringName)
 
-        int n = 300;
-        for (int i = 0; i < 2 * n; i++) {
-            builder.append(Character.toChars(0x20AC));
+        val n = 300
+        for (i in 0..<2 * n) {
+            builder.append(Character.toChars(0x20AC))
         }
 
-        String initial = builder.toString();
+        val initial = builder.toString()
 
-        final ArgumentCaptor<byte[]> argumentName = ArgumentCaptor.forClass(byte[].class);
-
-        mAdapterProperties.setName(initial);
-        verify(mNativeInterface)
-                .setAdapterProperty(
-                        eq(AbstractionLayer.BT_PROPERTY_BDNAME), argumentName.capture());
-
-        byte[] name = argumentName.getValue();
-
-        assertThat(name.length).isLessThan(initial.getBytes().length);
-
-        assertThat(initial).startsWith(new String(name));
+        val argumentName = argumentCaptor<ByteArray>()
+        adapterProperties.setName(initial)
+        verify(nativeInterface)
+            .setAdapterProperty(eq(AbstractionLayer.BT_PROPERTY_BDNAME), argumentName.capture())
+        val name = argumentName.firstValue
+        assertThat(name.size).isLessThan(initial.toByteArray().size)
+        assertThat(initial).startsWith(String(name))
     }
 
     @Test
-    public void isNativeDiscovering_initialValueIsFalse() {
+    fun isNativeDiscovering_initialValueIsFalse() {
         // Verifies that the default discovery state is false.
-        assertThat(mAdapterProperties.isNativeDiscovering()).isFalse();
+        assertThat(adapterProperties.isNativeDiscovering).isFalse()
     }
 
     @Test
-    public void discoveryStateChangeCallback_Started_setsNativeDiscoveringTrue() {
+    fun discoveryStateChangeCallback_Started_setsNativeDiscoveringTrue() {
         // Verifies that starting discovery updates the state and broadcasts the correct intent.
-        assertThat(mAdapterProperties.isNativeDiscovering()).isFalse();
+        assertThat(adapterProperties.isNativeDiscovering).isFalse()
 
         // Trigger discovery started callback.
-        mAdapterProperties.discoveryStateChangeCallback(AbstractionLayer.BT_DISCOVERY_STARTED);
+        adapterProperties.discoveryStateChangeCallback(AbstractionLayer.BT_DISCOVERY_STARTED)
 
         // Assert that native discovering is now true.
-        assertThat(mAdapterProperties.isNativeDiscovering()).isTrue();
+        assertThat(adapterProperties.isNativeDiscovering).isTrue()
 
         // Verify that an ACTION_DISCOVERY_STARTED intent was broadcast.
-        final ArgumentCaptor<Intent> intentCaptor = ArgumentCaptor.forClass(Intent.class);
-        verify(mAdapterService)
-                .sendBroadcast(
-                        intentCaptor.capture(),
-                        eq(android.Manifest.permission.BLUETOOTH_SCAN),
-                        any());
-        assertThat(intentCaptor.getValue().getAction())
-                .isEqualTo(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        val intentCaptor = argumentCaptor<Intent>()
+        verify(adapterService)
+            .sendBroadcast(
+                intentCaptor.capture(),
+                eq(android.Manifest.permission.BLUETOOTH_SCAN),
+                any<Bundle>(),
+            )
+        assertThat(intentCaptor.firstValue.action)
+            .isEqualTo(BluetoothAdapter.ACTION_DISCOVERY_STARTED)
     }
 
     @Test
-    public void discoveryStateChangeCallback_Stopped_setsNativeDiscoveringFalse() {
+    fun discoveryStateChangeCallback_Stopped_setsNativeDiscoveringFalse() {
         // Verifies that stopping discovery updates the state and broadcasts the correct intent.
         // Start discovery first to ensure the state changes.
-        mAdapterProperties.discoveryStateChangeCallback(AbstractionLayer.BT_DISCOVERY_STARTED);
-        assertThat(mAdapterProperties.isNativeDiscovering()).isTrue();
+        adapterProperties.discoveryStateChangeCallback(AbstractionLayer.BT_DISCOVERY_STARTED)
+        assertThat(adapterProperties.isNativeDiscovering).isTrue()
         // Clear invocations on the mock from the setup call to isolate verification.
-        clearInvocations(mAdapterService);
+        clearInvocations(adapterService)
 
         // Trigger discovery stopped callback.
-        mAdapterProperties.discoveryStateChangeCallback(AbstractionLayer.BT_DISCOVERY_STOPPED);
+        adapterProperties.discoveryStateChangeCallback(AbstractionLayer.BT_DISCOVERY_STOPPED)
 
         // Assert that native discovering is now false.
-        assertThat(mAdapterProperties.isNativeDiscovering()).isFalse();
+        assertThat(adapterProperties.isNativeDiscovering).isFalse()
 
         // Verify that clearDiscoveryData is called.
-        verify(mAdapterService).clearDiscoveryData();
+        verify(adapterService).clearDiscoveryData()
 
         // Verify that an ACTION_DISCOVERY_FINISHED intent was broadcast.
-        final ArgumentCaptor<Intent> intentCaptor = ArgumentCaptor.forClass(Intent.class);
-        verify(mAdapterService)
-                .sendBroadcast(
-                        intentCaptor.capture(),
-                        eq(android.Manifest.permission.BLUETOOTH_SCAN),
-                        any());
-        assertThat(intentCaptor.getValue().getAction())
-                .isEqualTo(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        val intentCaptor = argumentCaptor<Intent>()
+        verify(adapterService)
+            .sendBroadcast(
+                intentCaptor.capture(),
+                eq(android.Manifest.permission.BLUETOOTH_SCAN),
+                any<Bundle>(),
+            )
+        assertThat(intentCaptor.firstValue.action)
+            .isEqualTo(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+    }
+
+    companion object {
+        private val TEST_BT_ADDR_BYTES = byteArrayOf(0, 11, 22, 33, 44, 55)
+        private val TEST_BT_ADDR_BYTES_2 = byteArrayOf(0, 11, 22, 33, 44, 66)
+
+        @JvmStatic @Parameters(name = "{0}") fun getParams() = FlagsWrapper.progressionOf()
     }
 }
