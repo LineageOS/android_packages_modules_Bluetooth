96ffa9287d95b8d59bc4f1c847f37e5abece6121
Ömer Faruk Yılmaz <ofy@google.com>
2026-02-18 20:37:17 +0900

    Convert HearingAidServiceBinderTest to Kotlin
    - Update Mockito usage to mockito-kotlin syntax
    - Remove Hungarian notation ('m' prefix) from variable names
    - Replace Util functions with Kotlin Idiomatic versions
    
    Bug: 404652736
    Test: atest HearingAidServiceBinderTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: I635c5ed7968b9ce5abe99bf8fdb4d50e8d5a6c93

--- android/app/tests/unit/src/com/android/bluetooth/hearingaid/HearingAidServiceBinderTest.kt
+++ android/app/tests/unit/src/com/android/bluetooth/hearingaid/HearingAidServiceBinderTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,178 +14,166 @@
  * limitations under the License.
  */
 
-package com.android.bluetooth.hearingaid;
+package com.android.bluetooth.hearingaid
 
-import static android.bluetooth.BluetoothProfile.CONNECTION_POLICY_ALLOWED;
-import static android.bluetooth.BluetoothProfile.CONNECTION_POLICY_FORBIDDEN;
-import static android.bluetooth.BluetoothProfile.STATE_CONNECTED;
-import static android.bluetooth.BluetoothProfile.STATE_DISCONNECTED;
+import android.bluetooth.BluetoothHearingAid
+import android.bluetooth.BluetoothProfile.CONNECTION_POLICY_ALLOWED
+import android.bluetooth.BluetoothProfile.CONNECTION_POLICY_FORBIDDEN
+import android.bluetooth.BluetoothProfile.STATE_CONNECTED
+import android.bluetooth.BluetoothProfile.STATE_DISCONNECTED
+import android.content.AttributionSource
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.android.bluetooth.getTestDevice
+import com.android.tests.bluetooth.MockitoRule
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.verify
+import org.mockito.kotlin.whenever
 
-import static com.android.bluetooth.TestUtils.getTestDevice;
-
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
-
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothHearingAid;
-import android.content.AttributionSource;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.SmallTest;
-
-import com.android.tests.bluetooth.MockitoRule;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/** Test cases for {@link HearingAidServiceBinder}. */
+/** Test cases for [HearingAidServiceBinder]. */
 @SmallTest
-@RunWith(AndroidJUnit4.class)
-public class HearingAidServiceBinderTest {
-    @Rule public final MockitoRule mMockitoRule = new MockitoRule();
+@RunWith(AndroidJUnit4::class)
+class HearingAidServiceBinderTest {
+    @get:Rule val mockitoRule = MockitoRule()
 
-    @Mock private AttributionSource mSource;
-    @Mock private HearingAidService mService;
+    @Mock private lateinit var source: AttributionSource
+    @Mock private lateinit var service: HearingAidService
 
-    private HearingAidServiceBinder mBinder;
-    private final BluetoothDevice mDevice = getTestDevice(0);
+    private val device = getTestDevice(0)
+
+    private lateinit var binder: HearingAidServiceBinder
 
     @Before
-    public void setUp() throws Exception {
-        doReturn(true).when(mService).isAvailable();
-        mBinder = new HearingAidServiceBinder(mService);
+    fun setUp() {
+        doReturn(true).whenever(service).isAvailable
+        binder = HearingAidServiceBinder(service)
     }
 
     @Test
-    public void connect() {
-        mBinder.connect(mDevice, mSource);
-        verify(mService).connect(mDevice);
+    fun connect() {
+        binder.connect(device, source)
+        verify(service).connect(device)
     }
 
     @Test
-    public void disconnect() {
-        mBinder.disconnect(mDevice, mSource);
-        verify(mService).disconnect(mDevice);
+    fun disconnect() {
+        binder.disconnect(device, source)
+        verify(service).disconnect(device)
     }
 
     @Test
-    public void getConnectedDevices() {
-        List<BluetoothDevice> connectedDevices = new ArrayList<>();
-        connectedDevices.add(mDevice);
-        doReturn(connectedDevices).when(mService).getConnectedDevices();
+    fun getConnectedDevices() {
+        val connectedDevices = listOf(device)
+        doReturn(connectedDevices).whenever(service).connectedDevices
 
-        mBinder.getConnectedDevices(mSource);
-        verify(mService).getConnectedDevices();
+        binder.getConnectedDevices(source)
+        verify(service).connectedDevices
     }
 
     @Test
-    public void getDevicesMatchingConnectionStates() {
-        int[] states = new int[] {STATE_CONNECTED, STATE_DISCONNECTED};
-        List<BluetoothDevice> devices = new ArrayList<>();
-        devices.add(mDevice);
-        doReturn(devices).when(mService).getDevicesMatchingConnectionStates(states);
+    fun getDevicesMatchingConnectionStates() {
+        val states = intArrayOf(STATE_CONNECTED, STATE_DISCONNECTED)
+        val devices = listOf(device)
+        doReturn(devices).whenever(service).getDevicesMatchingConnectionStates(states)
 
-        mBinder.getDevicesMatchingConnectionStates(states, mSource);
-        verify(mService).getDevicesMatchingConnectionStates(states);
+        binder.getDevicesMatchingConnectionStates(states, source)
+        verify(service).getDevicesMatchingConnectionStates(states)
     }
 
     @Test
-    public void getConnectionState() {
-        doReturn(STATE_CONNECTED).when(mService).getConnectionState(mDevice);
+    fun getConnectionState() {
+        doReturn(STATE_CONNECTED).whenever(service).getConnectionState(device)
 
-        mBinder.getConnectionState(mDevice, mSource);
-        verify(mService).getConnectionState(mDevice);
+        binder.getConnectionState(device, source)
+        verify(service).getConnectionState(device)
     }
 
     @Test
-    public void setActiveDevice() {
-        mBinder.setActiveDevice(mDevice, mSource);
-        verify(mService).setActiveDevice(mDevice);
+    fun setActiveDevice() {
+        binder.setActiveDevice(device, source)
+        verify(service).setActiveDevice(device)
     }
 
     @Test
-    public void removeActiveDevice() {
-        mBinder.setActiveDevice(null, mSource);
-        verify(mService).removeActiveDevice(false);
+    fun removeActiveDevice() {
+        binder.setActiveDevice(null, source)
+        verify(service).removeActiveDevice(false)
     }
 
     @Test
-    public void getActiveDevices() {
-        List<BluetoothDevice> activeDevices = new ArrayList<>();
-        activeDevices.add(mDevice);
-        doReturn(activeDevices).when(mService).getActiveDevices();
+    fun getActiveDevices() {
+        val activeDevices = listOf(device)
+        doReturn(activeDevices).whenever(service).getActiveDevices()
 
-        mBinder.getActiveDevices(mSource);
-        verify(mService).getActiveDevices();
+        binder.getActiveDevices(source)
+        verify(service).getActiveDevices()
     }
 
     @Test
-    public void setConnectionPolicy() {
-        mBinder.setConnectionPolicy(mDevice, CONNECTION_POLICY_ALLOWED, mSource);
-        verify(mService).setConnectionPolicy(mDevice, CONNECTION_POLICY_ALLOWED);
+    fun setConnectionPolicy() {
+        binder.setConnectionPolicy(device, CONNECTION_POLICY_ALLOWED, source)
+        verify(service).setConnectionPolicy(device, CONNECTION_POLICY_ALLOWED)
     }
 
     @Test
-    public void getConnectionPolicy() {
-        doReturn(CONNECTION_POLICY_FORBIDDEN).when(mService).getConnectionPolicy(mDevice);
+    fun getConnectionPolicy() {
+        doReturn(CONNECTION_POLICY_FORBIDDEN).whenever(service).getConnectionPolicy(device)
 
-        mBinder.getConnectionPolicy(mDevice, mSource);
-        verify(mService).getConnectionPolicy(mDevice);
+        binder.getConnectionPolicy(device, source)
+        verify(service).getConnectionPolicy(device)
     }
 
     @Test
-    public void setVolume() {
-        int volume = 50;
+    fun setVolume() {
+        val volume = 50
 
-        mBinder.setVolume(volume, mSource);
-        verify(mService).setVolume(volume);
+        binder.setVolume(volume, source)
+        verify(service).setVolume(volume)
     }
 
     @Test
-    public void getHiSyncId() {
-        long hiSyncId = 1234567890L;
-        doReturn(hiSyncId).when(mService).getHiSyncId(mDevice);
+    fun getHiSyncId() {
+        val hiSyncId = 1234567890L
+        doReturn(hiSyncId).whenever(service).getHiSyncId(device)
 
-        mBinder.getHiSyncId(mDevice, mSource);
-        verify(mService).getHiSyncId(mDevice);
+        binder.getHiSyncId(device, source)
+        verify(service).getHiSyncId(device)
     }
 
     @Test
-    public void getDeviceSide() {
-        int side = BluetoothHearingAid.SIDE_LEFT;
-        doReturn(side).when(mService).getCapabilities(mDevice);
+    fun getDeviceSide() {
+        val side = BluetoothHearingAid.SIDE_LEFT
+        doReturn(side).whenever(service).getCapabilities(device)
 
-        mBinder.getDeviceSide(mDevice, mSource);
-        verify(mService).getCapabilities(mDevice);
+        binder.getDeviceSide(device, source)
+        verify(service).getCapabilities(device)
     }
 
     @Test
-    public void getDeviceMode() {
-        int mode = BluetoothHearingAid.MODE_BINAURAL;
-        doReturn(mode << 1).when(mService).getCapabilities(mDevice);
+    fun getDeviceMode() {
+        val mode = BluetoothHearingAid.MODE_BINAURAL
+        doReturn(mode shl 1).whenever(service).getCapabilities(device)
 
-        mBinder.getDeviceMode(mDevice, mSource);
-        verify(mService).getCapabilities(mDevice);
+        binder.getDeviceMode(device, source)
+        verify(service).getCapabilities(device)
     }
 
     @Test
-    public void getAdvertisementServiceData() {
-        BluetoothHearingAid.AdvertisementServiceData data =
-                new BluetoothHearingAid.AdvertisementServiceData(0, 0);
-        doReturn(data).when(mService).getAdvertisementServiceData(mDevice);
+    fun getAdvertisementServiceData() {
+        val data = BluetoothHearingAid.AdvertisementServiceData(0, 0)
+        doReturn(data).whenever(service).getAdvertisementServiceData(device)
 
-        mBinder.getAdvertisementServiceData(mDevice, mSource);
-        verify(mService).getAdvertisementServiceData(mDevice);
+        binder.getAdvertisementServiceData(device, source)
+        verify(service).getAdvertisementServiceData(device)
     }
 
     @Test
-    public void cleanup_doesNotCrash() {
-        mBinder.cleanup();
+    fun cleanup_doesNotCrash() {
+        binder.cleanup()
     }
 }
