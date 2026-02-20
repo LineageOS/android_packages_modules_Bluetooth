81596995d4fa0ca74c4a209c840378c845b2d794
Ömer Faruk Yılmaz <ofy@google.com>
2026-02-20 22:57:58 +0900

    Convert CsipSetCoordinatorServiceBinderTest to Kotlin
    - Update Mockito usage to mockito-kotlin syntax
    - Remove Hungarian notation ('m' prefix) from variable names
    - Replace Util functions with Kotlin Idiomatic versions
    
    Bug: 404652736
    Test: atest CsipSetCoordinatorServiceBinderTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: Ide3d345707b0650a4b27ab1176e92b801e4c48a5

--- android/app/tests/unit/src/com/android/bluetooth/csip/CsipSetCoordinatorServiceBinderTest.kt
+++ android/app/tests/unit/src/com/android/bluetooth/csip/CsipSetCoordinatorServiceBinderTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,121 +14,107 @@
  * limitations under the License.
  */
 
-package com.android.bluetooth.csip;
+package com.android.bluetooth.csip
 
-import static android.bluetooth.BluetoothProfile.CONNECTION_POLICY_ALLOWED;
-import static android.bluetooth.BluetoothProfile.STATE_CONNECTED;
+import android.bluetooth.BluetoothProfile.CONNECTION_POLICY_ALLOWED
+import android.bluetooth.BluetoothProfile.STATE_CONNECTED
+import android.bluetooth.IBluetoothCsipSetCoordinatorLockCallback
+import android.content.AttributionSource
+import android.os.ParcelUuid
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.android.bluetooth.getTestDevice
+import com.android.tests.bluetooth.MockitoRule
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 
-import static com.android.bluetooth.TestUtils.getTestDevice;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.IBluetoothCsipSetCoordinatorLockCallback;
-import android.content.AttributionSource;
-import android.os.ParcelUuid;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.SmallTest;
-
-import com.android.tests.bluetooth.MockitoRule;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-
-/** Test cases for {@link CsipSetCoordinatorServiceBinder}. */
+/** Test cases for [CsipSetCoordinatorServiceBinder]. */
 @SmallTest
-@RunWith(AndroidJUnit4.class)
-public class CsipSetCoordinatorServiceBinderTest {
+@RunWith(AndroidJUnit4::class)
+class CsipSetCoordinatorServiceBinderTest {
+    @get:Rule val mockitoRule = MockitoRule()
 
-    @Rule public final MockitoRule mMockitoRule = new MockitoRule();
+    @Mock private lateinit var source: AttributionSource
+    @Mock private lateinit var service: CsipSetCoordinatorService
 
-    @Mock private AttributionSource mSource;
-    @Mock private CsipSetCoordinatorService mService;
+    private val device = getTestDevice(45)
 
-    private final BluetoothDevice mDevice = getTestDevice(45);
-
-    private CsipSetCoordinatorServiceBinder mBinder;
+    private lateinit var binder: CsipSetCoordinatorServiceBinder
 
     @Before
-    public void setUp() throws Exception {
-        mBinder = new CsipSetCoordinatorServiceBinder(mService);
+    fun setUp() {
+        binder = CsipSetCoordinatorServiceBinder(service)
     }
 
     @Test
-    public void getConnectedDevices() {
-        mBinder.getConnectedDevices(mSource);
-        verify(mService).getConnectedDevices();
+    fun getConnectedDevices() {
+        binder.getConnectedDevices(source)
+        verify(service).connectedDevices
     }
 
     @Test
-    public void getDevicesMatchingConnectionStates() {
-        int[] states = new int[] {STATE_CONNECTED};
-
-        mBinder.getDevicesMatchingConnectionStates(states, mSource);
-        verify(mService).getDevicesMatchingConnectionStates(states);
+    fun getDevicesMatchingConnectionStates() {
+        val states = intArrayOf(STATE_CONNECTED)
+        binder.getDevicesMatchingConnectionStates(states, source)
+        verify(service).getDevicesMatchingConnectionStates(states)
     }
 
     @Test
-    public void getConnectionState() {
-        mBinder.getConnectionState(mDevice, mSource);
-        verify(mService).getConnectionState(mDevice);
+    fun getConnectionState() {
+        binder.getConnectionState(device, source)
+        verify(service).getConnectionState(device)
     }
 
     @Test
-    public void setConnectionPolicy() {
-        int connectionPolicy = CONNECTION_POLICY_ALLOWED;
-
-        mBinder.setConnectionPolicy(mDevice, connectionPolicy, mSource);
-        verify(mService).setConnectionPolicy(mDevice, connectionPolicy);
+    fun setConnectionPolicy() {
+        val connectionPolicy = CONNECTION_POLICY_ALLOWED
+        binder.setConnectionPolicy(device, connectionPolicy, source)
+        verify(service).setConnectionPolicy(device, connectionPolicy)
     }
 
     @Test
-    public void getConnectionPolicy() {
-        mBinder.getConnectionPolicy(mDevice, mSource);
-        verify(mService).getConnectionPolicy(mDevice);
+    fun getConnectionPolicy() {
+        binder.getConnectionPolicy(device, source)
+        verify(service).getConnectionPolicy(device)
     }
 
     @Test
-    public void lockGroup() {
-        int groupId = 100;
-        IBluetoothCsipSetCoordinatorLockCallback cb =
-                mock(IBluetoothCsipSetCoordinatorLockCallback.class);
-
-        mBinder.lockGroup(groupId, cb, mSource);
-        verify(mService).lockGroup(groupId, cb);
+    fun lockGroup() {
+        val groupId = 100
+        val cb = mock<IBluetoothCsipSetCoordinatorLockCallback>()
+        binder.lockGroup(groupId, cb, source)
+        verify(service).lockGroup(groupId, cb)
     }
 
     @Test
-    public void unlockGroup() {
-        ParcelUuid uuid = ParcelUuid.fromString("0000110A-0000-1000-8000-00805F9B34FB");
-
-        mBinder.unlockGroup(uuid, mSource);
-        verify(mService).unlockGroup(uuid.getUuid());
+    fun unlockGroup() {
+        val uuid = ParcelUuid.fromString("0000110A-0000-1000-8000-00805F9B34FB")
+        binder.unlockGroup(uuid, source)
+        verify(service).unlockGroup(uuid.uuid)
     }
 
     @Test
-    public void getAllGroupIds() {
-        ParcelUuid uuid = ParcelUuid.fromString("0000110A-0000-1000-8000-00805F9B34FB");
-
-        mBinder.getAllGroupIds(uuid, mSource);
-        verify(mService).getAllGroupIds(uuid);
+    fun getAllGroupIds() {
+        val uuid = ParcelUuid.fromString("0000110A-0000-1000-8000-00805F9B34FB")
+        binder.getAllGroupIds(uuid, source)
+        verify(service).getAllGroupIds(uuid)
     }
 
     @Test
-    public void getGroupUuidMapByDevice() {
-        mBinder.getGroupUuidMapByDevice(mDevice, mSource);
-        verify(mService).getGroupUuidMapByDevice(mDevice);
+    fun getGroupUuidMapByDevice() {
+        binder.getGroupUuidMapByDevice(device, source)
+        verify(service).getGroupUuidMapByDevice(device)
     }
 
     @Test
-    public void getDesiredGroupSize() {
-        int groupId = 100;
-        mBinder.getDesiredGroupSize(groupId, mSource);
-        verify(mService).getDesiredGroupSize(groupId);
+    fun getDesiredGroupSize() {
+        val groupId = 100
+        binder.getDesiredGroupSize(groupId, source)
+        verify(service).getDesiredGroupSize(groupId)
     }
 }
