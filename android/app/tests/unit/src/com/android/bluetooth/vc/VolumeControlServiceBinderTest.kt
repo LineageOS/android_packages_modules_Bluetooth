eff664598133e4533468b2aaf3e2246a8f68c633
Ömer Faruk Yılmaz <ofy@google.com>
2026-02-21 22:55:19 +0900

    Convert VolumeControlServiceBinderTest to Kotlin
    - Update Mockito usage to mockito-kotlin syntax
    - Remove Hungarian notation ('m' prefix) from variable names
    - Replace Util functions with Kotlin Idiomatic versions
    
    Bug: 404652736
    Test: atest VolumeControlServiceBinderTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: Ic2984ba51f8d44596a21ed594f0dcc5bc281e501

--- android/app/tests/unit/src/com/android/bluetooth/vc/VolumeControlServiceBinderTest.kt
+++ android/app/tests/unit/src/com/android/bluetooth/vc/VolumeControlServiceBinderTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,252 +14,238 @@
  * limitations under the License.
  */
 
-package com.android.bluetooth.vc;
+package com.android.bluetooth.vc
 
-import static android.bluetooth.BluetoothProfile.CONNECTION_POLICY_ALLOWED;
-import static android.bluetooth.BluetoothProfile.STATE_CONNECTED;
+import android.bluetooth.BluetoothProfile.CONNECTION_POLICY_ALLOWED
+import android.bluetooth.BluetoothProfile.STATE_CONNECTED
+import android.bluetooth.IAudioInputCallback
+import android.content.AttributionSource
+import android.platform.test.flag.junit.SetFlagsRule
+import androidx.test.filters.SmallTest
+import com.android.bluetooth.getTestDevice
+import com.android.tests.bluetooth.FlagsWrapper
+import com.android.tests.bluetooth.MockitoRule
+import java.util.function.Consumer
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doAnswer
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import platform.test.runner.parameterized.ParameterizedAndroidJunit4
+import platform.test.runner.parameterized.Parameters
 
-import static com.android.bluetooth.TestUtils.getTestDevice;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.IAudioInputCallback;
-import android.content.AttributionSource;
-import android.platform.test.flag.junit.SetFlagsRule;
-
-import androidx.test.filters.SmallTest;
-
-import com.android.tests.bluetooth.FlagsWrapper;
-import com.android.tests.bluetooth.MockitoRule;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-
-import platform.test.runner.parameterized.ParameterizedAndroidJunit4;
-import platform.test.runner.parameterized.Parameters;
-
-import java.util.List;
-import java.util.function.Consumer;
-
-/** Test cases for {@link VolumeControlServiceBinder}. */
+/** Test cases for [VolumeControlServiceBinder]. */
 @SmallTest
-@RunWith(ParameterizedAndroidJunit4.class)
-public class VolumeControlServiceBinderTest {
-    @Rule public final SetFlagsRule mSetFlagsRule;
-    @Rule public final MockitoRule mMockitoRule = new MockitoRule();
+@RunWith(ParameterizedAndroidJunit4::class)
+class VolumeControlServiceBinderTest(flags: FlagsWrapper) {
+    @get:Rule val setFlagsRule = SetFlagsRule(flags.flags)
+    @get:Rule val mockitoRule = MockitoRule()
 
-    @Mock private AttributionSource mSource;
-    @Mock private VolumeControlService mService;
+    @Mock private lateinit var source: AttributionSource
+    @Mock private lateinit var service: VolumeControlService
 
-    private final BluetoothDevice mDevice = getTestDevice(25);
+    private val device = getTestDevice(25)
 
-    private VolumeControlServiceBinder mBinder;
-
-    @Parameters(name = "{0}")
-    public static List<FlagsWrapper> getParams() {
-        return FlagsWrapper.progressionOf();
-    }
-
-    public VolumeControlServiceBinderTest(FlagsWrapper flags) {
-        mSetFlagsRule = new SetFlagsRule(flags.getFlags());
-    }
+    private lateinit var binder: VolumeControlServiceBinder
 
     @Before
-    public void setUp() throws Exception {
-        doReturn(true).when(mService).isAvailable();
-        doAnswer(
-                        inv -> {
-                            ((Consumer) inv.getArgument(0)).accept(mService);
-                            return null;
-                        })
-                .when(mService)
-                .post(any());
-        mBinder = new VolumeControlServiceBinder(mService);
+    fun setUp() {
+        doReturn(true).whenever(service).isAvailable
+        doAnswer { inv ->
+                (inv.getArgument(0) as Consumer<VolumeControlService>).accept(service)
+                null
+            }
+            .whenever(service)
+            .post(any())
+
+        binder = VolumeControlServiceBinder(service)
     }
 
     @Test
-    public void getConnectedDevices() {
-        mBinder.getConnectedDevices(mSource);
-        verify(mService).getConnectedDevices();
+    fun getConnectedDevices() {
+        binder.getConnectedDevices(source)
+        verify(service).connectedDevices
     }
 
     @Test
-    public void getDevicesMatchingConnectionStates() {
-        int[] states = new int[] {STATE_CONNECTED};
+    fun getDevicesMatchingConnectionStates() {
+        val states = intArrayOf(STATE_CONNECTED)
 
-        mBinder.getDevicesMatchingConnectionStates(states, mSource);
-        verify(mService).getDevicesMatchingConnectionStates(states);
+        binder.getDevicesMatchingConnectionStates(states, source)
+        verify(service).getDevicesMatchingConnectionStates(states)
     }
 
     @Test
-    public void getConnectionState() {
-        mBinder.getConnectionState(mDevice, mSource);
-        verify(mService).getConnectionState(mDevice);
+    fun getConnectionState() {
+        binder.getConnectionState(device, source)
+        verify(service).getConnectionState(device)
     }
 
     @Test
-    public void setConnectionPolicy() {
-        int connectionPolicy = CONNECTION_POLICY_ALLOWED;
+    fun setConnectionPolicy() {
+        val connectionPolicy = CONNECTION_POLICY_ALLOWED
 
-        mBinder.setConnectionPolicy(mDevice, connectionPolicy, mSource);
-        verify(mService).setConnectionPolicy(mDevice, connectionPolicy);
+        binder.setConnectionPolicy(device, connectionPolicy, source)
+        verify(service).setConnectionPolicy(device, connectionPolicy)
     }
 
     @Test
-    public void getConnectionPolicy() {
-        mBinder.getConnectionPolicy(mDevice, mSource);
-        verify(mService).getConnectionPolicy(mDevice);
+    fun getConnectionPolicy() {
+        binder.getConnectionPolicy(device, source)
+        verify(service).getConnectionPolicy(device)
     }
 
     @Test
-    public void isVolumeOffsetAvailable() {
-        mBinder.isVolumeOffsetAvailable(mDevice, mSource);
-        verify(mService).isVolumeOffsetAvailable(mDevice);
+    fun isVolumeOffsetAvailable() {
+        binder.isVolumeOffsetAvailable(device, source)
+        verify(service).isVolumeOffsetAvailable(device)
     }
 
     @Test
-    public void getNumberOfVolumeOffsetInstances() {
-        mBinder.getNumberOfVolumeOffsetInstances(mDevice, mSource);
-        verify(mService).getNumberOfVolumeOffsetInstances(mDevice);
+    fun getNumberOfVolumeOffsetInstances() {
+        binder.getNumberOfVolumeOffsetInstances(device, source)
+        verify(service).getNumberOfVolumeOffsetInstances(device)
     }
 
     @Test
-    public void setVolumeOffset() {
-        int instanceId = 1;
-        int volumeOffset = 2;
+    fun setVolumeOffset() {
+        val instanceId = 1
+        val volumeOffset = 2
 
-        mBinder.setVolumeOffset(mDevice, instanceId, volumeOffset, mSource);
-        verify(mService).setVolumeOffset(mDevice, instanceId, volumeOffset);
+        binder.setVolumeOffset(device, instanceId, volumeOffset, source)
+        verify(service).setVolumeOffset(device, instanceId, volumeOffset)
     }
 
     @Test
-    public void setDeviceVolume() {
-        int volume = 1;
-        boolean isGroupOp = true;
+    fun setDeviceVolume() {
+        val volume = 1
+        val isGroupOp = true
 
-        mBinder.setDeviceVolume(mDevice, volume, isGroupOp, mSource);
-        verify(mService).setDeviceVolume(mDevice, volume, isGroupOp);
+        binder.setDeviceVolume(device, volume, isGroupOp, source)
+        verify(service).setDeviceVolume(device, volume, isGroupOp)
     }
 
     @Test
-    public void getNumberOfAudioInputControlServices() {
-        mBinder.getNumberOfAudioInputControlServices(mSource, mDevice);
+    fun getNumberOfAudioInputControlServices() {
+        binder.getNumberOfAudioInputControlServices(source, device)
     }
 
     @Test
-    public void registerAudioInputControlCallback() {
-        int instanceId = 1;
-        IAudioInputCallback callback = mock(IAudioInputCallback.class);
+    fun registerAudioInputControlCallback() {
+        val instanceId = 1
+        val callback = mock<IAudioInputCallback>()
 
-        mBinder.registerAudioInputControlCallback(mSource, mDevice, instanceId, callback);
+        binder.registerAudioInputControlCallback(source, device, instanceId, callback)
     }
 
     @Test
-    public void unregisterAudioInputControlCallback() {
-        int instanceId = 1;
-        IAudioInputCallback callback = mock(IAudioInputCallback.class);
+    fun unregisterAudioInputControlCallback() {
+        val instanceId = 1
+        val callback = mock<IAudioInputCallback>()
 
-        mBinder.unregisterAudioInputControlCallback(mSource, mDevice, instanceId, callback);
+        binder.unregisterAudioInputControlCallback(source, device, instanceId, callback)
     }
 
     @Test
-    public void getAudioInputGainSettingUnit() {
-        int instanceId = 1;
-        mBinder.getAudioInputGainSettingUnit(mSource, mDevice, instanceId);
+    fun getAudioInputGainSettingUnit() {
+        val instanceId = 1
+        binder.getAudioInputGainSettingUnit(source, device, instanceId)
     }
 
     @Test
-    public void getAudioInputGainSettingMin() {
-        int instanceId = 1;
-        mBinder.getAudioInputGainSettingMin(mSource, mDevice, instanceId);
+    fun getAudioInputGainSettingMin() {
+        val instanceId = 1
+        binder.getAudioInputGainSettingMin(source, device, instanceId)
     }
 
     @Test
-    public void getAudioInputGainSettingMax() {
-        int instanceId = 1;
-        mBinder.getAudioInputGainSettingMax(mSource, mDevice, instanceId);
+    fun getAudioInputGainSettingMax() {
+        val instanceId = 1
+        binder.getAudioInputGainSettingMax(source, device, instanceId)
     }
 
     @Test
-    public void getAudioInputDescription() {
-        int instanceId = 1;
-        mBinder.getAudioInputDescription(mSource, mDevice, instanceId);
+    fun getAudioInputDescription() {
+        val instanceId = 1
+        binder.getAudioInputDescription(source, device, instanceId)
     }
 
     @Test
-    public void isAudioInputDescriptionWritable() {
-        int instanceId = 1;
-        mBinder.isAudioInputDescriptionWritable(mSource, mDevice, instanceId);
+    fun isAudioInputDescriptionWritable() {
+        val instanceId = 1
+        binder.isAudioInputDescriptionWritable(source, device, instanceId)
     }
 
     @Test
-    public void setAudioInputDescription() {
-        int instanceId = 1;
-        String description = "test";
-        mBinder.setAudioInputDescription(mSource, mDevice, instanceId, description);
+    fun setAudioInputDescription() {
+        val instanceId = 1
+        val description = "test"
+        binder.setAudioInputDescription(source, device, instanceId, description)
     }
 
     @Test
-    public void getAudioInputStatus() {
-        int instanceId = 1;
-        mBinder.getAudioInputStatus(mSource, mDevice, instanceId);
+    fun getAudioInputStatus() {
+        val instanceId = 1
+        binder.getAudioInputStatus(source, device, instanceId)
     }
 
     @Test
-    public void getAudioInputType() {
-        int instanceId = 1;
-        mBinder.getAudioInputType(mSource, mDevice, instanceId);
+    fun getAudioInputType() {
+        val instanceId = 1
+        binder.getAudioInputType(source, device, instanceId)
     }
 
     @Test
-    public void getAudioInputGainSetting() {
-        int instanceId = 1;
-        mBinder.getAudioInputGainSetting(mSource, mDevice, instanceId);
+    fun getAudioInputGainSetting() {
+        val instanceId = 1
+        binder.getAudioInputGainSetting(source, device, instanceId)
     }
 
     @Test
-    public void setAudioInputGainSetting() {
-        int instanceId = 1;
-        int gainSetting = 2;
-        mBinder.setAudioInputGainSetting(mSource, mDevice, instanceId, gainSetting);
+    fun setAudioInputGainSetting() {
+        val instanceId = 1
+        val gainSetting = 2
+        binder.setAudioInputGainSetting(source, device, instanceId, gainSetting)
     }
 
     @Test
-    public void getAudioInputGainMode() {
-        int instanceId = 1;
-        mBinder.getAudioInputGainMode(mSource, mDevice, instanceId);
+    fun getAudioInputGainMode() {
+        val instanceId = 1
+        binder.getAudioInputGainMode(source, device, instanceId)
     }
 
     @Test
-    public void setAudioInputGainMode() {
-        int instanceId = 1;
-        int gainMode = 2;
-        mBinder.setAudioInputGainMode(mSource, mDevice, instanceId, gainMode);
+    fun setAudioInputGainMode() {
+        val instanceId = 1
+        val gainMode = 2
+        binder.setAudioInputGainMode(source, device, instanceId, gainMode)
     }
 
     @Test
-    public void getAudioInputMute() {
-        int instanceId = 1;
-        mBinder.getAudioInputMute(mSource, mDevice, instanceId);
+    fun getAudioInputMute() {
+        val instanceId = 1
+        binder.getAudioInputMute(source, device, instanceId)
     }
 
     @Test
-    public void setAudioInputMute() {
-        int instanceId = 1;
-        int mute = 2;
-        mBinder.setAudioInputMute(mSource, mDevice, instanceId, mute);
+    fun setAudioInputMute() {
+        val instanceId = 1
+        val mute = 2
+        binder.setAudioInputMute(source, device, instanceId, mute)
     }
 
     @Test
-    public void cleanup_doesNotCrash() {
-        mBinder.cleanup();
+    fun cleanup_doesNotCrash() {
+        binder.cleanup()
+    }
+
+    companion object {
+        @JvmStatic @Parameters(name = "{0}") fun getParams() = FlagsWrapper.progressionOf()
     }
 }
