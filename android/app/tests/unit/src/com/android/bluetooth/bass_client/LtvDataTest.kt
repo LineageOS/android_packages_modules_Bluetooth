5dce02eac12152753d1e52ebba70fb0cc5c99df9
Michal Belusiak <belusiak@google.com>
2026-02-23 22:19:56 +0100

    Bass: Add parsing of Streaming Audio Contexts LTV data
    Bug: 463606876
    Bug: 462182753
    Flag: com.android.bluetooth.flags.leaudio_broadcast_auto_switch_announcement
    Test: atest BaseDataTest LtvDataTest
    Change-Id: I01c8799548fef1a53b2d8efc779c99a3c90d0b1a

--- android/app/tests/unit/src/com/android/bluetooth/bass_client/LtvDataTest.kt
+++ android/app/tests/unit/src/com/android/bluetooth/bass_client/LtvDataTest.kt
@@ -18,6 +18,7 @@ package com.android.bluetooth.bass_client
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.bluetooth.bass_client.LtvData.AudioActiveState
+import com.android.bluetooth.flags.Flags
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -31,6 +32,9 @@ class LtvDataTest {
         private const val AUDIO_ACTIVE_STATE_LENGTH = 0x02
         private const val AUDIO_ACTIVE_STATE_TRUE = 0x01
         private const val AUDIO_ACTIVE_STATE_FALSE = 0x00
+
+        private const val STREAMING_AUDIO_CONTEXTS_TYPE = 0x02
+        private const val STREAMING_AUDIO_CONTEXTS_LENGTH = 0x03
     }
 
     @Test
@@ -89,5 +93,46 @@ class LtvDataTest {
             byteArrayOf(AUDIO_ACTIVE_STATE_LENGTH.toByte(), AUDIO_ACTIVE_STATE_TYPE.toByte())
         val data = LtvData.parse(metadata)
         assertThat(data.audioActiveState).isEqualTo(AudioActiveState.NONE)
+    }
+
+    @Test
+    fun parse_streamingAudioContexts() {
+        val contexts = 0x0401 // Example context
+        val metadata =
+            byteArrayOf(
+                STREAMING_AUDIO_CONTEXTS_LENGTH.toByte(),
+                STREAMING_AUDIO_CONTEXTS_TYPE.toByte(),
+                0x01.toByte(), // LSB
+                0x04.toByte(), // MSB
+            )
+        val data = LtvData.parse(metadata)
+        if (Flags.leaudioBroadcastAutoSwitchAnnouncement()) {
+            assertThat(data.streamingAudioContexts).isEqualTo(contexts)
+        } else {
+            assertThat(data.streamingAudioContexts).isNull()
+        }
+        assertThat(data.audioActiveState).isEqualTo(AudioActiveState.NONE)
+    }
+
+    @Test
+    fun parse_mixedMetadata() {
+        val contexts = 0x0004 // Media
+        val metadata =
+            byteArrayOf(
+                AUDIO_ACTIVE_STATE_LENGTH.toByte(),
+                AUDIO_ACTIVE_STATE_TYPE.toByte(),
+                AUDIO_ACTIVE_STATE_TRUE.toByte(),
+                STREAMING_AUDIO_CONTEXTS_LENGTH.toByte(),
+                STREAMING_AUDIO_CONTEXTS_TYPE.toByte(),
+                0x04.toByte(), // LSB
+                0x00.toByte(), // MSB
+            )
+        val data = LtvData.parse(metadata)
+        assertThat(data.audioActiveState).isEqualTo(AudioActiveState.TRUE)
+        if (Flags.leaudioBroadcastAutoSwitchAnnouncement()) {
+            assertThat(data.streamingAudioContexts).isEqualTo(contexts)
+        } else {
+            assertThat(data.streamingAudioContexts).isNull()
+        }
     }
 }
