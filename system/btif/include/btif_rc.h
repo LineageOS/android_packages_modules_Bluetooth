c6acef09e100227757ae7b8a4340be64bb8fcfb3
Chris Manton <cmanton@google.com>
2026-02-25 16:02:25 -0800

    btif::rc Extract typical structures into header file from source
    Migrate internal structure definitions from btif_rc.cc to
    btif_rc.h. This modularization allows for cleaner dependency
    management and will enable unit tests to reference these types
    directly.
    
    Bug: 487732284
    Test: atest net_test_btif_rc
    Flag: EXEMPT PURE_REFACTOR
    Change-Id: Id6aa9cca551357497f207f2d7bdb543be1db5c75

--- system/btif/include/btif_rc.h
+++ system/btif/include/btif_rc.h
@@ -16,12 +16,211 @@
 
 #pragma once
 
+#include <bluetooth/log.h>
 #include <bluetooth/types/address.h>
 
-#include "bta_av_api.h"
+#include <cstdint>
+#include <mutex>
+
+#include "bta/include/bta_av_api.h"
+#include "btif_status.h"
 #include "hardware/bt_rc.h"
+#include "osi/include/alarm.h"
+#include "osi/include/list.h"
+#include "stack/include/avrc_defs.h"
 
 class RawAddress;
+
+/*****************************************************************************
+ *  Constants & Macros
+ *****************************************************************************/
+
+#define RC_INVALID_TRACK_ID (0xFFFFFFFFFFFFFFFFULL)
+
+/* cod value for Headsets */
+#define COD_AV_HEADSETS 0x0404
+/* for AVRC 1.4 need to change this */
+#define MAX_RC_NOTIFICATIONS AVRC_EVT_VOLUME_CHANGE
+
+#define IDX_GET_PLAY_STATUS_RSP 0
+#define IDX_LIST_APP_ATTR_RSP 1
+#define IDX_LIST_APP_VALUE_RSP 2
+#define IDX_GET_CURR_APP_VAL_RSP 3
+#define IDX_SET_APP_VAL_RSP 4
+#define IDX_GET_APP_ATTR_TXT_RSP 5
+#define IDX_GET_APP_VAL_TXT_RSP 6
+#define IDX_GET_ELEMENT_ATTR_RSP 7
+#define IDX_SET_ADDR_PLAYER_RSP 8
+#define IDX_SET_BROWSED_PLAYER_RSP 9
+#define IDX_GET_FOLDER_ITEMS_RSP 10
+#define IDX_CHG_PATH_RSP 11
+#define IDX_GET_ITEM_ATTR_RSP 12
+#define IDX_PLAY_ITEM_RSP 13
+#define IDX_GET_TOTAL_NUM_OF_ITEMS_RSP 14
+#define IDX_SEARCH_RSP 15
+#define IDX_ADD_TO_NOW_PLAYING_RSP 16
+
+/* Update MAX value whenever IDX will be changed */
+#define MAX_CMD_QUEUE_LEN 17
+
+#define MAX_VOLUME 128
+#define MAX_LABEL 16
+#define MAX_TRANSACTIONS_PER_SESSION 16
+#define PLAY_STATUS_PLAYING 1
+#define BTIF_RC_NUM_CONN BT_RC_NUM_APP
+
+/* Configurable playback_position_changed_update interval */
+#define PLAY_POS_UPDATE_INTERVAL_PROPERTY \
+  "bluetooth.avrcp.controller.playback_pos_update_interval_sec"
+// Default interval associated with AVRC_EVT_PLAY_POS_CHANGED
+#define DEFAULT_PLAY_POS_UPDATE_INTERVAL_SEC 2
+
+#define CHECK_RC_CONNECTED(p_dev)                              \
+  do {                                                         \
+    if ((p_dev) == NULL || !(p_dev)->rc_connected) {           \
+      bluetooth::log::warn("called when RC is not connected"); \
+      return BtifStatus(NOT_READY);                            \
+    }                                                          \
+  } while (0)
+
+#define CHECK_BR_CONNECTED(p_dev)                              \
+  do {                                                         \
+    if ((p_dev) == NULL || !(p_dev)->br_connected) {           \
+      bluetooth::log::warn("called when BR is not connected"); \
+      return BtifStatus(NOT_READY);                            \
+    }                                                          \
+  } while (0)
+
+/*****************************************************************************
+ *  Type definitions
+ *****************************************************************************/
+
+typedef struct {
+  uint8_t bNotify;
+  uint8_t label;
+} btif_rc_reg_notifications_t;
+
+typedef struct {
+  uint8_t label;
+  uint8_t ctype;
+  bool is_rsp_pending;
+} btif_rc_cmd_ctxt_t;
+
+/* 2 second timeout to get command response, then we free label */
+#define BTIF_RC_TIMEOUT_MS (2 * 1000)
+
+typedef enum { eNOT_REGISTERED, eREGISTERED, eINTERIM } btif_rc_nfn_reg_status_t;
+
+typedef struct {
+  uint8_t event_id;
+  uint8_t label;
+  btif_rc_nfn_reg_status_t status;
+} btif_rc_supported_event_t;
+
+#define BTIF_RC_STS_TIMEOUT 0xFE
+
+typedef struct {
+  bool query_started;
+  uint8_t num_attrs;
+  uint8_t num_ext_attrs;
+
+  uint8_t attr_index;
+  uint8_t ext_attr_index;
+  uint8_t ext_val_index;
+  btrc_player_app_attr_t attrs[AVRC_MAX_APP_ATTR_SIZE];
+  btrc_player_app_ext_attr_t ext_attrs[AVRC_MAX_APP_ATTR_SIZE];
+} btif_rc_player_app_settings_t;
+
+// The context associated with a passthru command
+typedef struct {
+  uint8_t rc_id;
+  uint8_t key_state;
+  uint8_t custom_id;
+} rc_passthru_context_t;
+
+// The context associated with a vendor command
+typedef struct {
+  uint8_t pdu_id;
+  uint8_t event_id;
+} rc_vendor_context_t;
+
+// The context associated with a browsing command
+typedef struct {
+  uint8_t pdu_id;
+} rc_browse_context_t;
+
+typedef union {
+  rc_vendor_context_t vendor;
+  rc_browse_context_t browse;
+  rc_passthru_context_t passthru;
+} rc_command_context_t;
+
+// The context associated with any command transaction requiring a label.
+// The opcode determines how to determine the data in the union. Context is
+// used to track which requests have which labels
+typedef struct {
+  RawAddress rc_addr;
+  uint8_t label;
+  uint8_t opcode;
+  rc_command_context_t command;
+} rc_transaction_context_t;
+
+typedef struct {
+  bool in_use;
+  uint8_t label;
+  rc_transaction_context_t context;
+  alarm_t* timer;
+} rc_transaction_t;
+
+typedef struct {
+  std::recursive_mutex label_lock;
+  rc_transaction_t transaction[MAX_TRANSACTIONS_PER_SESSION];
+} rc_transaction_set_t;
+
+/* TODO : Merge btif_rc_reg_notifications_t and btif_rc_cmd_ctxt_t to a single
+ * struct */
+typedef struct {
+  bool rc_connected;
+  bool br_connected;  // Browsing channel.
+  uint8_t rc_handle;
+  tBTA_AV_FEAT rc_features;
+  uint16_t rc_cover_art_psm;  // AVRCP-BIP psm
+  btrc_connection_state_t rc_state;
+  RawAddress rc_addr;
+  btif_rc_cmd_ctxt_t rc_pdu_info[MAX_CMD_QUEUE_LEN];
+  btif_rc_reg_notifications_t rc_notif[MAX_RC_NOTIFICATIONS];
+  unsigned int rc_volume;
+  uint8_t rc_vol_label;
+  list_t* rc_supported_event_list;
+  btif_rc_player_app_settings_t rc_app_settings;
+  alarm_t* rc_play_status_timer;
+  bool rc_features_processed;
+  uint64_t rc_playing_uid;
+  bool rc_procedure_complete;
+  rc_transaction_set_t transaction_set;
+  tBTA_AV_FEAT peer_ct_features;
+  tBTA_AV_FEAT peer_tg_features;
+  uint8_t launch_cmd_pending; /* true: getcap/regvolume */
+} btif_rc_device_cb_t;
+
+#define RC_PENDING_ACT_GET_CAP (1 << 0)
+#define RC_PENDING_ACT_REG_VOL (1 << 1)
+#define RC_PENDING_ACT_REPORT_CONN (1 << 2)
+
+typedef struct {
+  std::mutex lock;
+  btif_rc_device_cb_t rc_multi_cb[BTIF_RC_NUM_CONN];
+} rc_cb_t;
+
+typedef struct {
+  uint8_t handle;
+} btif_rc_handle_t;
+
+#define BTIF_STS_GEN_ERROR 0x06
+
+/*****************************************************************************
+ *  Function declarations
+ *****************************************************************************/
 
 const btrc_ctrl_interface_t* btif_rc_ctrl_get_interface(void);
 
