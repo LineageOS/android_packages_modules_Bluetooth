6f06d4e546f5e75c66873f08651ea9bbaafb251a
Ömer Faruk Yılmaz <ofy@google.com>
2026-02-28 17:15:26 +0900

    Convert record FilterParams to a Kotlin data class
    Bug: 394958283
    Test: atest FilterParamsTest
    Flag: EXEMPT PURE_REFACTOR
    Change-Id: If954797df57ee15e42ff43d855330b8b96e1489c

--- android/app/src/com/android/bluetooth/le_scan/FilterParams.kt
+++ android/app/src/com/android/bluetooth/le_scan/FilterParams.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,19 +14,20 @@
  * limitations under the License.
  */
 
-package com.android.bluetooth.le_scan;
+package com.android.bluetooth.le_scan
 
 // All values of this class are accessed from native; see com_android_bluetooth_scan.cpp
-public record FilterParams(
-        int clientInterface,
-        int filterIndex,
-        int featureSelection,
-        int listLogicType,
-        int filterLogicType,
-        int rssiHighValue,
-        int rssiLowValue,
-        int delayMode,
-        int foundTimeout,
-        int lostTimeout,
-        int foundTimeoutCount,
-        int numberOfTrackEntries) {}
+data class FilterParams(
+    @get:JvmName("clientInterface") val clientInterface: Int,
+    @get:JvmName("filterIndex") val filterIndex: Int,
+    @get:JvmName("featureSelection") val featureSelection: Int,
+    @get:JvmName("listLogicType") val listLogicType: Int,
+    @get:JvmName("filterLogicType") val filterLogicType: Int,
+    @get:JvmName("rssiHighValue") val rssiHighValue: Int,
+    @get:JvmName("rssiLowValue") val rssiLowValue: Int,
+    @get:JvmName("delayMode") val delayMode: Int,
+    @get:JvmName("foundTimeout") val foundTimeout: Int,
+    @get:JvmName("lostTimeout") val lostTimeout: Int,
+    @get:JvmName("foundTimeoutCount") val foundTimeoutCount: Int,
+    @get:JvmName("numberOfTrackEntries") val numberOfTrackEntries: Int,
+)
