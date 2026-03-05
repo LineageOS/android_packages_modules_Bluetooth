7de4c0410be6d3e3474bdfd6421d52ac75f9a13d
Ömer Faruk Yılmaz <ofy@google.com>
2026-03-05 23:11:04 +0900

    ScanBinder calls from framework will always be on binder thread
    With the removal of flag `leaudio_broadcast_improve_source_operations`
    in ag/38761656, this CL is basically a revert of the previous CL:
      ag/37023217 Allow all `ScanBinder` calls directly if already on Scan thread
    
    Bug: 397863857
    Bug: 444010402
    Test: atest com.android.bluetooth.le_scan
    Flag: EXEMPT PURE_REFACTOR
    Change-Id: Id806cbaba0194487d3e0f75592b562268d377d9e

--- android/app/src/com/android/bluetooth/le_scan/ScanBinder.kt
+++ android/app/src/com/android/bluetooth/le_scan/ScanBinder.kt
@@ -56,7 +56,10 @@ class ScanBinder(
         source: AttributionSource,
         method: String,
         block: ScanController.() -> Unit,
-    ) = getController(source, method)?.let { it.runOrDoOnScanThread(it, block) }
+    ) =
+        getController(source, method)?.let { controller ->
+            controller.doOnScanThread { controller.block() }
+        }
 
     @RequiresPermission(BLUETOOTH_SCAN)
     private fun getController(source: AttributionSource, method: String): ScanController? {
@@ -160,7 +163,7 @@ class ScanBinder(
 
     override fun numHwTrackFiltersAvailable(source: AttributionSource): Int {
         val scan = getController(source, "numHwTrackFiltersAvailable") ?: return 0
-        return scan.runOrFetchOnScanThread(scan, 0) { scan.numHwTrackFiltersAvailable() }
+        return scan.fetchOnScanThread({ scan.numHwTrackFiltersAvailable() }, 0)
     }
 
     @RequiresPermission(value = BLUETOOTH_PRIVILEGED, conditional = true)
@@ -219,27 +222,5 @@ class ScanBinder(
         }
 
         enforcePrivilegedPermissionIfNeeded(filters)
-    }
-
-    // TODO(b/444010402) Delete on Flags.leaudioBroadcastImproveSourceOperations() cleanup
-    private fun <T> ScanController.runOrDoOnScanThread(target: T, block: T.() -> Unit) {
-        if (isOnScanThread) {
-            target.block()
-        } else {
-            doOnScanThread { target.block() }
-        }
-    }
-
-    // TODO(b/444010402) Delete on Flags.leaudioBroadcastImproveSourceOperations() cleanup
-    private fun <T, R> ScanController.runOrFetchOnScanThread(
-        target: T,
-        defaultValue: R,
-        block: T.() -> R,
-    ): R {
-        return if (isOnScanThread) {
-            target.block()
-        } else {
-            fetchOnScanThread<R>({ target.block() }, defaultValue)
-        }
     }
 }
