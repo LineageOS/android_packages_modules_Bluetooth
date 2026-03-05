710bf5949bc69b49a814700683f165da54c7c553
Henri Chataing <henrichataing@google.com>
2026-03-05 17:00:37 +0100

    a2dp_aidl_transport: Change log tag to bluetooth-a2dp-aidl
    All source files in  system/audio_hal_interface/aidl/a2dp
    should have the same log tag
    
    Bug: 331817295
    Flag: EXEMPT PURE_REFACTOR
    Test: m com.android.bt
    Change-Id: I6cbdcba0ffa27ae7a25e69ff9f9c04193d7b3625

--- system/audio_hal_interface/aidl/a2dp/a2dp_aidl_transport.cc
+++ system/audio_hal_interface/aidl/a2dp/a2dp_aidl_transport.cc
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_TAG "bluetooth-a2dp-ahal-aidl"
+#define LOG_TAG "bluetooth-a2dp-aidl"
 
 #include "a2dp_aidl_transport.h"
 
