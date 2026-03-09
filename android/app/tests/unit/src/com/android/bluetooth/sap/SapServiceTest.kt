c93c5df6490538ef5da9785a14a72fb45ae107b8
Ömer Faruk Yılmaz <ofy@google.com>
2026-03-09 13:01:40 +0900

    Convert SapServiceTest to Kotlin
    - Update Mockito usage to mockito-kotlin syntax
    - Remove Hungarian notation ('m' prefix) from variable names
    - Replace Util functions with Kotlin Idiomatic versions
    
    Bug: 404652736
    Test: atest SapServiceTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: Ic35ed6d5e4c6adb8c23659f29e97239f1cd63e4f

--- android/app/tests/unit/src/com/android/bluetooth/sap/SapServiceTest.kt
+++ android/app/tests/unit/src/com/android/bluetooth/sap/SapServiceTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,99 +14,97 @@
  * limitations under the License.
  */
 
-package com.android.bluetooth.sap;
+package com.android.bluetooth.sap
 
-import static android.bluetooth.BluetoothProfile.CONNECTION_POLICY_ALLOWED;
-import static android.bluetooth.BluetoothProfile.CONNECTION_POLICY_FORBIDDEN;
-import static android.bluetooth.BluetoothProfile.CONNECTION_POLICY_UNKNOWN;
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothProfile
+import android.bluetooth.BluetoothProfile.CONNECTION_POLICY_ALLOWED
+import android.bluetooth.BluetoothProfile.CONNECTION_POLICY_FORBIDDEN
+import android.bluetooth.BluetoothProfile.CONNECTION_POLICY_UNKNOWN
+import android.content.Intent
+import android.os.Looper
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.android.bluetooth.btservice.AdapterService
+import com.android.bluetooth.getTestDevice
+import com.android.bluetooth.mockBluetoothManager
+import com.android.tests.bluetooth.MockitoRule
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.whenever
 
-import static com.android.bluetooth.TestUtils.getTestDevice;
-import static com.android.bluetooth.TestUtils.mockGetBluetoothManager;
-
-import static com.google.common.truth.Truth.assertThat;
-
-import static org.mockito.Mockito.doReturn;
-
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothProfile;
-import android.content.Intent;
-import android.os.Looper;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.MediumTest;
-
-import com.android.bluetooth.btservice.AdapterService;
-import com.android.tests.bluetooth.MockitoRule;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-
-/** Test cases for {@link SapService}. */
+/** Test cases for [SapService]. */
 @MediumTest
-@RunWith(AndroidJUnit4.class)
-public class SapServiceTest {
-    @Rule public final MockitoRule mMockitoRule = new MockitoRule();
+@RunWith(AndroidJUnit4::class)
+class SapServiceTest {
+    @get:Rule val mockitoRule = MockitoRule()
 
-    @Mock private AdapterService mAdapterService;
+    @Mock private lateinit var adapterService: AdapterService
 
-    private final BluetoothDevice mDevice = getTestDevice(0);
+    private val device = getTestDevice(0)
 
-    private SapService mService;
+    private lateinit var service: SapService
 
     @Before
-    public void setUp() {
+    fun setUp() {
         if (Looper.myLooper() == null) {
-            Looper.prepare();
+            Looper.prepare()
         }
-        mockGetBluetoothManager(mAdapterService);
-        mService = new SapService(mAdapterService);
-        mService.setAvailable(true);
+        adapterService.mockBluetoothManager()
+        service = SapService(adapterService)
+        service.isAvailable = true
     }
 
     @After
-    public void tearDown() {
-        mService.cleanup();
+    fun tearDown() {
+        service.cleanup()
     }
 
     /** Test get connection policy for BluetoothDevice */
     @Test
-    public void testGetConnectionPolicy() {
+    fun testGetConnectionPolicy() {
         doReturn(CONNECTION_POLICY_UNKNOWN)
-                .when(mAdapterService)
-                .getProfileConnectionPolicy(mDevice, BluetoothProfile.SAP);
-        assertThat(mService.getConnectionPolicy(mDevice)).isEqualTo(CONNECTION_POLICY_UNKNOWN);
+            .whenever(adapterService)
+            .getProfileConnectionPolicy(device, BluetoothProfile.SAP)
+        assertThat(service.getConnectionPolicy(device)).isEqualTo(CONNECTION_POLICY_UNKNOWN)
 
         doReturn(CONNECTION_POLICY_FORBIDDEN)
-                .when(mAdapterService)
-                .getProfileConnectionPolicy(mDevice, BluetoothProfile.SAP);
-        assertThat(mService.getConnectionPolicy(mDevice)).isEqualTo(CONNECTION_POLICY_FORBIDDEN);
+            .whenever(adapterService)
+            .getProfileConnectionPolicy(device, BluetoothProfile.SAP)
+        assertThat(service.getConnectionPolicy(device)).isEqualTo(CONNECTION_POLICY_FORBIDDEN)
 
         doReturn(CONNECTION_POLICY_ALLOWED)
-                .when(mAdapterService)
-                .getProfileConnectionPolicy(mDevice, BluetoothProfile.SAP);
+            .whenever(adapterService)
+            .getProfileConnectionPolicy(device, BluetoothProfile.SAP)
 
-        assertThat(mService.getConnectionPolicy(mDevice)).isEqualTo(CONNECTION_POLICY_ALLOWED);
+        assertThat(service.getConnectionPolicy(device)).isEqualTo(CONNECTION_POLICY_ALLOWED)
     }
 
     @Test
-    public void testGetRemoteDevice() {
-        assertThat(mService.getRemoteDevice()).isNull();
+    fun testGetRemoteDevice() {
+        assertThat(service.remoteDevice).isNull()
     }
 
     @Test
-    public void testGetRemoteDeviceName() {
-        assertThat(SapService.getRemoteDeviceName()).isNull();
+    fun testGetRemoteDeviceName() {
+        assertThat(SapService.getRemoteDeviceName()).isNull()
     }
 
     @Test
-    public void testReceiver_ConnectionAccessReplyIntent_shouldNotCrash() {
-        Intent intent = new Intent(BluetoothDevice.ACTION_CONNECTION_ACCESS_REPLY);
-        intent.putExtra(
-                BluetoothDevice.EXTRA_ACCESS_REQUEST_TYPE, BluetoothDevice.REQUEST_TYPE_SIM_ACCESS);
-        mService.mSapReceiver.onReceive(null, intent);
+    fun testReceiver_ConnectionAccessReplyIntent_shouldNotCrash() {
+        val intent =
+            Intent(BluetoothDevice.ACTION_CONNECTION_ACCESS_REPLY).apply {
+                putExtra(
+                    BluetoothDevice.EXTRA_ACCESS_REQUEST_TYPE,
+                    BluetoothDevice.REQUEST_TYPE_SIM_ACCESS,
+                )
+            }
+        service.mSapReceiver.onReceive(null, intent)
     }
 }
