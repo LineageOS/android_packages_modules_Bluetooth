6ccc4ee5b0881198ec0d62a9d04676749c2644b0
Ömer Faruk Yılmaz <ofy@google.com>
2026-03-10 15:52:22 +0900

    Convert PbapClientObexTransportTest to Kotlin
    - Update Mockito usage to mockito-kotlin syntax
    - Remove Hungarian notation ('m' prefix) from variable names
    - Replace Util functions with Kotlin Idiomatic versions
    
    Bug: 404652736
    Test: atest PbapClientObexTransportTest
    Flag: EXEMPT TEST_ONLY
    Change-Id: I6ee72ed6f84df0ba5c429adb77bff42eee40049b

--- android/app/tests/unit/src/com/android/bluetooth/pbapclient/PbapClientObexTransportTest.kt
+++ android/app/tests/unit/src/com/android/bluetooth/pbapclient/PbapClientObexTransportTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 The Android Open Source Project
+ * Copyright (C) 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,175 +14,165 @@
  * limitations under the License.
  */
 
-package com.android.bluetooth.pbapclient;
+package com.android.bluetooth.pbapclient
 
-import static com.android.bluetooth.TestUtils.getTestDevice;
+import android.bluetooth.BluetoothSocket
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.bluetooth.getTestDevice
+import com.android.tests.bluetooth.MockitoRule
+import com.google.common.truth.Truth.assertThat
+import java.io.InputStream
+import java.io.OutputStream
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoMoreInteractions
+import org.mockito.kotlin.whenever
 
-import static com.google.common.truth.Truth.assertThat;
+/** Test cases for [PbapClientObexTransport]. */
+@RunWith(AndroidJUnit4::class)
+class PbapClientObexTransportTest {
+    @get:Rule val mockitoRule = MockitoRule()
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+    private val device = getTestDevice(1)
 
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import com.android.tests.bluetooth.MockitoRule;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-/** Test cases for {@link PbapClientObexTransport}. */
-@RunWith(AndroidJUnit4.class)
-public class PbapClientObexTransportTest {
-    @Rule public final MockitoRule mMockitoRule = new MockitoRule();
-
-    private final BluetoothDevice mDevice = getTestDevice(1);
-
-    @Mock private PbapClientSocket mMockSocket;
-    @Mock private InputStream mMockInputStream;
-    @Mock private OutputStream mMockOutputStream;
+    @Mock private lateinit var mockSocket: PbapClientSocket
+    @Mock private lateinit var mockInputStream: InputStream
+    @Mock private lateinit var mockOutputStream: OutputStream
 
     @Before
-    public void setUp() throws IOException {
-        doReturn(mMockInputStream).when(mMockSocket).getInputStream();
-        doReturn(mMockOutputStream).when(mMockSocket).getOutputStream();
-        doReturn(BluetoothSocket.TYPE_L2CAP).when(mMockSocket).getConnectionType();
-        doReturn(255).when(mMockSocket).getMaxTransmitPacketSize();
-        doReturn(255).when(mMockSocket).getMaxReceivePacketSize();
-        doReturn(mDevice).when(mMockSocket).getRemoteDevice();
+    fun setUp() {
+        doReturn(mockInputStream).whenever(mockSocket).inputStream
+        doReturn(mockOutputStream).whenever(mockSocket).outputStream
+        doReturn(BluetoothSocket.TYPE_L2CAP).whenever(mockSocket).connectionType
+        doReturn(255).whenever(mockSocket).maxTransmitPacketSize
+        doReturn(255).whenever(mockSocket).maxReceivePacketSize
+        doReturn(device).whenever(mockSocket).remoteDevice
     }
 
     @Test
-    public void testCloseTransport_socketIsClosed() throws IOException {
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        transport.close();
-        verify(mMockSocket).close();
+    fun testCloseTransport_socketIsClosed() {
+        val transport = PbapClientObexTransport(mockSocket)
+        transport.close()
+        verify(mockSocket).close()
     }
 
     @Test
-    public void testOpenDataInputStream_containsSocketStream() throws IOException {
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        DataInputStream in = transport.openDataInputStream();
+    fun testOpenDataInputStream_containsSocketStream() {
+        val transport = PbapClientObexTransport(mockSocket)
+        val inputStream = transport.openDataInputStream()
 
         // DataInputStreams don't allow access to their underlying object, so we
         // can just do an operation and make sure the mock stream is used for
         // said operation
-        doReturn(1).when(mMockInputStream).read();
-        in.readBoolean();
-        verify(mMockInputStream).read();
+        doReturn(1).whenever(mockInputStream).read()
+        inputStream.readBoolean()
+        verify(mockInputStream).read()
     }
 
     @Test
-    public void testOpenDataOutputStream_containsSocketStream() throws IOException {
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        DataOutputStream out = transport.openDataOutputStream();
+    fun testOpenDataOutputStream_containsSocketStream() {
+        val transport = PbapClientObexTransport(mockSocket)
+        val out = transport.openDataOutputStream()
 
         // DataOutputStreams don't allow access to their underlying object, so we
         // can just do an operation and make sure the mock stream is used for
         // said operation
-        out.flush();
-        verify(mMockOutputStream).flush();
+        out.flush()
+        verify(mockOutputStream).flush()
     }
 
     @Test
-    public void testOpenInputStream_containsSocketStream() throws IOException {
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        InputStream in = transport.openInputStream();
-        assertThat(mMockInputStream).isEqualTo(in);
+    fun testOpenInputStream_containsSocketStream() {
+        val transport = PbapClientObexTransport(mockSocket)
+        val inputStream = transport.openInputStream()
+        assertThat(inputStream).isEqualTo(mockInputStream)
     }
 
     @Test
-    public void testOpenOutputStream_containsSocketStream() throws IOException {
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        OutputStream out = transport.openOutputStream();
-        assertThat(mMockOutputStream).isEqualTo(out);
+    fun testOpenOutputStream_containsSocketStream() {
+        val transport = PbapClientObexTransport(mockSocket)
+        val out = transport.openOutputStream()
+        assertThat(out).isEqualTo(mockOutputStream)
     }
 
     @Test
-    public void testConnect_doesNothing() throws IOException {
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        transport.connect();
-        verifyNoMoreInteractions(mMockSocket);
+    fun testConnect_doesNothing() {
+        val transport = PbapClientObexTransport(mockSocket)
+        transport.connect()
+        verifyNoMoreInteractions(mockSocket)
     }
 
     @Test
-    public void testCreate_doesNothing() throws IOException {
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        transport.create();
-        verifyNoMoreInteractions(mMockSocket);
+    fun testCreate_doesNothing() {
+        val transport = PbapClientObexTransport(mockSocket)
+        transport.create()
+        verifyNoMoreInteractions(mockSocket)
     }
 
     @Test
-    public void testDisconnect_doesNothing() throws IOException {
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        transport.disconnect();
-        verifyNoMoreInteractions(mMockSocket);
+    fun testDisconnect_doesNothing() {
+        val transport = PbapClientObexTransport(mockSocket)
+        transport.disconnect()
+        verifyNoMoreInteractions(mockSocket)
     }
 
     @Test
-    public void testListen_doesNothing() throws IOException {
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        transport.listen();
-        verifyNoMoreInteractions(mMockSocket);
+    fun testListen_doesNothing() {
+        val transport = PbapClientObexTransport(mockSocket)
+        transport.listen()
+        verifyNoMoreInteractions(mockSocket)
     }
 
     @Test
-    public void testIsConnected_returnsTrue() throws IOException {
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        assertThat(transport.isConnected()).isTrue();
-        verifyNoMoreInteractions(mMockSocket);
+    fun testIsConnected_returnsTrue() {
+        val transport = PbapClientObexTransport(mockSocket)
+        assertThat(transport.isConnected).isTrue()
+        verifyNoMoreInteractions(mockSocket)
     }
 
     @Test
-    public void testGetMaxTransmitPacketSize_transportL2cap_returns255() {
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        assertThat(transport.getMaxTransmitPacketSize()).isEqualTo(255);
+    fun testGetMaxTransmitPacketSize_transportL2cap_returns255() {
+        val transport = PbapClientObexTransport(mockSocket)
+        assertThat(transport.maxTransmitPacketSize).isEqualTo(255)
     }
 
     @Test
-    public void testGetMaxTransmitPacketSize_transportRfcomm_returnsUnspecified() {
-        doReturn(BluetoothSocket.TYPE_RFCOMM).when(mMockSocket).getConnectionType();
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        assertThat(transport.getMaxTransmitPacketSize())
-                .isEqualTo(PbapClientObexTransport.PACKET_SIZE_UNSPECIFIED);
+    fun testGetMaxTransmitPacketSize_transportRfcomm_returnsUnspecified() {
+        doReturn(BluetoothSocket.TYPE_RFCOMM).whenever(mockSocket).connectionType
+        val transport = PbapClientObexTransport(mockSocket)
+        assertThat(transport.maxTransmitPacketSize)
+            .isEqualTo(PbapClientObexTransport.PACKET_SIZE_UNSPECIFIED)
     }
 
     @Test
-    public void testGetMaxReceivePacketSize_transportL2cap_returns255() {
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        assertThat(transport.getMaxReceivePacketSize()).isEqualTo(255);
+    fun testGetMaxReceivePacketSize_transportL2cap_returns255() {
+        val transport = PbapClientObexTransport(mockSocket)
+        assertThat(transport.maxReceivePacketSize).isEqualTo(255)
     }
 
     @Test
-    public void testGetMaxReceivePacketSize_transportRfcomm_returnsUnspecified() {
-        doReturn(BluetoothSocket.TYPE_RFCOMM).when(mMockSocket).getConnectionType();
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        assertThat(transport.getMaxReceivePacketSize())
-                .isEqualTo(PbapClientObexTransport.PACKET_SIZE_UNSPECIFIED);
+    fun testGetMaxReceivePacketSize_transportRfcomm_returnsUnspecified() {
+        doReturn(BluetoothSocket.TYPE_RFCOMM).whenever(mockSocket).connectionType
+        val transport = PbapClientObexTransport(mockSocket)
+        assertThat(transport.maxReceivePacketSize)
+            .isEqualTo(PbapClientObexTransport.PACKET_SIZE_UNSPECIFIED)
     }
 
     @Test
-    public void testIsSrmSupported_transportL2cap_returnsTrue() {
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        assertThat(transport.isSrmSupported()).isTrue();
+    fun testIsSrmSupported_transportL2cap_returnsTrue() {
+        val transport = PbapClientObexTransport(mockSocket)
+        assertThat(transport.isSrmSupported).isTrue()
     }
 
     @Test
-    public void testIsSrmSupported_transportRfcomm_returnsFalse() {
-        doReturn(BluetoothSocket.TYPE_RFCOMM).when(mMockSocket).getConnectionType();
-        PbapClientObexTransport transport = new PbapClientObexTransport(mMockSocket);
-        assertThat(transport.isSrmSupported()).isFalse();
+    fun testIsSrmSupported_transportRfcomm_returnsFalse() {
+        doReturn(BluetoothSocket.TYPE_RFCOMM).whenever(mockSocket).connectionType
+        val transport = PbapClientObexTransport(mockSocket)
+        assertThat(transport.isSrmSupported).isFalse()
     }
 }
